7c5867815a8fcd7c8ae2e419e79929264c9051cd
Mahesh Lanka <mlanka@codeaurora.org>
2011-12-23 13:17:09 +0000

    libstagefright: Enable meta mode for 7x27a video encoder
    msm7627a family of chipsets supports only one input frame at
    one time. camera expects the input frame to be released before
    sending another frame. so send one input buffer and available
    output bufefrs to video encoder component. Release the input
    buffer to camera (to get another frame) after receiving input
    frame done callback from video encoder component.
    
    Change-Id: I8fbc911f046ff7e2a3047dd65d8c09ac2d8608e2

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1284,6 +1284,9 @@ void StagefrightRecorder::clipVideoFrameWidth() {
 
 status_t StagefrightRecorder::checkVideoEncoderCapabilities(
         bool *supportsCameraSourceMetaDataMode) {
+#ifdef USE_SUBMIT_ONE_INPUT_BUFFER
+    *supportsCameraSourceMetaDataMode = true;
+#else
     /* hardware codecs must support camera source meta data mode */
     Vector<CodecCapabilities> codecs;
     OMXClient client;
@@ -1295,6 +1298,7 @@ status_t StagefrightRecorder::checkVideoEncoderCapabilities(
              mVideoEncoder == VIDEO_ENCODER_H264 ? MEDIA_MIMETYPE_VIDEO_AVC : ""),
             false /* decoder */, true /* hwCodec */, &codecs);
     *supportsCameraSourceMetaDataMode = codecs.size() > 0;
+#endif
     ALOGV("encoder %s camera source meta-data mode",
             *supportsCameraSourceMetaDataMode ? "supports" : "DOES NOT SUPPORT");
 
@@ -1653,6 +1657,10 @@ status_t StagefrightRecorder::setupVideoEncoder(
     uint32_t encoder_flags = 0;
     if (mIsMetaDataStoredInVideoBuffers) {
         encoder_flags |= OMXCodec::kStoreMetaDataInVideoBuffers;
+#ifdef USE_SUBMIT_ONE_INPUT_BUFFER
+        ALOGW("msm7627 family of chipsets supports, only one buffer at a time");
+        encoder_flags |= OMXCodec::kOnlySubmitOneInputBufferAtOneTime;
+#endif
     }
 
     // Do not wait for all the input buffers to become available.
