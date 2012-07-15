5402ce924f8961e4b3f6ed9142640e9c736ac27f
XpLoDWilD <xplodgui@gmail.com>
2012-07-15 13:53:18 +0200

    Add Samsung OMX and Color Format support (2/2)
    Change-Id: I76ca4c2bcd61ee27156d6104f212b8ec0b491fbd

--- media/libstagefright/colorconversion/Android.mk
+++ media/libstagefright/colorconversion/Android.mk
@@ -9,6 +9,10 @@ LOCAL_C_INCLUDES := \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/hardware/msm7k
 
+ifeq ($(TARGET_BOARD_PLATFORM), exynos4)
+LOCAL_CFLAGS += -DMALI_ALIGNMENT
+endif
+
 LOCAL_MODULE:= libstagefright_color_conversion
 
 include $(BUILD_STATIC_LIBRARY)
