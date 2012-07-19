f6a63dd23c9f4d29eb5fa09cc0c5488d9b5f34c8
Danny Baumann <dannybaumann@web.de>
2012-07-19 20:59:50 +0200

    Fix a couple of nasty heap corruption bugs.
    - When replacing the buffer pointer with another one, the allocated
      length wasn't updated. As the TI encoder relies of those being matched
    up (it e.g. does a memset(pBuffer, 0, nAllocLen) at certain places), this
    could lead to random memory being overwritten (or to a segfault when
    reaching the end of the mapping)
    - When replacing the buffer, the old buffer wasn't saved and restored
      before calling freeBuffer. This led to a different address passed to
    free() than was returned by malloc(), could lead to all kinds of weird,
    undefined behaviour.
    
    Change-Id: I225db23eba2c015ecf40f13d5afb168beb129539

--- include/media/stagefright/OMXCodec.h
+++ include/media/stagefright/OMXCodec.h
@@ -172,6 +172,8 @@ private:
         size_t mSize;
         void *mData;
         MediaBuffer *mMediaBuffer;
+        OMX_U8 *mAllocatedBuffer;
+        OMX_U32 mAllocatedSize;
     };
 
     struct CodecSpecificData {
--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -1699,6 +1699,8 @@ status_t OMXCodec::allocateBuffersOnPort(OMX_U32 portIndex) {
         BufferInfo info;
         info.mData = NULL;
         info.mSize = def.nBufferSize;
+        info.mAllocatedBuffer = NULL;
+        info.mAllocatedSize = 0;
 
         IOMX::buffer_id buffer;
         if (portIndex == kPortIndexInput
@@ -1991,6 +1993,8 @@ status_t OMXCodec::allocateOutputBuffersFromNativeWindow() {
         info.mSize = def.nBufferSize;
         info.mStatus = OWNED_BY_US;
         info.mMem = NULL;
+        info.mAllocatedBuffer = NULL;
+        info.mAllocatedSize = 0;
         info.mMediaBuffer = new MediaBuffer(graphicBuffer);
         info.mMediaBuffer->setObserver(this);
         mPortBuffers[kPortIndexOutput].push(info);
@@ -2942,6 +2946,12 @@ status_t OMXCodec::freeBuffer(OMX_U32 portIndex, size_t bufIndex) {
 
     BufferInfo *info = &buffers->editItemAt(bufIndex);
 
+    if (info->mAllocatedBuffer != NULL) {
+        OMX_BUFFERHEADERTYPE *header = (OMX_BUFFERHEADERTYPE *) info->mBuffer;
+        header->pBuffer = info->mAllocatedBuffer;
+        header->nAllocLen = info->mAllocatedSize;
+    }
+
     status_t err = mOMX->freeBuffer(mNode, portIndex, info->mBuffer);
 
     if (err == OK && info->mMediaBuffer != NULL) {
@@ -3279,8 +3289,14 @@ bool OMXCodec::drainInputBuffer(BufferInfo *info) {
 
             CHECK(header->pBuffer == info->mData);
 
+            if (info->mAllocatedBuffer == NULL) {
+                info->mAllocatedBuffer = header->pBuffer;
+                info->mAllocatedSize = header->nAllocLen;
+            }
+
             header->pBuffer =
                 (OMX_U8 *)srcBuffer->data() + srcBuffer->range_offset();
+            header->nAllocLen = srcBuffer->size() - srcBuffer->range_offset();
 
             releaseBuffer = false;
             info->mMediaBuffer = srcBuffer;
