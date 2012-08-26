f2f28aa393efc1f2b8d26aaa403fa458d28da7d1
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-08-25 23:50:33 -0400

    Stagefright fix to support HD2 hardware encoder
    Added fix for older devices to be able to record video.
    This does NOT fix Qualcomm removing video support for
    ARMv6. I'm working on that, but email me for a quick
    fix right now using the vendor prebuilts
    
    This allows devices with an older processor to be able
    to encode and record video. This was a problem in ICS
    and remains one in Jelly Bean as well. This fixes that.
    Patch set 1: Initial Commit
    Patch set 2: Added missing QCOM_HARDWARE, added missing
    methods and ifdefs (thanks to AndroidMeda)
    Patch set 3 and 4: AndroidMeda fixed up a few things
    and redid the formatting (I blame Xcode on the stupid
    formatting issues....)
    Patch set 5: Finalized the new code, cleanup for merge
    
    Change-Id: I316e42c6b8b668d52b88ee5c7d0ef7eb25ff7e44

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1434,9 +1434,21 @@ status_t StagefrightRecorder::setupCameraSource(
                 mTimeBetweenTimeLapseFrameCaptureUs);
         *cameraSource = mCameraSourceTimeLapse;
     } else {
+#ifdef QCOM_HARDWARE
+        bool useMeta = true;
+        char value[PROPERTY_VALUE_MAX];
+        if (property_get("debug.camcorder.disablemeta", value, NULL) &&
+            atoi(value)) {
+            useMeta = false;
+        }
+#endif
         *cameraSource = CameraSource::CreateFromCamera(
                 mCamera, mCameraProxy, mCameraId, videoSize, mFrameRate,
+#ifdef QCOM_HARDWARE
+                mPreviewSurface, useMeta);
+#else
                 mPreviewSurface, true /*storeMetaDataInVideoBuffers*/);
+#endif
     }
     mCamera.clear();
     mCameraProxy.clear();
