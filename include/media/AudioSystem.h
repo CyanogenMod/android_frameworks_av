56828e8fe3a891a931310d2a9eb9279a10ed2ab7
Sidipotu Ashok <sashok@codeaurora.org>
2012-08-29 12:02:29 +0530

    audoflinger: Add A2DP Notifications.
    LPAPlayer needs A2DP Notifications to support LPA over BT-A2DP
    devices.
    
    Change-Id: Ib43231fd51070059cbe876ec54ee7c542bf2caa3

--- include/media/AudioSystem.h
+++ include/media/AudioSystem.h
@@ -144,6 +144,9 @@ public:
         INPUT_CLOSED,
         INPUT_CONFIG_CHANGED,
         STREAM_CONFIG_CHANGED,
+#ifdef QCOM_HARDWARE
+        A2DP_OUTPUT_STATE,
+#endif
         NUM_CONFIG_EVENTS
     };
 
