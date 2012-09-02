e575d59f14359818d24547ac4b37b6c798524ab5
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-09-02 04:33:40 +0100

    libmedia: Add ICS audiotrack constructor
    Change-Id: I5c1a3d1093cbf16111638a37601093c5eb977dd4

--- media/libmedia/AudioTrack.cpp
+++ media/libmedia/AudioTrack.cpp
@@ -788,6 +788,11 @@ audio_io_handle_t AudioTrack::getOutput_l()
     }
 }
 
+extern "C" int _ZNK7android10AudioTrack12getSessionIdEv();
+extern "C" int _ZN7android10AudioTrack12getSessionIdEv() {
+    return _ZNK7android10AudioTrack12getSessionIdEv();
+}
+
 status_t AudioTrack::attachAuxEffect(int effectId)
 {
     AutoMutex lock(mLock);
