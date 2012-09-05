649c43ca3915492f08ffa5ef0d1ea51b7dd3430c
cretin45 <cretin45@gmail.com>
2012-09-05 11:32:07 -0700

    camera: Full support for HTC camera switch
    * Adds the changes for using the "htcwc" switch.
    * ICS commit: https://github.com/CyanogenMod/android_frameworks_base/commit/6fa3953df37f6744af7c777f4ac803d4df44af33
    
    Change-Id: I845d628106e386608590df7a1ed4f635f655f140

--- services/camera/libcameraservice/Android.mk
+++ services/camera/libcameraservice/Android.mk
@@ -24,6 +24,10 @@ ifeq ($(BOARD_USE_SAMSUNG_V4L2_ION), true)
 LOCAL_CFLAGS += -DBOARD_USE_SAMSUNG_V4L2_ION
 endif
 
+ifeq ($(BOARD_HAVE_HTC_FFC), true)
+LOCAL_CFLAGS += -DBOARD_HAVE_HTC_FFC
+endif
+
 LOCAL_MODULE:= libcameraservice
 
 include $(BUILD_SHARED_LIBRARY)
