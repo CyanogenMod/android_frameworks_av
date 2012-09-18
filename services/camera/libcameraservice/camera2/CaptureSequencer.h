1f266efe8ce2976b6531fab8698716354b2247f8
Eino-Ville Talvala <etalvala@google.com>
2012-09-18 16:01:11 -0700

    Camera2: Increase capture timeout.
    Low-light modes may take longer to capture a picture, so increase the
    timeouts.
    
    Change-Id: Id50b1e4fd3186da4e75b2cc1c226ced2f8d394ac

--- services/camera/libcameraservice/camera2/CaptureSequencer.h
+++ services/camera/libcameraservice/camera2/CaptureSequencer.h
@@ -93,7 +93,7 @@ class CaptureSequencer:
     static const nsecs_t kWaitDuration = 100000000; // 100 ms
     static const int kMaxTimeoutsForPrecaptureStart = 2; // 200 ms
     static const int kMaxTimeoutsForPrecaptureEnd = 10;  // 1 sec
-    static const int kMaxTimeoutsForCaptureEnd    = 20;  // 2 sec
+    static const int kMaxTimeoutsForCaptureEnd    = 40;  // 4 sec
 
     wp<Camera2Client> mClient;
     wp<ZslProcessor> mZslProcessor;
