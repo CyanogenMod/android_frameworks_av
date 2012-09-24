2eddc1d2f99fb280786e30ec91f3c5df78556166
Dave Burke <daveburke@google.com>
2012-09-24 16:42:14 -0700

    Tune video bitrate to 5 mbps.
    Change-Id: Ieb7ab1675db6fb21087a1258465f866ecdc5475b

--- media/libstagefright/wifi-display/source/Converter.cpp
+++ media/libstagefright/wifi-display/source/Converter.cpp
@@ -115,7 +115,7 @@ status_t Converter::initEncoder() {
     mOutputFormat->setString("mime", outputMIME.c_str());
 
     int32_t audioBitrate = getBitrate("media.wfd.audio-bitrate", 64000);
-    int32_t videoBitrate = getBitrate("media.wfd.video-bitrate", 2500000);
+    int32_t videoBitrate = getBitrate("media.wfd.video-bitrate", 5000000);
 
     ALOGI("using audio bitrate of %d bps, video bitrate of %d bps",
           audioBitrate, videoBitrate);
