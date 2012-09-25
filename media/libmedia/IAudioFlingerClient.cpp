1e59905b892d0b000ce99653f482731f848b015d
Andreas Schneider <asn@cryptomilk.org>
2012-09-25 10:33:55 +0200

    libmedia: Fix build on non qcom hardware.
    Change-Id: I3eca4d5612785bbdb3dc4d23fca4f96280b34461

--- media/libmedia/IAudioFlingerClient.cpp
+++ media/libmedia/IAudioFlingerClient.cpp
@@ -51,11 +51,13 @@ public:
                 uint32_t stream = *(const uint32_t *)param2;
                 ALOGV("ioConfigChanged stream %d", stream);
                 data.writeInt32(stream);
-            } else if (event != AudioSystem::OUTPUT_CLOSED && event != AudioSystem::INPUT_CLOSED &&
+            } else if (event != AudioSystem::OUTPUT_CLOSED
+                       && event != AudioSystem::INPUT_CLOSED
 #ifdef QCOM_HARDWARE
-                    event != AudioSystem::EFFECT_CONFIG_CHANGED &&
+                       && event != AudioSystem::EFFECT_CONFIG_CHANGED
+                       && event != AudioSystem::A2DP_OUTPUT_STATE
 #endif
-                    event != AudioSystem::A2DP_OUTPUT_STATE) {
+                      ) {
                 const AudioSystem::OutputDescriptor *desc = (const AudioSystem::OutputDescriptor *)param2;
                 data.writeInt32(desc->samplingRate);
                 data.writeInt32(desc->format);
