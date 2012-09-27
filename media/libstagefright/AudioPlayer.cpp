f823a7ce4de5069aaad026fbafb01de7c9273d91
Phani Kumar Allada <pallad@codeaurora.org>
2012-09-27 18:21:54 +0530

    framework/av: Fix for frame drop in video playback
    - Breaks in video playback seen in video clips having audio content
      with sampling rates less than 16kHz.
    - While extrapolating real time with system time, the real time is wrongly
      caped.
    - Fix this issue by limiting the extrapolated real time only when EOS
      is reached.
    
    Change-Id: I95b6a8f7f1f1fd561c92955cf52151ae2890c9a7
    CRs-Fixed: 400398

--- media/libstagefright/AudioPlayer.cpp
+++ media/libstagefright/AudioPlayer.cpp
@@ -792,7 +792,7 @@ int64_t AudioPlayer::getRealTimeUsLocked() const {
 
     diffUs -= mNumFramesPlayedSysTimeUs;
 
-    if(result + diffUs <= mPositionTimeRealUs)
+    if((result + diffUs <= mPositionTimeRealUs) || (!mReachedEOS))
         return result + diffUs;
     else
         return mPositionTimeRealUs;
