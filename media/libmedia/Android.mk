538f498bbd5a2da51c9037a2689afc97f8bded8f
Glenn Kasten <gkasten@google.com>
2012-11-26 10:40:24 -0800

    Add template class SingleStateQueue
    Change-Id: If7e2bc9b2a216524ee9cbb68682e2634933b4973

--- media/libmedia/Android.mk
+++ media/libmedia/Android.mk
@@ -53,6 +53,13 @@ LOCAL_SRC_FILES:= \
     SoundPool.cpp \
     SoundPoolThread.cpp
 
+LOCAL_SRC_FILES += ../libnbaio/roundup.c
+
+# for <cutils/atomic-inline.h>
+LOCAL_CFLAGS += -DANDROID_SMP=$(if $(findstring true,$(TARGET_CPU_SMP)),1,0)
+LOCAL_SRC_FILES += SingleStateQueue.cpp
+LOCAL_CFLAGS += -DSINGLE_STATE_QUEUE_INSTANTIATIONS='"SingleStateQueueInstantiations.cpp"'
+
 LOCAL_SHARED_LIBRARIES := \
 	libui libcutils libutils libbinder libsonivox libicuuc libexpat \
         libcamera_client libstagefright_foundation \
