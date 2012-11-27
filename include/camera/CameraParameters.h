2eec3d39eed9f116a4dded5e39eff2a6f04a337d
codeworkx <codeworkx@cyanogenmod.org>
2012-11-27 01:22:13 +0100

    camera: add parameters for samsung cameras
    From: codeworkx <codeworkx@cyanogenmod.com>
    Date: Sat, 22 Sep 2012 11:36:26 +0200
    Subject: [PATCH] camera: added weather key for samsung cameras
    
    Change-Id: I1c6dc38f548fb3087abbdf7d7f83c39afaa217a1
    ---
    
    From: Steve Kondik <shade@chemlab.org>
    Date: Tue, 15 May 2012 09:41:02 -0700
    Subject: [PATCH] camera: Add parameters for some Samsung cameras
    
     * Enable with SAMSUNG_CAMERA_HARDWARE
     * Adds anti-shake, metering, and wdr modes.
    
    Change-Id: I080c90e11fcb8fa48900c01a454868f173f61eb0
    ---

--- include/camera/CameraParameters.h
+++ include/camera/CameraParameters.h
@@ -525,6 +525,13 @@ public:
     // stream and record stabilized videos.
     static const char KEY_VIDEO_STABILIZATION_SUPPORTED[];
 
+#ifdef SAMSUNG_CAMERA_HARDWARE
+    static const char KEY_ANTI_SHAKE_MODE[];
+    static const char KEY_METERING[];
+    static const char KEY_WDR[];
+    static const char KEY_WEATHER[];
+#endif
+
     // Value for KEY_ZOOM_SUPPORTED or KEY_SMOOTH_ZOOM_SUPPORTED.
     static const char TRUE[];
     static const char FALSE[];
