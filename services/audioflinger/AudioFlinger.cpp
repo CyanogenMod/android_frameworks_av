132fb55504a3aeebd1a77f418bedb26e11b266d0
Bumble-Bee <bigbeeshane@gmail.com>
2012-11-29 16:26:40 +0000

    Allow JB_MR0 Audio prebuilts to load
    Usage :
    	add "COMMON_GLOBAL_CFLAGS += -DMR0_AUDIO_BLOB" to
    	BoardConfig.mk

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -206,7 +206,7 @@ static int load_audio_interface(const char *if_name, audio_hw_device_t **dev)
     if (rc) {
         goto out;
     }
-#ifndef ICS_AUDIO_BLOB
+#if !defined(ICS_AUDIO_BLOB) && !defined(MR0_AUDIO_BLOB)
     if ((*dev)->common.version != AUDIO_DEVICE_API_VERSION_CURRENT) {
         ALOGE("%s wrong audio hw device version %04x", __func__, (*dev)->common.version);
         rc = BAD_VALUE;
