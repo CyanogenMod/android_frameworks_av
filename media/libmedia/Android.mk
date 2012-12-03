ba2fbceb77b76de094011eb0c07f74a8d904b251
Mingming Yin <mingming@codeaurora.org>
2012-12-02 18:54:34 -0800

    audio: Compile AudioParameter as shared library
    - AudioParameter as shared lib is needed by BT
      support in WFD source, SRS TruMedia
    
    Change-Id: I464b428ace0cbb57ce6bf7bf3b57d51a7d56f032

--- media/libmedia/Android.mk
+++ media/libmedia/Android.mk
@@ -11,6 +11,15 @@ include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES:= AudioParameter.cpp
+LOCAL_MODULE:= libaudioparameter
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := libutils libcutils
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES:= \
     AudioTrack.cpp \
     AudioTrackShared.cpp \
