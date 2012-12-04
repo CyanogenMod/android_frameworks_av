29e240659657b69d6658a6d1b2f54157b1fbcd20
codeworkx <codeworkx@cyanogenmod.org>
2012-12-04 20:22:11 +0100

    exynos4: add ION support
    Source:
    http://git.insignal.co.kr/samsung/exynos/android/platform/frameworks/av/commit/?h=exynos-jb&id=1614612f7ca2a00473d202dbedcb135fadc608ad
    http://git.insignal.co.kr/samsung/exynos/android/platform/frameworks/av/commit/?h=exynos-jb&id=e7da986b03f3c3f940f0eafebe753e2b2f2fe692
    
    Change-Id: Ie40b98cfe3360f3ed0345be4e185d49cfce9d4a9

--- media/libstagefright/colorconversion/Android.mk
+++ media/libstagefright/colorconversion/Android.mk
@@ -9,6 +9,10 @@ LOCAL_C_INCLUDES := \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/hardware/msm7k
 
+ifeq ($(BOARD_USE_V4L2_ION), true)
+LOCAL_CFLAGS += -DUSE_V4L2_ION
+endif
+
 LOCAL_MODULE:= libstagefright_color_conversion
 
 include $(BUILD_STATIC_LIBRARY)
