184e110d88e82cff39a7a1863db67603c0b371b9
Mingming Yin <mingming@codeaurora.org>
2012-12-13 13:22:39 -0800

    audio: Add support for QCOM's VOIP solution
    Change-Id: I108c4aea30b3e807209910ade429060e23545cde

--- media/libmedia/AudioTrack.cpp
+++ media/libmedia/AudioTrack.cpp
@@ -264,6 +264,15 @@ status_t AudioTrack::set(
         flags = (audio_output_flags_t)(flags &~AUDIO_OUTPUT_FLAG_DEEP_BUFFER);
     }
 
+    if ((streamType == AUDIO_STREAM_VOICE_CALL)
+         && (channelMask == AUDIO_CHANNEL_OUT_MONO)
+         && ((sampleRate == 8000 || sampleRate == 16000)))
+    {
+        ALOGD("Turn on Direct Output for VOIP RX");
+        flags = (audio_output_flags_t)(flags | AUDIO_OUTPUT_FLAG_VOIP_RX|AUDIO_OUTPUT_FLAG_DIRECT);
+    }
+
+
     if (!audio_is_output_channel(channelMask)) {
         ALOGE("Invalid channel mask %#x", channelMask);
         return BAD_VALUE;
