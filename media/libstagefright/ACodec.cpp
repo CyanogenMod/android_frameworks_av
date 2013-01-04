a4e8786b3741f2ac51220721defe4c1500569a05
Andrew Dodd <atd7@cornell.edu>
2013-01-03 23:16:44 -0500

    exynos4:  Part of Netflix fix
    codeworkx pulled the Samsung colorformat patch from Insignal's
    exynos-jb branch, which is an invalid source for exynos4 patches.
    
    It was checking for an OMX codec name that didn't exist.
    
    This still doesn't stop Netflix JPlayer failing in 10.1, but
    does fix Netflix failing if you disable JPlayer by convincing
    it that it's on an older Android version.
    
    Change-Id: Ia55878dc08f81e2900307558e35c4e0c5b9cac2f

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -797,7 +797,8 @@ status_t ACodec::submitOutputMetaDataBuffer() {
 void ACodec::setNativeWindowColorFormat(OMX_COLOR_FORMATTYPE &eNativeColorFormat)
 {
     // In case of Samsung decoders, we set proper native color format for the Native Window
-    if (!strcasecmp(mComponentName.c_str(), "OMX.Exynos.AVC.Decoder")) {
+    if (!strcasecmp(mComponentName.c_str(), "OMX.SEC.AVC.Decoder")
+        || !strcasecmp(mComponentName.c_str(), "OMX.SEC.FP.AVC.Decoder")) {
         switch (eNativeColorFormat) {
             case OMX_COLOR_FormatYUV420SemiPlanar:
                 eNativeColorFormat = (OMX_COLOR_FORMATTYPE)HAL_PIXEL_FORMAT_YCbCr_420_SP;
