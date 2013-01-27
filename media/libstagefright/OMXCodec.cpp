400827cdcc7b3982fd9e5c08d6568e1fb0f1809e
Hashcode <hashcode0f@gmail.com>
2013-01-27 02:38:04 -0800

    Video Encoder: Enable Ducati h264 encoder profile change
    This patch enables the TI ducati H264 encoder profile
    via BoardConfig setting:
    BOARD_USE_TI_DUCATI_H264_PROFILE := true
    
    Allows correct video decoding on Motorola OMAP4 / Kindle Fires
    and other devices using newer DOMX libs.
    
    Effectively a cherry-pick of omapzoom commit (with creative
    board setting name):
    http://www.omapzoom.org/?p=platform/frameworks/av.git;a=commit;h=e28784d5c68c8699cfd9ebe0231e7132d8b13dad
    
    Change-Id: Idc49b00030558a22a9e50e8798e5814ad54fe841
    Signed-off-by: Hashcode <hashcode0f@gmail.com>

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -1199,7 +1199,12 @@ status_t OMXCodec::setupAVCEncoderParameters(const sp<MetaData>& meta) {
     h264type.eLevel = static_cast<OMX_VIDEO_AVCLEVELTYPE>(profileLevel.mLevel);
 
     // XXX
+#ifdef USE_TI_DUCATI_H264_PROFILE
+    if ((strncmp(mComponentName, "OMX.TI.DUCATI1", 14) != 0)
+            && (h264type.eProfile != OMX_VIDEO_AVCProfileBaseline)) {
+#else
     if (h264type.eProfile != OMX_VIDEO_AVCProfileBaseline) {
+#endif
         ALOGW("Use baseline profile instead of %d for AVC recording",
             h264type.eProfile);
         h264type.eProfile = OMX_VIDEO_AVCProfileBaseline;
