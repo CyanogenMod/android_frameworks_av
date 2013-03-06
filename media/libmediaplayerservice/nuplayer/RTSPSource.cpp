0ad03bc59d090a0455f858d2f629834c105f6f37
Fredrik Rosin <fredrik.rosin@sonymobile.com>
2013-03-06 13:42:53 +0100

    Prevent double disconnects in RTSPSource
    Prevent a null pointer from occuring by checking that we are not
    already disconnected before performing a disconnect, in case
    multiple disconnects have been issued.
    
    Change-Id: Ib88800cd246086fd57f77d10748d41b1aefe2084

--- media/libmediaplayerservice/nuplayer/RTSPSource.cpp
+++ media/libmediaplayerservice/nuplayer/RTSPSource.cpp
@@ -459,6 +459,10 @@ void NuPlayer::RTSPSource::onConnected() {
 }
 
 void NuPlayer::RTSPSource::onDisconnected(const sp<AMessage> &msg) {
+    if (mState == DISCONNECTED) {
+        return;
+    }
+
     status_t err;
     CHECK(msg->findInt32("result", &err));
     CHECK_NE(err, (status_t)OK);
