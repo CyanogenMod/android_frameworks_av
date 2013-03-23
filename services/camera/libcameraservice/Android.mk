c32c0a43aaae3d8637a28aa251f88fda10739527
tbalden <illespal@gmail.com>
2013-03-23 23:06:45 +0100

    Camera: QCOM legacy definitions,HTC cam switch,NO_UPDATE_PREVIEW
    Updating Camera parameters, and service with several
    compatibility patches:
    
    1., QCOM legacy CameraParameters and functions:
    Adding these will make ICS qcomm camera blobs work on JB 4.2.
    Use BOARD_USES_QCOM_LEGACY_CAM_PARAMS := true in BoardConfig
    to use this.
    
    Also fixes the problem of legacy qcom
    camera blobs with recent libcameraservice QCOM_HARDWARE changes,
    where the orientation is messed up on first
    preview: the fix is leaving out setPreviewWindow(window) for
    legacy qcomm cam blobs.
    
    2., Also adding NO_UPDATE_PREVIEW flag for legacy purposes.
    
    3., Adding adding the set orientation patch:
    Always set buffers orientation when setting the qcom camera preview window
    Fixes the rotated preview on TouchPad
    https://github.com/CyanogenMod/android_frameworks_base/commit/1a8e41a3c7434737db89b604352575f8b3109e7a
    
    4., camera: Full support for HTC camera switch
    
    Change-Id: I2bfc72b8ef027665356788f1db7f96b31d037dbe

--- services/camera/libcameraservice/Android.mk
+++ services/camera/libcameraservice/Android.mk
@@ -40,6 +40,14 @@ LOCAL_C_INCLUDES += \
     system/media/camera/include \
     external/jpeg
 
+ifeq ($(BOARD_USES_QCOM_LEGACY_CAM_PARAMS),true)
+    LOCAL_CFLAGS += -DQCOM_LEGACY_CAM_PARAMS
+endif
+
+ifeq ($(BOARD_HAVE_HTC_FFC), true)
+LOCAL_CFLAGS += -DBOARD_HAVE_HTC_FFC
+endif
+
 LOCAL_MODULE:= libcameraservice
 
 include $(BUILD_SHARED_LIBRARY)
