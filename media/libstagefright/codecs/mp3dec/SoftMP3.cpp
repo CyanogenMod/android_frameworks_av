2242fbd1d9327dff9c25ac2aba0fa7d5d471bb88
Andreas Huber <andih@google.com>
2013-04-16 16:35:19 -0700

    Apparently the mp3 decoder glitches if it was configured with the wrong
    sample rate. Make sure we tell it about the one the extractor provides.
    
    Change-Id: Ice5b9cf55485ca1a8f099a14be0202be9540e781
    related-to-bug: 8621639

--- media/libstagefright/codecs/mp3dec/SoftMP3.cpp
+++ media/libstagefright/codecs/mp3dec/SoftMP3.cpp
@@ -166,6 +166,21 @@ OMX_ERRORTYPE SoftMP3::internalSetParameter(
             return OMX_ErrorNone;
         }
 
+        case OMX_IndexParamAudioPcm:
+        {
+            const OMX_AUDIO_PARAM_PCMMODETYPE *pcmParams =
+                (const OMX_AUDIO_PARAM_PCMMODETYPE *)params;
+
+            if (pcmParams->nPortIndex != 1) {
+                return OMX_ErrorUndefined;
+            }
+
+            mNumChannels = pcmParams->nChannels;
+            mSamplingRate = pcmParams->nSamplingRate;
+
+            return OMX_ErrorNone;
+        }
+
         default:
             return SimpleSoftOMXComponent::internalSetParameter(index, params);
     }
