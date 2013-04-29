8589aa8043d929f2d09400e9d30cb96b7bbc63bb
Matt Filetto <matt.filetto@gmail.com>
2013-04-28 23:04:33 -0700

    camera: Add a few missing legacy camera params.
    Fixes camera for some HTC msm7x30 devices
    
    Change-Id: If6975e3a5470fdeb603bf8ad9c164c9a000309ef

--- camera/CameraParameters.cpp
+++ camera/CameraParameters.cpp
@@ -61,6 +61,10 @@ const char CameraParameters::KEY_MAX_BURST_PICTURE_COUNT[] = "max-burst-picture-
 const char CameraParameters::KEY_SUPPORTED_CONTINUOUS_AF[] = "continuous-af-mode";
 const char CameraParameters::KEY_SUPPORTED_CAF[] = "continuous-af-values";
 const char CameraParameters::KEY_SUPPORTED_CAPTURE_MODES[] = "capture-mode-values";
+const char CameraParameters::KEY_TAKING_PICTURE_ZOOM[] = "taking-picture-zoom";
+const char CameraParameters::KEY_PANORAMA_MODE[] = "panorama-mode";
+const char CameraParameters::PANORAMA_MODE_NOT_INPROGRESS[] = "not-in-progress";
+const char CameraParameters::PANORAMA_MODE_INPROGRESS[] = "in-progress";
 #endif
 #endif
 const char CameraParameters::KEY_PICTURE_SIZE[] = "picture-size";
