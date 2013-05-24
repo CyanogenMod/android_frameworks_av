ae20f437e02b6b561fd35649771be4cb1132bf26
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2013-05-24 16:28:54 +0530

    libstagefright: Fix for OMXCodec::read() getting blocked
    - Sometimes notification sound is not heard if it is played
      and stopped multiple times in a short duration.
    - If OMXCodec::read() gets blocked for filled buffers while
      in pause, it never returns as mFilledBuffers is always empty.
      Due to this AudioTrack destructor never exits as it waits for
      the AudioCallback thread to unblock it.
    - Ensure OMXCodec::read() returns even if timeout occurs while
      in pause and handle the same in AudioPlayer by returning zero
      bytes in AudioCallback.
    
    CRs-Fixed: 478768
    Change-Id: I3dd70c89b250d5dde67ed7ce59dcca72cd0c2f81

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -3807,14 +3807,6 @@ status_t OMXCodec::waitForBufferFilled_l() {
         return mBufferFilled.wait(mLock);
     }
     status_t err = mBufferFilled.waitRelative(mLock, kBufferFilledEventTimeOutNs);
-#ifdef QCOM_HARDWARE
-    if ((err == -ETIMEDOUT) && (mPaused == true)){
-        // When the audio playback is paused, the fill buffer maybe timed out
-        // if input data is not available to decode. Hence, considering the
-        // timed out as a valid case.
-        err = OK;
-    }
-#endif
     if (err != OK) {
         CODEC_LOGE("Timed out waiting for output buffers: %d/%d",
             countBuffersWeOwn(mPortBuffers[kPortIndexInput]),
@@ -4635,6 +4627,15 @@ status_t OMXCodec::read(
 
     while (mState != ERROR && !mNoMoreOutputData && mFilledBuffers.empty()) {
         if ((err = waitForBufferFilled_l()) != OK) {
+#ifdef QCOM_HARDWARE
+            if ((err == -ETIMEDOUT) && (mPaused == true) && !mIsVideo) {
+                // When the audio playback is paused, the fill buffer maybe timed out
+                // if input data is not available to decode. Hence, considering the
+                // timed out as a valid case.
+                ALOGV("returned OK instead of timedout from read() as mPaused is true");
+                err = OK;
+            }
+#endif
             return err;
         }
     }
