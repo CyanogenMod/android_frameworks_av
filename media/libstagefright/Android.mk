DolbyLaboratories/caf-platform-frameworks-av

libstagefright: Add support for High Frame Rate Encoding
- Based on kkeyhfr key value from meta data, add support in OMXCodec and
  MPEG4Writer for HFR mode
- Assume normal mode recording if kKeyHfr is absent
- Increase bit rate for high frame rate (HFR) recording feature to reflect
  the corresponding increase in frame rate

Change-Id: I0a69f8d9322a768677781d08dd910dc5772c5292

media/libstagefright/Android.mk
@@ -74,6 +74,7 @@ LOCAL_SRC_FILES:=                         \
         mp4/TrackFragment.cpp             \
         ExtendedExtractor.cpp             \
         FMA2DPWriter.cpp                  \
+        QCUtilityClass.cpp                \
 
 LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/av/include/media/stagefright/timedtext \

CyanogenMod/android_frameworks_av

media/libstagefright/Android.mk
@@ -92,7 +92,8 @@ LOCAL_SRC_FILES += \
         QCMediaDefs.cpp                   \
         QCOMXCodec.cpp                    \
         WAVEWriter.cpp                    \
-        ExtendedExtractor.cpp
+        ExtendedExtractor.cpp             \
+        QCUtilityClass.cpp
 
 LOCAL_C_INCLUDES += \
         $(TOP)/hardware/qcom/media/mm-core/inc