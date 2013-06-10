fe568acf0968775a7007ff3c2f8ab48d5f925a12
Yamit Mehta <ymehta@codeaurora.org>
2013-06-10 20:00:35 +0530

    frameworks/av: Enable subsystem restart for LPA/Tunnel
    -Audio Daemon in Audio HAL intimates AudioFlinger
     of ADSP status (online/offline) using set_paramaters.
    -AudioFlinger send set_parameters to its clients
     which is DirectTrack used for LPA/Tunnel playback.
    -In case of ADSP crash during LPA/Tunnel playback,
     clean up of DirectTrack is initiated.
    
    Change-Id: If83102ddde1da880fb769d86a20884185c049bca

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -1024,9 +1024,25 @@ status_t AudioFlinger::setParameters(audio_io_handle_t ioHandle, const String8& 
             }
             mHardwareStatus = AUDIO_HW_IDLE;
         }
-        // disable AEC and NS if the device is a BT SCO headset supporting those pre processings
+
         AudioParameter param = AudioParameter(keyValuePairs);
-        String8 value;
+        String8 value, key;
+        int i = 0;
+
+        key = String8(AudioParameter::keyADSPStatus);
+        if (param.get(key, value) == NO_ERROR) {
+            ALOGV("Set keyADSPStatus:%s", value.string());
+            if (value == "ONLINE" || value == "OFFLINE") {
+               if (!mDirectAudioTracks.isEmpty()) {
+                   for (i=0; i < mDirectAudioTracks.size(); i++) {
+                       mDirectAudioTracks.valueAt(i)->stream->common.set_parameters(
+                          &mDirectAudioTracks.valueAt(i)->stream->common, keyValuePairs.string());
+                   }
+               }
+           }
+        }
+
+        // disable AEC and NS if the device is a BT SCO headset supporting those pre processings
         if (param.get(String8(AUDIO_PARAMETER_KEY_BT_NREC), value) == NO_ERROR) {
             bool btNrecIsOff = (value == AUDIO_PARAMETER_VALUE_OFF);
             if (mBtNrecIsOff != btNrecIsOff) {
