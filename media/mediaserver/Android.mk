03938b81c03cdc3ac23aa13ae29bf0a9f6d2ad1a
Mingming Yin <mingming@codeaurora.org>
2013-06-14 17:26:56 -0700

    mediaserver: conditionally launch ListenService
    Conditionally instantiate ListenService from MediaService
    
    Change-Id: Ib7a7e9bec41c64b0787433268d7ab769b852c124

--- media/mediaserver/Android.mk
+++ media/mediaserver/Android.mk
@@ -10,6 +10,10 @@ endif
 
 include $(CLEAR_VARS)
 
+ifeq ($(QCOM_LISTEN_FEATURE),true)
+  LOCAL_CFLAGS += -DQCOM_LISTEN_FEATURE_ENABLE
+endif
+
 LOCAL_SRC_FILES:= \
 	main_mediaserver.cpp 
 
@@ -35,5 +39,10 @@ LOCAL_C_INCLUDES := \
     frameworks/av/services/camera/libcameraservice
 
 LOCAL_MODULE:= mediaserver
+
+ifeq ($(QCOM_LISTEN_FEATURE),true)
+  LOCAL_SHARED_LIBRARIES += liblisten
+  LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audio-listen
+endif
 
 include $(BUILD_EXECUTABLE)
