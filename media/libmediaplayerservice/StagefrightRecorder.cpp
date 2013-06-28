214db79bc89f69e381d3cb81f5b575e5d73de332
Pavan Chikkala <pavanc@codeaurora.org>
2013-06-28 11:58:04 +0530

    libstagefright: Use 64 bit offsets only when needed.
    For enabling >2GB recording, 64 bit offsets are
    needed for file writing. So, this feature was turned
    on by default. This in turn increased the file size.
    With this change, by default this feature will be
    off and turned on only when required.
    
    - Use 64 bit offsets for resolutions >= 720p.
    - Limit maximum file size for recording to 4GB.
    - Set max file size only if no value is set from the client
    CRs-Fixed: 273144, 285785, 288319, 328461
    
    Change-Id: Ica37cb0414642c68ada18e0b7ba184b55692c6f2

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -415,6 +415,11 @@ status_t StagefrightRecorder::setParamMaxFileSizeBytes(int64_t bytes) {
         ALOGW("Target file size (%lld bytes) is too small to be respected", bytes);
     }
 
+    if (bytes >= 0xffffffffLL) {
+        ALOGW("Target file size (%lld bytes) too larger than supported, clip to 4GB", bytes);
+        bytes = 0xffffffffLL;
+    }
+
     mMaxFileSizeBytes = bytes;
     return OK;
 }
@@ -738,7 +743,18 @@ status_t StagefrightRecorder::setClientName(const String16& clientName) {
 }
 
 status_t StagefrightRecorder::prepare() {
-    return OK;
+  ALOGV(" %s E", __func__ );
+
+  if(mVideoSource != VIDEO_SOURCE_LIST_END && mVideoEncoder != VIDEO_ENCODER_LIST_END && mVideoHeight && mVideoWidth &&             /*Video recording*/
+         (mMaxFileDurationUs <=0 ||             /*Max duration is not set*/
+         (mVideoHeight * mVideoWidth < 720 * 1280 && mMaxFileDurationUs > 30*60*1000*1000) ||
+         (mVideoHeight * mVideoWidth >= 720 * 1280 && mMaxFileDurationUs > 10*60*1000*1000))) {
+    /*Above Check can be further optimized for lower resolutions to reduce file size*/
+    ALOGV("File is huge so setting 64 bit file offsets");
+    setParam64BitFileOffset(true);
+  }
+  ALOGV(" %s X", __func__ );
+  return OK;
 }
 
 status_t StagefrightRecorder::start() {
