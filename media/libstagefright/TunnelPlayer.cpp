f8a241914c29d9401b7fb1a390a5546b90ccf8b5
Hendrik Borghorst <hendrikborghorst@gmail.com>
2013-07-12 16:27:47 +0200

    TunnelAudio: Fixes playback with Qualcomm tunnel audio for players which use gapless api
    TunnelAudio player in its current stage shows an very odd behavior.
    It plays one song over DSP processor and the next is played with software decoding.
    
    This is because there are two open instances of awesomeplayer at all time.
    At the moment awesomeplayer checks at the creation if DSP processor if free, if not it falls back to software.
    
    This patch fixes that by delaying that check to the actual call of play() so,
    that the awesomeplayer which currently holds the DSP processor can free it. This is also done earlier than before.
    
    At the moment I can only test this patch on an Sony Xperia Z
    and I would really like if someone could help and test it further.
    
    PS2: Disabled delayed codec initialization for HTTPStreaming and fixed VideoSize not notifyed
    PS3: actually added changed files, sorry
    PS4: White spaces fixed
    PS6: Added guard in seek if codecs are initialized, check if tunnel is supported before delaying and
         if DSP is already in use.
    PS7: Added missing #ifdef
    PS8: Rebased for cm-10.2
    
    PS2(after rebase): fix compilation on other qcom devices
    
    Change-Id: I1bdb2351aef300778a3375cc06e048dba455121e

--- media/libstagefright/TunnelPlayer.cpp
+++ media/libstagefright/TunnelPlayer.cpp
@@ -54,6 +54,8 @@ static const char   mName[] = "TunnelPlayer";
 
 namespace android {
 int TunnelPlayer::mTunnelObjectsAlive = 0;
+bool TunnelPlayer::mTunnelObjectEarlyDeletable = false;
+bool TunnelPlayer::mTunnelObjectEarlyDeleted = false;
 
 TunnelPlayer::TunnelPlayer(
                     const sp<MediaPlayerBase::AudioSink> &audioSink, bool &initCheck,
@@ -332,6 +334,7 @@ size_t TunnelPlayer::AudioSinkCallback(
                 //in the case of seek all these flags will be reset
                 me->mReachedOutputEOS = true;
                 ALOGV("postAudioEOS mSeeking %d", me->mSeeking);
+                mTunnelObjectEarlyDeletable = true;
                 me->mObserver->postAudioEOS(0);
             }else {
                 ALOGV("postAudioEOS ignored since %d", me->mSeeking);
