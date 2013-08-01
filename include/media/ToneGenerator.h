CyanogenMod/android_frameworks_av

commit 09108adeca8cbbf3fbb21f8aea2a2ff250db9531

ToneGenerator: fix stop/destroy concurrency
There is a problem if the stopTone() method is called
from two different threads (for instance if the destructor is called
while stopTone() is waiting for the audio callback to finish).

In this case, the second call to stopTone() will not wait for the
condition to be signaled and call clearWaveGens() while the callback
can still be active, thus causing a crash.

There is a similar problem in case of concurrent calls to startTone()
and stopTone().

The fix consists in making sure that stopTone() always waits for call
back completion or timeout and exits before calling clearWaveGens()
if a concurrent start request is detected.

Bug 8163071

Change-Id: I9ddb4390407701dcad5bf83660fd9903f0d72268

media/libmedia/ToneGenerator.cpp
@@ -976,20 +976,25 @@ void ToneGenerator::stopTone() {
     ALOGV("stopTone");
 
     mLock.lock();
-    if (mState == TONE_PLAYING || mState == TONE_STARTING || mState == TONE_RESTARTING) {
-        mState = TONE_STOPPING;
+    if (mState != TONE_IDLE && mState != TONE_INIT) {
+        if (mState == TONE_PLAYING || mState == TONE_STARTING || mState == TONE_RESTARTING) {
+            mState = TONE_STOPPING;
+        }
         ALOGV("waiting cond");
         status_t lStatus = mWaitCbkCond.waitRelative(mLock, seconds(3));
         if (lStatus == NO_ERROR) {
+            // If the tone was restarted exit now before calling clearWaveGens();
+            if (mState != TONE_INIT) {
+                return;
+            }
             ALOGV("track stop complete, time %d", (unsigned int)(systemTime()/1000000));
         } else {
             ALOGE("--- Stop timed out");
             mState = TONE_IDLE;
             mpAudioTrack->stop();
         }
+        clearWaveGens();
     }
-
-    clearWaveGens();
 
     mLock.unlock();
 }
@@ -1299,7 +1304,7 @@ audioCallback_EndLoop:
         }
 
         if (lSignal)
-            lpToneGen->mWaitCbkCond.signal();
+            lpToneGen->mWaitCbkCond.broadcast();
         lpToneGen->mLock.unlock();
     }
 }

commit ff0f1690aa98d1ccf3e99bb3b5df5f368ac42b0e

Tone generator: fixes for low latency audio
TL;DR; fixes a race condition in audio when making a call

Squashed commit of the following:

commit e47286acdb49cda23ec07849c6932d31c469e610
Author: Eric Laurent <elaurent@google.com>
Date:   Mon May 13 12:47:53 2013 -0700

    Fix unreleased mutex in ToneGenerator stopTone()

    Commit 09108ade introduced a regression by not releasing
    the ToneGenerator mutex before exiting in case of error.

    Bug: 8852855
    Change-Id: I8ba2755b218842e2034ed8dbd54b18bf2a5fc571

commit 385d87f392c887ea9d17921440abc905d1de276c
Author: Eric Laurent <elaurent@google.com>
Date:   Thu Mar 28 11:58:45 2013 -0700

    ToneGenerator: fix overflow in stopTone

    Fix overflow in tone duration calculation introduced in
    commit 681be039.

    Bug 6607077

    Change-Id: Ie12f13701345c2b2d3be0b3c4d71cbfa2394a29b

commit b6393c76874c2ae0de533826a13125a473fcd2a5
Author: Eric Laurent <elaurent@google.com>
Date:   Wed Mar 27 12:05:40 2013 -0700

    ToneGenerator: fix truncated tones

    The first DTMF tone after a silent period is truncated.
    This is because the phone app starts and stops the tone
    when the user presses and releases the key.
    This combined to the fact that the tones use the low latency path
    and that when the output stream exists standby there is a period
    of several milliseconds during which no audio is mixed until the
    stream is "warmed up".
    The result is that much less audio is generated than the actual
    key press duration.

    The fix consists in storing the tone start time and making sure that
    the number of samples generated corresponds at least to the time difference
    between the tone start and stop commands.

    Bug 6607077

    Change-Id: I070d20dd8600c25a9e5d5a60c1d3313b7917b00d

commit eaadac867b5342b66bd08d5ad72f33c0cc6dac96
Author: Eric Laurent <elaurent@google.com>
Date:   Tue Mar 26 16:37:19 2013 -0700

    ToneGenerator: fix stop/destroy concurrency

include/media/ToneGenerator.h
@@ -271,6 +271,7 @@ private:
     float mVolume;  // Volume applied to audio track
     audio_stream_type_t mStreamType; // Audio stream used for output
     unsigned int mProcessSize;  // Size of audio blocks generated at a time by audioCallback() (in PCM frames).
+    struct timespec mStartTime; // tone start time: needed to guaranty actual tone duration
 
     bool initAudioTrack();
     static void audioCallback(int event, void* user, void *info);

media/libmedia/ToneGenerator.cpp
@@ -916,6 +916,9 @@ bool ToneGenerator::startTone(tone_type toneType, int durationMs) {
             ALOGV("Immediate start, time %d", (unsigned int)(systemTime()/1000000));
             lResult = true;
             mState = TONE_STARTING;
+            if (clock_gettime(CLOCK_MONOTONIC, &mStartTime) != 0) {
+                mStartTime.tv_sec = 0;
+            }
             mLock.unlock();
             mpAudioTrack->start();
             mLock.lock();
@@ -934,6 +937,7 @@ bool ToneGenerator::startTone(tone_type toneType, int durationMs) {
     } else {
         ALOGV("Delayed start");
         mState = TONE_RESTARTING;
+        mStartTime.tv_sec = 0;
         lStatus = mWaitCbkCond.waitRelative(mLock, seconds(3));
         if (lStatus == NO_ERROR) {
             if (mState != TONE_IDLE) {
@@ -970,20 +974,49 @@ void ToneGenerator::stopTone() {
     ALOGV("stopTone");
 
     mLock.lock();
-    if (mState == TONE_PLAYING || mState == TONE_STARTING || mState == TONE_RESTARTING) {
-        mState = TONE_STOPPING;
+    if (mState != TONE_IDLE && mState != TONE_INIT) {
+        if (mState == TONE_PLAYING || mState == TONE_STARTING || mState == TONE_RESTARTING) {
+            struct timespec stopTime;
+            // If the start time is valid, make sure that the number of audio samples produced
+            // corresponds at least to the time between the start and stop commands.
+            // This is needed in case of cold start of the output stream.
+            if ((mStartTime.tv_sec != 0) && (clock_gettime(CLOCK_MONOTONIC, &stopTime) == 0)) {
+                time_t sec = stopTime.tv_sec - mStartTime.tv_sec;
+                long nsec = stopTime.tv_nsec - mStartTime.tv_nsec;
+                long durationMs;
+                if (nsec < 0) {
+                    --sec;
+                    nsec += 1000000000;
+                }
+
+                if ((sec + 1) > ((long)(INT_MAX / mSamplingRate))) {
+                    mMaxSmp = sec * mSamplingRate;
+                } else {
+                    // mSamplingRate is always > 1000
+                    sec = sec * 1000 + nsec / 1000000; // duration in milliseconds
+                    mMaxSmp = (unsigned int)(((int64_t)sec * mSamplingRate) / 1000);
+                }
+                ALOGV("stopTone() forcing mMaxSmp to %d, total for far %d", mMaxSmp,  mTotalSmp);
+            } else {
+                mState = TONE_STOPPING;
+            }
+        }
         ALOGV("waiting cond");
         status_t lStatus = mWaitCbkCond.waitRelative(mLock, seconds(3));
         if (lStatus == NO_ERROR) {
+            // If the tone was restarted exit now before calling clearWaveGens();
+            if (mState != TONE_INIT) {
+                mLock.unlock();
+                return;
+            }
             ALOGV("track stop complete, time %d", (unsigned int)(systemTime()/1000000));
         } else {
             ALOGE("--- Stop timed out");
             mState = TONE_IDLE;
             mpAudioTrack->stop();
         }
+        clearWaveGens();
     }
-
-    clearWaveGens();
 
     mLock.unlock();
 }
@@ -1252,6 +1285,9 @@ audioCallback_EndLoop:
             ALOGV("Cbk restarting track");
             if (lpToneGen->prepareWave()) {
                 lpToneGen->mState = TONE_STARTING;
+                if (clock_gettime(CLOCK_MONOTONIC, &lpToneGen->mStartTime) != 0) {
+                    lpToneGen->mStartTime.tv_sec = 0;
+                }
                 // must reload lpToneDesc as prepareWave() may change mpToneDesc
                 lpToneDesc = lpToneGen->mpToneDesc;
             } else {
@@ -1293,7 +1329,7 @@ audioCallback_EndLoop:
         }
 
         if (lSignal)
-            lpToneGen->mWaitCbkCond.signal();
+            lpToneGen->mWaitCbkCond.broadcast();
         lpToneGen->mLock.unlock();
     }
 }