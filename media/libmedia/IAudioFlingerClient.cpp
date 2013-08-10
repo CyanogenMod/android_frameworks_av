085f16156a811098380974d8d767d0876ec71fda
Steve Kondik <shade@chemlab.org>
2013-08-09 20:20:32 -0700

    audio: Add A2DP notification support
     * Forward porting this code.
     * Sends notifications to legacy LPAPlayer for routing A2DP thru LPA.
    
    [mikeioannina]: Adjust for android 5.1
    
    Change-Id: I53eebda30b2e5cd60eeac5eb2a4cdb53e38381c9

--- media/libmedia/IAudioFlingerClient.cpp
+++ media/libmedia/IAudioFlingerClient.cpp
@@ -54,9 +54,10 @@ public:
             data.writeInt32(stream);
         } else if (event != AudioSystem::OUTPUT_CLOSED && event != AudioSystem::INPUT_CLOSED
 #ifdef QCOM_DIRECTTRACK
-                   && event != AudioSystem::EFFECT_CONFIG_CHANGED
+                && event != AudioSystem::A2DP_OUTPUT_STATE
+                && event != AudioSystem::EFFECT_CONFIG_CHANGED
 #endif
-            ) {
+        ) {
             const AudioSystem::OutputDescriptor *desc = (const AudioSystem::OutputDescriptor *)param2;
             data.writeInt32(desc->samplingRate);
             data.writeInt32(desc->format);
