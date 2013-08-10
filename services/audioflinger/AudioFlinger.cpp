05cdc9f0fe645962d8727cf3d94f4c9e7f36efb1
Steve Kondik <shade@chemlab.org>
2013-08-09 20:20:32 -0700

    audio: Add A2DP notification support
     * Forward porting this code.
     * Sends notifications to legacy LPAPlayer for routing A2DP thru LPA.
    
    [mikeioannina]: Adjust for android 4.4
    
    Change-Id: I53eebda30b2e5cd60eeac5eb2a4cdb53e38381c9

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -205,6 +205,9 @@ AudioFlinger::AudioFlinger()
 void AudioFlinger::onFirstRef()
 {
     int rc = 0;
+#ifdef QCOM_DIRECTTRACK
+    mA2DPHandle = -1;
+#endif
 
     Mutex::Autolock _l(mLock);
 
@@ -806,6 +809,9 @@ status_t AudioFlinger::setMasterVolume(float value)
         return PERMISSION_DENIED;
     }
 
+#ifdef QCOM_DIRECTTRACK
+    mA2DPHandle = -1;
+#endif
     Mutex::Autolock _l(mLock);
     mMasterVolume = value;
 
@@ -1361,6 +1367,13 @@ void AudioFlinger::registerClient(const sp<IAudioFlingerClient>& client)
             mRecordThreads.valueAt(i)->sendIoConfigEvent(AudioSystem::INPUT_OPENED);
         }
     }
+#ifdef QCOM_DIRECTTRACK
+    // Send the notification to the client only once.
+    if (mA2DPHandle != -1) {
+        ALOGV("A2DP active. Notifying the registered client");
+        client->ioConfigChanged(AudioSystem::A2DP_OUTPUT_STATE, mA2DPHandle, &mA2DPHandle);
+    }
+#endif
 }
 
 #ifdef QCOM_DIRECTTRACK
@@ -1933,6 +1946,15 @@ audio_io_handle_t AudioFlinger::openOutput(audio_module_handle_t module,
 #endif
         }
 
+#ifdef QCOM_DIRECTTRACK
+        // if the device is a A2DP, then this is an A2DP Output
+        if (audio_is_a2dp_device((audio_devices_t) *pDevices))
+        {
+            mA2DPHandle = id;
+            ALOGV("A2DP device activated. The handle is set to %d", mA2DPHandle);
+        }
+#endif
+
         if (pSamplingRate != NULL) {
             *pSamplingRate = config.sample_rate;
         }
@@ -2043,6 +2065,14 @@ status_t AudioFlinger::closeOutput_nonvirtual(audio_io_handle_t output)
 
 
         mPlaybackThreads.removeItem(output);
+#ifdef QCOM_DIRECTTRACK
+        if (mA2DPHandle == output)
+        {
+            mA2DPHandle = -1;
+            ALOGV("A2DP OutputClosed Notifying Client");
+            audioConfigChanged_l(AudioSystem::A2DP_OUTPUT_STATE, mA2DPHandle, &mA2DPHandle);
+        }
+#endif
         // save all effects to the default thread
         if (mPlaybackThreads.size()) {
             PlaybackThread *dstThread = checkPlaybackThread_l(mPlaybackThreads.keyAt(0));
@@ -2308,7 +2338,12 @@ status_t AudioFlinger::setStreamOutput(audio_stream_type_t stream, audio_io_hand
 #endif
             thread->invalidateTracks(stream);
     }
-
+#ifdef QCOM_DIRECTTRACK
+    if ( mA2DPHandle == output ) {
+        ALOGV("A2DP Activated and hence notifying the client");
+        audioConfigChanged_l(AudioSystem::A2DP_OUTPUT_STATE, mA2DPHandle, &output);
+    }
+#endif
     return NO_ERROR;
 }
 
--- services/audioflinger/AudioFlinger.h
+++ services/audioflinger/AudioFlinger.h
@@ -766,6 +766,7 @@ private:
                 bool                                mBtNrecIsOff;
 #ifdef QCOM_DIRECTTRACK
                 DefaultKeyedVector<audio_io_handle_t, AudioSessionDescriptor *> mDirectAudioTracks;
+                int                                 mA2DPHandle; // Handle to notify A2DP connection status
                 // protected by mLock
                 volatile bool                       mIsEffectConfigChanged;
 #endif
