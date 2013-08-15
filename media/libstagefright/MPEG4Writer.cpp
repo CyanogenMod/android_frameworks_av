b559509499eca01986cdd2e94d23ce7da2e9b107
Peter Karlsson <peter1.karlsson@sonymobile.com>
2013-08-15 16:11:22 +0200

    MPEG4Writer: fix mMoovBoxBuffer memory leak
    In some cirumstances the writing of the moov box
    leaks memory since mMoovBoxBuffer is not freed.
    
    Change-Id: I441360f6c7e84e31fea06067eca98b66b86b54bc
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>

--- media/libstagefright/MPEG4Writer.cpp
+++ media/libstagefright/MPEG4Writer.cpp
@@ -361,6 +361,7 @@ MPEG4Writer::MPEG4Writer(const char *filename)
       mWriterThreadStarted(false),
       mOffset(0),
       mMdatOffset(0),
+      mMoovBoxBuffer(NULL),
       mEstimatedMoovBoxSize(0),
       mInterleaveDurationUs(1000000),
       mLatitudex10000(0),
@@ -389,6 +390,7 @@ MPEG4Writer::MPEG4Writer(int fd)
       mWriterThreadStarted(false),
       mOffset(0),
       mMdatOffset(0),
+      mMoovBoxBuffer(NULL),
       mEstimatedMoovBoxSize(0),
       mInterleaveDurationUs(1000000),
       mLatitudex10000(0),
@@ -410,6 +412,8 @@ MPEG4Writer::~MPEG4Writer() {
         mTracks.erase(it);
     }
     mTracks.clear();
+    free(mMoovBoxBuffer);
+    mMoovBoxBuffer = NULL;
 }
 
 status_t MPEG4Writer::dump(
