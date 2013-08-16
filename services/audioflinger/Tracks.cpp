fb614e6f0a5ffd24c61e32a7905f247377c9427e
Shreyas Nagasandra Chandrasekhar <snagas@codeaurora.org>
2013-08-16 12:10:07 +0530

    audioflinger: Use channel count for buffer size calculation
    - For VOIP(hangout) usecase, when the ringtone is played,
      application sets flag as VOIP and the channel as stereo.
      But , on the server (audioflinger) side , the channel
      was always considered as MONO. This was leading to a
      corruption of the Cblk.
    - Added changes to consider channel count in allocating the
      buffer size.
    
    CRs-Fixed: 523984 522525
    
    Change-Id: I160d7cf7855161efa201a86846aec67cb24d35dd

--- services/audioflinger/Tracks.cpp
+++ services/audioflinger/Tracks.cpp
@@ -88,7 +88,7 @@ AudioFlinger::ThreadBase::TrackBase::TrackBase(
         mChannelCount(popcount(channelMask)),
 #ifdef QCOM_HARDWARE
         mFrameSize((audio_is_linear_pcm(format)||audio_is_supported_compressed(format)) ?
-                ((int16_t)flags == VOICE_COMMUNICATION_FLAG? sizeof(int16_t): mChannelCount * audio_bytes_per_sample(format)): sizeof(int8_t)),
+                ((int16_t)flags == VOICE_COMMUNICATION_FLAG? mChannelCount * sizeof(int16_t): mChannelCount * audio_bytes_per_sample(format)): sizeof(int8_t)),
 #else
         mFrameSize(audio_is_linear_pcm(format) ?
                 mChannelCount * audio_bytes_per_sample(format) : sizeof(int8_t)),
@@ -115,7 +115,7 @@ AudioFlinger::ThreadBase::TrackBase::TrackBase(
     uint8_t channelCount = popcount(channelMask);
     size_t bufferSize = 0;
     if ((int16_t)flags == VOICE_COMMUNICATION_FLAG) {
-        bufferSize = frameCount * mFrameSize;
+        bufferSize = channelCount * frameCount * mFrameSize;
     } else {
        if ( (format == AUDIO_FORMAT_PCM_16_BIT) ||
             (format == AUDIO_FORMAT_PCM_8_BIT))
