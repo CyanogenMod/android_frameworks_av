4c7bf3981fb511b50d7fa8776cd2dcd4fa671e82
Tom Briden <tom@decompile.me.uk>
2013-09-03 20:49:26 +0100

    Added ifndef ICS_AUDIO_BLOB around is_source_active (3/3)
    For older audio blobs where this function doesn't exist, receiving
    a notification crashes the audio service.
    
    Change-Id: I554f00ba7f171d2ea91627cae2281623383b3ca7

--- services/audioflinger/AudioPolicyService.cpp
+++ services/audioflinger/AudioPolicyService.cpp
@@ -501,6 +501,7 @@ bool AudioPolicyService::isStreamActiveRemotely(audio_stream_type_t stream, uint
 
 bool AudioPolicyService::isSourceActive(audio_source_t source) const
 {
+#ifndef ICS_AUDIO_BLOB
     if (mpAudioPolicy == NULL) {
         return false;
     }
@@ -509,6 +510,9 @@ bool AudioPolicyService::isSourceActive(audio_source_t source) const
     }
     Mutex::Autolock _l(mLock);
     return mpAudioPolicy->is_source_active(mpAudioPolicy, source);
+#else
+    return false;
+#endif
 }
 
 status_t AudioPolicyService::queryDefaultPreProcessing(int audioSession,
