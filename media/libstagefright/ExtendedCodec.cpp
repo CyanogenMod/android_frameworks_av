e7a2ae881707133e785a8b07a803d6cd1f4854e4
Surajit Podder <spodder@codeaurora.org>
2013-10-10 19:21:55 +0530

    libstagefright: Disable multi slice mode
    Disable multi slice mode video encoding based on platform.
    
    CRs-Fixed: 546591
    Change-Id: Ib986e418c68013ba9ea09ecd1c735396565c9f5b

--- media/libstagefright/ExtendedCodec.cpp
+++ media/libstagefright/ExtendedCodec.cpp
@@ -1071,6 +1071,16 @@ bool ExtendedCodec::useHWAACDecoder(const char *mime) {
     return false;
 }
 
+void ExtendedCodec::overrideErrorCorrectionParameters(
+         OMX_VIDEO_PARAM_ERRORCORRECTIONTYPE &errorCorrectionType) {
+    char mDeviceName[100];
+    property_get("ro.board.platform", mDeviceName, "0");
+    if (!strncmp(mDeviceName, "msm8610", 7)) {
+        errorCorrectionType.bEnableResync = OMX_FALSE;
+        errorCorrectionType.nResynchMarkerSpacing = 0;
+    }
+}
+
 } //namespace android
 
 #else //ENABLE_QC_AV_ENHANCEMENTS
@@ -1228,6 +1238,11 @@ namespace android {
             const sp<IOMX> &omx, IOMX::node_id nodeID, bool* isEnabled,
             const char* componentName) {
         *isEnabled = false;
+        return;
+    }
+
+    void ExtendedCodec::overrideErrorCorrectionParameters(
+             OMX_VIDEO_PARAM_ERRORCORRECTIONTYPE &errorCorrectionType) {
         return;
     }
 
