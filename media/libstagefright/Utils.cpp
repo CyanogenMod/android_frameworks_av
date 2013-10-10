79b95a7c1ed7bc87a029c544b3a0b5928ef9f033
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2013-10-10 15:42:11 +0530

    Revert "frameworks/av: AAC CSD support for MediaCodec path"
    This reverts commit 74a7b1cb44e10b9c213fb110264b6bab410d8fff.
    Currently AAC CSD is retrieved in MediaCodec using
    kKeyAacCodecSpecificData. This support was added as part of
    commit Id046b0d4bf96f029c1b6b5d78d753073e38ac841.
    
    Change-Id: I54bc6204c1fe88f4e4545f501e6bd1bb2a8beaf9
    CRs-Fixed: 527284

--- media/libstagefright/Utils.cpp
+++ media/libstagefright/Utils.cpp
@@ -29,10 +29,6 @@
 #include <media/stagefright/Utils.h>
 #include <media/stagefright/ExtendedCodec.h>
 
-#ifdef ENABLE_QC_AV_ENHANCEMENTS
-#include "QCMetaData.h"
-#endif
-
 namespace android {
 
 uint16_t U16_AT(const uint8_t *ptr) {
@@ -128,30 +124,6 @@ status_t convertMetaDataToMessage(
         if (meta->findInt32(kKeyIsADTS, &isADTS)) {
             msg->setInt32("is-adts", true);
         }
-
-#ifdef ENABLE_QC_AV_ENHANCEMENTS
-        uint32_t type;
-        const void *data;
-        size_t size;
-
-        if (meta->findData(kKeyAacCodecSpecificData, &type, &data, &size)) {
-            if (size > 0 && data != NULL) {
-                sp<ABuffer> buffer = new ABuffer(size);
-                if (buffer != NULL) {
-                    memcpy(buffer->data(), data, size);
-                    buffer->meta()->setInt32("csd", true);
-                    buffer->meta()->setInt64("timeUs", 0);
-                    msg->setBuffer("csd-0", buffer);
-                }
-                else {
-                    ALOGE("kKeyAacCodecSpecificData ABuffer Allocation failed");
-                }
-            }
-            else {
-                ALOGE("Not a valid data pointer or size == 0");
-            }
-       }
-#endif
     }
 
     int32_t maxInputSize;
