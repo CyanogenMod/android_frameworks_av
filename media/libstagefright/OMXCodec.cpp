edce9e95ef3e82cf264ab311e6e323bff99c1b4c
Maheshwar Ajja <majja@codeaurora.org>
2013-10-17 17:18:24 +0530

    libstagefright: fix thumbnail issue
    Set thumbnail mode before setting video resolution to
    video decoder to resolve thumbnails generation issue.
    
    CRs-fixed: 563422
    Change-Id: I42503676c2199bcb390b0b27652570d0f8188d13

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -797,6 +797,9 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
                 return err;
             }
         } else {
+            ExtendedCodec::configureVideoDecoder(
+                    meta, mMIME, mOMX, mFlags, mNode, mComponentName);
+
             status_t err = setVideoOutputFormat(
                     mMIME, meta);
 
@@ -804,8 +807,6 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
                 return err;
             }
 
-            ExtendedCodec::configureVideoDecoder(
-                    meta, mMIME, mOMX, mFlags, mNode, mComponentName);
             ExtendedCodec::configureFramePackingFormat(
                     meta, mOMX, mNode);
             ExtendedCodec::enableSmoothStreaming(
