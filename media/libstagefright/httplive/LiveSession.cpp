f6a4082897f4efb61a70d47d7cb185fc74fbe30a
Oscar Rydhé <oscar.rydhe@sonymobile.com>
2013-10-28 20:49:57 +0100

    HLS: getDuration should return -1 for live streams
    According to the MediaPlayer API documentation -1 should
    be returned if the duration is not available. But for HLS 0
    is returned. Change this.
    
    Change-Id: Ib7704a94ac8dc104ceed4da4491f0d03cb8f4560

--- media/libstagefright/httplive/LiveSession.cpp
+++ media/libstagefright/httplive/LiveSession.cpp
@@ -924,7 +924,7 @@ status_t LiveSession::onSeek(const sp<AMessage> &msg) {
 }
 
 status_t LiveSession::getDuration(int64_t *durationUs) const {
-    int64_t maxDurationUs = 0ll;
+    int64_t maxDurationUs = -1ll;
     for (size_t i = 0; i < mFetcherInfos.size(); ++i) {
         int64_t fetcherDurationUs = mFetcherInfos.valueAt(i).mDurationUs;
 
