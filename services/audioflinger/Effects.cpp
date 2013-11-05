06b808a2cb4448e445e51ab98af45619e9cb0712
Sharad Sangle <assangle@codeaurora.org>
2013-11-05 18:08:24 +0530

    audioflinger: SRS effects are not working for LPA
        -Issue: If SRS effects are applied on LPA playback,
         in ARM side there is a delay of 4-5 seconds for effects
         application
        -Root cause: In applyEffects SRS effects are applied first and
         then Android effects are applied.
         In case of active android effects, SRS effects are called
         first and then android effects are called, so buffer with
         SRS effects is getting overwritten with buffer with
         android effects.
         If there are no android effects applied then all
         the SRS effects are overwritten with decoded data.
         So until next write call, SRS effects are not applied and hence delay
         of 4-5 seconds
        -Fix: In applyEffects first apply android effects and then
         apply SRS effects
    
    Change-Id: I36b2133256d168f06b13c3d4a2bad00312d571ac
    CRs-Fixed: 566278

--- services/audioflinger/Effects.cpp
+++ services/audioflinger/Effects.cpp
@@ -1985,6 +1985,10 @@ bool AudioFlinger::applyEffectsOn(void *token, int16_t *inBuffer,
             memcpy(outBuffer, inBuffer, size);
         }
     }
+#ifdef SRS_PROCESSING
+   POSTPRO_PATCH_ICS_OUTPROC_DIRECT_SAMPLES(token, AUDIO_FORMAT_PCM_16_BIT, outBuffer, size, mLPASampleRate, mLPANumChannels);
+#endif
+
     return true;
 }
 
