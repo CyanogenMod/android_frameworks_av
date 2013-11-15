fe85627de175a5a0cf7cb2dd39e2f34ab0914458
Eric Laurent <elaurent@google.com>
2013-11-15 12:02:28 -0800

    audioflinger: do not use raw pointer for tracks
    Commit 9da3d95 surfaced a problem caused by the use of a raw
    pointer to a track in offload thread implementation.
    
    Pointers to tracks should always be weak or strong pointers.
    
    Bug: 11708529.
    Change-Id: Ic48632532d186c9be8261f73cefdf824b9fbbd2b

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -4096,8 +4096,7 @@ AudioFlinger::OffloadThread::OffloadThread(const sp<AudioFlinger>& audioFlinger,
     :   DirectOutputThread(audioFlinger, output, id, device, OFFLOAD),
         mHwPaused(false),
         mFlushPending(false),
-        mPausedBytesRemaining(0),
-        mPreviousTrack(NULL)
+        mPausedBytesRemaining(0)
 {
     //FIXME: mStandby should be set to true by ThreadBase constructor
     mStandby = true;
