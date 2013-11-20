075c4e2b37872eb70857737632202a190430b679
Franco Rapetti <frapeti@gmail.com>
2013-11-20 19:17:10 -0200

    SurfaceMediaSource.cpp: add camera support

--- media/libstagefright/SurfaceMediaSource.cpp
+++ media/libstagefright/SurfaceMediaSource.cpp
@@ -57,7 +57,11 @@ SurfaceMediaSource::SurfaceMediaSource(uint32_t bufferWidth, uint32_t bufferHeig
     mBufferQueue = new BufferQueue();
     mBufferQueue->setDefaultBufferSize(bufferWidth, bufferHeight);
     mBufferQueue->setConsumerUsageBits(GRALLOC_USAGE_HW_VIDEO_ENCODER |
+#ifdef STE_HARDWARE
+            GRALLOC_USAGE_HW_2D);
+#else
             GRALLOC_USAGE_HW_TEXTURE);
+#endif
 
     sp<ISurfaceComposer> composer(ComposerService::getComposerService());
 
