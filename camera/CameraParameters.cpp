d42843e539ddbcc020c95230dc0dd239f67b722e
Robert Rozic <rooobertinhooo@gmail.com>
2013-11-23 13:56:11 +0100

    Update CameraParameters.cpp

--- camera/CameraParameters.cpp
+++ camera/CameraParameters.cpp
@@ -364,16 +364,14 @@ const char CameraParameters::PIXEL_FORMAT_YUV420SP_ADRENO[] = "yuv420sp-adreno";
 #endif
 #ifdef STE_HARDWARE
 const char CameraParameters::PIXEL_FORMAT_YUV420SPNV12[] = "yuv420spnv12";
-#endif
-const char CameraParameters::PIXEL_FORMAT_YUV422I[] = "yuv422i-yuyv";
-const char CameraParameters::PIXEL_FORMAT_YUV420P[]  = "yuv420p";
-#ifdef STE_HARDWARE
 const char CameraParameters::PIXEL_FORMAT_YUV420MB[] = "yuv420mb";
 const char CameraParameters::PIXEL_FORMAT_YVU422SP[] = "yvu422sp";
 const char CameraParameters::PIXEL_FORMAT_YVU422P[] = "yvu422p";
 const char CameraParameters::PIXEL_FORMAT_YVU420SP[] = "yvu420sp";
 const char CameraParameters::PIXEL_FORMAT_YVU420P[]  = "yvu420p";
 #endif
+const char CameraParameters::PIXEL_FORMAT_YUV422I[] = "yuv422i-yuyv";
+const char CameraParameters::PIXEL_FORMAT_YUV420P[]  = "yuv420p";
 const char CameraParameters::PIXEL_FORMAT_RGB565[] = "rgb565";
 const char CameraParameters::PIXEL_FORMAT_RGBA8888[] = "rgba8888";
 const char CameraParameters::PIXEL_FORMAT_JPEG[] = "jpeg";
@@ -549,15 +547,16 @@ void CameraParameters::setOrientation(int orientation)
 }
 #endif
 
+
+// Values for light fx settings
+const char CameraParameters::LIGHTFX_LOWLIGHT[] = "low-light";
+const char CameraParameters::LIGHTFX_HDR[] = "high-dynamic-range";
+
 #ifdef STE_HARDWARE
 // keys for record stride and sliceheight
 const char CameraParameters::KEY_RECORD_STRIDE[] = "record-stride";
 const char CameraParameters::KEY_RECORD_SLICE_HEIGHT[] = "record-slice-height";
 #endif
-
-// Values for light fx settings
-const char CameraParameters::LIGHTFX_LOWLIGHT[] = "low-light";
-const char CameraParameters::LIGHTFX_HDR[] = "high-dynamic-range";
 
 CameraParameters::CameraParameters()
                 : mMap()
