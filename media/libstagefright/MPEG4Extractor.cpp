54e34db0921989b5089b1bc375572b69a22f33fd
Manikanta Kanamarlapudi <kmanikan@codeaurora.org>
2013-11-28 11:55:29 +0530

    libstagefright: Added mime-types for enca/encv atoms
    - enca/encv are encrypted audio and video atoms for
      mp4a and mp4v. Hence returning aac and mpeg4 mime-types
      for enca and encv.
    
    CRs-Fixed: 578506
    
    Change-Id: Ie568ea7eec73e110eef07e2b1b921afefc8c54a7

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -333,6 +333,9 @@ static const char *FourCC2MIME(uint32_t fourcc) {
         case FOURCC('m', 'p', '4', 'a'):
             return MEDIA_MIMETYPE_AUDIO_AAC;
 
+        case FOURCC('e', 'n', 'c', 'a'):
+            return MEDIA_MIMETYPE_AUDIO_AAC;
+
         case FOURCC('.', 'm', 'p', '3'):
             return MEDIA_MIMETYPE_AUDIO_MPEG;
 
@@ -343,6 +346,9 @@ static const char *FourCC2MIME(uint32_t fourcc) {
             return MEDIA_MIMETYPE_AUDIO_AMR_WB;
 
         case FOURCC('m', 'p', '4', 'v'):
+            return MEDIA_MIMETYPE_VIDEO_MPEG4;
+
+        case FOURCC('e', 'n', 'c', 'v'):
             return MEDIA_MIMETYPE_VIDEO_MPEG4;
 
         case FOURCC('s', '2', '6', '3'):
