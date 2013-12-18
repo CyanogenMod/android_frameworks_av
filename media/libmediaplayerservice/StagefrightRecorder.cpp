892421a4fb991ea516ff5e62495ccf32714d4bd4
Pavan Chikkala <pavanc@codeaurora.org>
2013-12-18 15:06:43 +0530

    audio: Check the return code of MediaWriter::start
    - Return code of MediaWriter::start method is not checked
      in startRawAudioRecording. Due to this, sound recorder
      application is not receiving the error whenever there is
      an error in starting of the recording
    - Fix is to check the error and return it back to
      the client accordingly
    
    Change-Id: I15916211da7a2eb0f27fbf9d4151410a8bdfdd42
    CRs-Fixed: 591788

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1139,9 +1139,8 @@ status_t StagefrightRecorder::startRawAudioRecording() {
         mWriter->setMaxFileSize(mMaxFileSizeBytes);
     }
     mWriter->setListener(mListener);
-    mWriter->start();
-
-    return OK;
+    status = mWriter->start();
+    return status;
 }
 
 status_t StagefrightRecorder::startRTPRecording() {
