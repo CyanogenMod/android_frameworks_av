938d59763ff7c6838df3043d30c3af39301dd03e
Surajit Podder <spodder@codeaurora.org>
2014-01-27 18:15:27 +0530

    libstagefright: Fix crash during seek
    OMXCodec state is changed to EXECUTING when flush
    is complete. Update assert condition to check
    EXECUTING state instead of FLUSHING.
    
    CRs-Fixed: 607762
    Change-Id: I12b3eedfc83e00578d70af3ef903c5a26ccde3eb

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -4564,8 +4564,7 @@ status_t OMXCodec::read(
 
 #ifdef QCOM_HARDWARE
     if (seeking) {
-        CHECK_EQ((int)mState, (int)FLUSHING);
-        setState(EXECUTING);
+        CHECK_EQ((int)mState, (int)EXECUTING);
     }
 #endif
 
