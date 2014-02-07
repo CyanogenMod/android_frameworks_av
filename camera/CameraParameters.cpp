e6800880e8ea92da9321828707933e5028a99c7d
Tom Marshall <tdm@cyngn.com>
2014-02-07 11:50:59 -0800

    camera: Add required symbols for Oppo devices
    Change-Id: Iaf43f53130963449f4ec6d3116c58965ead1f475

--- camera/CameraParameters.cpp
+++ camera/CameraParameters.cpp
@@ -270,6 +270,9 @@ const char CameraParameters::WHITE_BALANCE_DAYLIGHT[] = "daylight";
 const char CameraParameters::WHITE_BALANCE_CLOUDY_DAYLIGHT[] = "cloudy-daylight";
 const char CameraParameters::WHITE_BALANCE_TWILIGHT[] = "twilight";
 const char CameraParameters::WHITE_BALANCE_SHADE[] = "shade";
+#ifdef OPPO_CAMERA_HARDWARE
+const char CameraParameters::WHITE_BALANCE_MANUAL_CCT[] = "manual-cct";
+#endif
 
 // Values for effect settings.
 const char CameraParameters::EFFECT_NONE[] = "none";
@@ -378,6 +381,9 @@ const char CameraParameters::FOCUS_MODE_FIXED[] = "fixed";
 const char CameraParameters::FOCUS_MODE_EDOF[] = "edof";
 const char CameraParameters::FOCUS_MODE_CONTINUOUS_VIDEO[] = "continuous-video";
 const char CameraParameters::FOCUS_MODE_CONTINUOUS_PICTURE[] = "continuous-picture";
+#ifdef OPPO_CAMERA_HARDWARE
+const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual";
+#endif
 #if defined(QCOM_HARDWARE)
 #ifdef QCOM_LEGACY_CAM_PARAMS
 const char CameraParameters::FOCUS_MODE_CONTINUOUS_CAMERA[] = "continuous-camera";
