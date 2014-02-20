e0316d77fa34df207d41f47853cdb71da68efdd8
Surajit Podder <spodder@codeaurora.org>
2014-02-20 16:22:41 +0530

    libstagefright: Render video frames on vsync
    Render video frames on vsync, which helps to
    synchronize display and decoder activity, and
    reduces power consumption
    
    CRs-Fixed: 649540
    Change-Id: I0981d7723621de963d22f7fd7ca0eed48f016090

--- media/libstagefright/include/ExtendedUtils.h
+++ media/libstagefright/include/ExtendedUtils.h
@@ -40,6 +40,9 @@
 #include <OMX_Video.h>
 #include <android/native_window.h>
 
+#include <gui/DisplayEventReceiver.h>
+#include <utils/Looper.h>
+
 #define MIN_BITERATE_AAC 24000
 #define MAX_BITERATE_AAC 320000
 
@@ -152,6 +155,47 @@ struct ExtendedUtils {
     static void createSecurePool();
 
     static void drainSecurePool();
+};
+
+class VSyncLocker : public RefBase {
+private:
+    //Number of frames profiled for calculating fps
+    static const int kMaxProfileCount = 60;
+
+    enum SyncState {
+        PROFILE_FPS,
+        ENABLE_SYNC,
+        BLOCK_SYNC,
+    };
+
+    volatile bool mExitVsyncEvent;
+    Looper *mLooper;
+    SyncState mSyncState;
+    int64_t mStartTime;
+    int mProfileCount;
+
+    pthread_t mThread;
+    Mutex mVsyncLock;
+    Condition mVSyncCondition;
+    DisplayEventReceiver mDisplayEventReceiver;
+
+    virtual void updateSyncState();
+    virtual void waitOnVSync();
+
+public:
+    static bool isSyncRenderEnabled();
+    static int receiver(int fd, int events, void *context);
+    static void *ThreadWrapper(void *context);
+
+    virtual void resetProfile();
+    virtual void blockSync();
+    virtual void blockOnVSync();
+    virtual void start();
+    void VSyncEvent();
+    void signalVSync();
+
+    virtual ~VSyncLocker();
+    VSyncLocker();
 };
 
 }
