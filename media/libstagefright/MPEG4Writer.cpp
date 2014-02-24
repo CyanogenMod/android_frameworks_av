cb542dbf394e147d85fc1f108912f74d1f0adc09
quic_mqiu <quic_mqiu@codeaurora.org>
2014-02-24 10:59:59 +0800

    Revert "Audio: Add Pause/Resume in AMR-WB tunnel recording"
    This reverts commit e51f3b1dd8b8b2668af3c467a330b18714d29966.
    
    There's no mSource->stop() in MPEG4Writer::Track::pause(),
    so no need to add this limitation here.
    
    CRs-Fixed: 620437
    
    Change-Id: I7b7134e2f58083b7dd9cfb3c0943d703ba228ecb

--- media/libstagefright/MPEG4Writer.cpp
+++ media/libstagefright/MPEG4Writer.cpp
@@ -1783,17 +1783,15 @@ status_t MPEG4Writer::Track::stop() {
 
     status_t err = (status_t) dummy;
 
-    if (!mPaused) {
-        ALOGD("Stopping %s track source", mIsAudio? "Audio": "Video");
-        {
-            status_t status = mSource->stop();
-            if (err == OK && status != OK && status != ERROR_END_OF_STREAM) {
-                err = status;
-            }
+    ALOGD("Stopping %s track source", mIsAudio? "Audio": "Video");
+    {
+        status_t status = mSource->stop();
+        if (err == OK && status != OK && status != ERROR_END_OF_STREAM) {
+            err = status;
         }
-
-        ALOGD("%s track stopped", mIsAudio? "Audio": "Video");
     }
+
+    ALOGD("%s track stopped", mIsAudio? "Audio": "Video");
     return err;
 }
 
