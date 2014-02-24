8657af8955e882bf45e9273cadaaded3f4ed7250
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2014-02-24 15:12:27 +0530

    frameworks/av: Enhance logging for audio usecases
    Add/Enable msg logging
    -Start and End of playback
    -To print file name
    -Mode of playback and component used
    
    Change-Id: I2c92d535ab76c9078af39b50fcfde4613f50a581

--- media/libstagefright/AudioPlayer.cpp
+++ media/libstagefright/AudioPlayer.cpp
@@ -96,6 +96,7 @@ status_t AudioPlayer::start(bool sourceAlreadyStarted) {
             return err;
         }
     }
+    ALOGD("start of Playback, useOffload %d",useOffload());
 
     // We allow an optional INFO_FORMAT_CHANGED at the very beginning
     // of playback, if there is one, getFormat below will retrieve the
@@ -311,11 +312,13 @@ void AudioPlayer::pause(bool playPendingSamples) {
             mSourcePaused = true;
         }
     }
+    ALOGD("Pause Playback at %lld",getMediaTimeUs());
 }
 
 status_t AudioPlayer::resume() {
     CHECK(mStarted);
     CHECK(mSource != NULL);
+    ALOGD("Resume Playback at %lld",getMediaTimeUs());
     if (mSourcePaused == true) {
         mSourcePaused = false;
         mSource->start();
@@ -338,7 +341,7 @@ status_t AudioPlayer::resume() {
 void AudioPlayer::reset() {
     CHECK(mStarted);
 
-    ALOGV("reset: mPlaying=%d mReachedEOS=%d useOffload=%d",
+    ALOGD("reset: mPlaying=%d mReachedEOS=%d useOffload=%d",
                                 mPlaying, mReachedEOS, useOffload() );
 
     if (mAudioSink.get() != NULL) {
