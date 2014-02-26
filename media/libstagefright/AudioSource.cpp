8ffa6cd52b04a3d85963941c49168ef2812b76f4
Divya Narayanan Poojary <dnaray@codeaurora.org>
2014-02-26 18:25:27 +0530

    audio: calculate mute duration using getOutputLatency
    -AudioSource mutes for predefined duration
     (kAutoRampStartUs)  at the beginning of a recording.
     Since kAutoRampStartUs is zero, video recording tone
     is also getting recorded in the beggining.
    
    -Fix is to get the output latency of current playback
     stream from audio system to calculate mute duration.
    
    Change-Id: Ib9f4d38fcc8478d148719b70358644c581ba2f25
    CRs-fixed: 615511

--- media/libstagefright/AudioSource.cpp
+++ media/libstagefright/AudioSource.cpp
@@ -88,6 +88,15 @@ AudioSource::AudioSource(
                     this,
                     frameCount /*notificationFrames*/);
         mInitCheck = mRecord->initCheck();
+        mAutoRampStartUs = kAutoRampStartUs;
+        uint32_t playbackLatencyMs = 0;
+        if (AudioSystem::getOutputLatency(&playbackLatencyMs,
+                                          AUDIO_STREAM_DEFAULT) == OK) {
+            if (2*playbackLatencyMs*1000LL > kAutoRampStartUs) {
+                mAutoRampStartUs = 2*playbackLatencyMs*1000LL;
+            }
+        }
+        ALOGD("Start autoramp from %lld", mAutoRampStartUs);
     } else {
         mInitCheck = status;
     }
@@ -117,6 +126,7 @@ AudioSource::AudioSource( audio_source_t inputSource, const sp<MetaData>& meta )
     } else {
         CHECK(0);
     }
+    mAutoRampStartUs = 0;
     CHECK(channels == 1 || channels == 2);
 
     mRecord = new AudioRecord(
@@ -289,14 +299,14 @@ status_t AudioSource::read(
     CHECK(buffer->meta_data()->findInt64(kKeyTime, &timeUs));
     int64_t elapsedTimeUs = timeUs - mStartTimeUs;
     if ( mFormat == AUDIO_FORMAT_PCM_16_BIT ) {
-        if (elapsedTimeUs < kAutoRampStartUs) {
+        if (elapsedTimeUs < mAutoRampStartUs) {
             memset((uint8_t *) buffer->data(), 0, buffer->range_length());
-        } else if (elapsedTimeUs < kAutoRampStartUs + kAutoRampDurationUs) {
+        } else if (elapsedTimeUs < mAutoRampStartUs + kAutoRampDurationUs) {
             int32_t autoRampDurationFrames =
-                        ((int64_t)kAutoRampDurationUs * mSampleRate + 500000LL) / 1000000LL; //Need type casting
+                    ((int64_t)kAutoRampDurationUs * mSampleRate + 500000LL) / 1000000LL;
 
             int32_t autoRampStartFrames =
-                        ((int64_t)kAutoRampStartUs * mSampleRate + 500000LL) / 1000000LL; //Need type casting
+                    ((int64_t)kAutoRampStartUs * mSampleRate + 500000LL) / 1000000LL;
 
             int32_t nFrames = mNumFramesReceived - autoRampStartFrames;
             rampVolume(nFrames, autoRampDurationFrames,
