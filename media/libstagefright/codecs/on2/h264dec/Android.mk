db46c2f8ff4b305a217adbbd406a61d10ae71db7
Ashok Bhat <ashok.bhat@arm.com>
2014-02-28 20:12:05 +0000

    Check for ARCH_ARM_HAVE_NEON flag only for ARM arch
    This patch prevents ARM neon code from being included
    for Arm64, where ARM is the 2nd architecture.
    
    Change-Id: I5a283991034f2650a0e7d4227aef967be276fc9c
    Signed-off-by: Ashok Bhat <ashok.bhat@arm.com>

--- media/libstagefright/codecs/on2/h264dec/Android.mk
+++ media/libstagefright/codecs/on2/h264dec/Android.mk
@@ -84,8 +84,8 @@ MY_OMXDL_ASM_SRC := \
 	./omxdl/arm_neon/vc/m4p10/src_gcc/omxVCM4P10_DequantTransformResidualFromPairAndAdd_s.S \
 	./omxdl/arm_neon/vc/m4p10/src_gcc/omxVCM4P10_TransformDequantChromaDCFromPair_s.S \
 
-
-ifeq ($(ARCH_ARM_HAVE_NEON),true)
+ifeq ($(TARGET_ARCH),arm)
+  ifeq ($(ARCH_ARM_HAVE_NEON),true)
     LOCAL_ARM_NEON   := true
 #    LOCAL_CFLAGS     := -std=c99 -D._NEON -D._OMXDL
     LOCAL_CFLAGS     := -DH264DEC_NEON -DH264DEC_OMXDL
@@ -94,6 +94,7 @@ ifeq ($(ARCH_ARM_HAVE_NEON),true)
     LOCAL_C_INCLUDES += $(LOCAL_PATH)/./omxdl/arm_neon/api \
                         $(LOCAL_PATH)/./omxdl/arm_neon/vc/api \
                         $(LOCAL_PATH)/./omxdl/arm_neon/vc/m4p10/api
+  endif
 endif
 
 LOCAL_SHARED_LIBRARIES := \
