519c0ca354f1f9dbcb012c8457c390286f480064
Leena Winterrowd <lenhardw@codeaurora.org>
2014-03-04 16:04:05 -0800

    stagefright: Omit audio from timelapse recording use-case
    Timelapse recording is only valid for video encoding, but audio
    is subjected to the same infinite wait. This can result in an ANR
    if encoded data is not being produced. Only wait forever in video
    use cases.
    
    Change-Id: I37d34c2235d110dc8954b66d830891fdccc50c0c

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -3468,7 +3468,7 @@ void OMXCodec::setState(State newState) {
 
 status_t OMXCodec::waitForBufferFilled_l() {
 
-    if (mIsEncoder) {
+    if (mIsEncoder && mIsVideo) {
         // For timelapse video recording, the timelapse video recording may
         // not send an input frame for a _long_ time. Do not use timeout
         // for video encoding.
