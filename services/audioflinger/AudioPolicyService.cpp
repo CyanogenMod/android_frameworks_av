8ef2b5b30be4ca45cc368ffc430e406fec6da480
Jesper Tragardh <jesper.tragardh@sonymobile.com>
2014-03-24 14:12:43 +0100

    Fix memory leak when filtering commands in insertCommand_l()
    When AudioPolicyService::AudioCommandThread::insertCommand_l filters
    a command from the queue, the AudioCommand* is removed from the
    vector mAudioCommands by calling mAudioCommands.removeAt(). This will
    destroy the pointer and compact the vector, but not delete the
    AudioCommand object.
    
    This patch adds code to delete a filtered out AudioCommand and its
    data object mParam. However, mParam was void* and deleting a void
    pointer is undefined behavior according to the C++
    standard. Therefore, the data classes are modified to inherit
    from an abstract base class with a virtual destructor.
    
    The deletion of non-filtered commands in
    AudioPolicyService::AudioCommandThread::threadLoop() was already
    correct, as each case casts mParam to the proper type before
    deleting.
    
    This also fixes AOSP issue 36370.
    
    Change-Id: I2b63838aff3608132aa496f964a929f78c4b6267

--- services/audioflinger/AudioPolicyService.cpp
+++ services/audioflinger/AudioPolicyService.cpp
@@ -706,6 +706,10 @@ AudioPolicyService::AudioCommandThread::~AudioCommandThread()
     if (!mAudioCommands.isEmpty()) {
         release_wake_lock(mName.string());
     }
+    for (size_t k=0; k < mAudioCommands.size(); k++) {
+        delete mAudioCommands[k]->mParam;
+        delete mAudioCommands[k];
+    }
     mAudioCommands.clear();
     delete mpToneGenerator;
 }
@@ -877,7 +881,7 @@ void AudioPolicyService::AudioCommandThread::startToneCommand(ToneGenerator::ton
     ToneData *data = new ToneData();
     data->mType = type;
     data->mStream = stream;
-    command->mParam = (void *)data;
+    command->mParam = data;
     Mutex::Autolock _l(mLock);
     insertCommand_l(command);
     ALOGV("AudioCommandThread() adding tone start type %d, stream %d", type, stream);
@@ -978,7 +982,7 @@ void AudioPolicyService::AudioCommandThread::stopOutputCommand(audio_io_handle_t
     data->mIO = output;
     data->mStream = stream;
     data->mSession = session;
-    command->mParam = (void *)data;
+    command->mParam = data;
     Mutex::Autolock _l(mLock);
     insertCommand_l(command);
     ALOGV("AudioCommandThread() adding stop output %d", output);
@@ -991,7 +995,7 @@ void AudioPolicyService::AudioCommandThread::releaseOutputCommand(audio_io_handl
     command->mCommand = RELEASE_OUTPUT;
     ReleaseOutputData *data = new ReleaseOutputData();
     data->mIO = output;
-    command->mParam = (void *)data;
+    command->mParam = data;
     Mutex::Autolock _l(mLock);
     insertCommand_l(command);
     ALOGV("AudioCommandThread() adding release output %d", output);
@@ -1082,6 +1086,10 @@ void AudioPolicyService::AudioCommandThread::insertCommand_l(AudioCommand *comma
         for (size_t k = i + 1; k < mAudioCommands.size(); k++) {
             if (mAudioCommands[k] == removedCommands[j]) {
                 ALOGV("suppressing command: %d", mAudioCommands[k]->mCommand);
+                // for commands that are not filtered,
+                // command->mParam is deleted in threadLoop
+                delete mAudioCommands[k]->mParam;
+                delete mAudioCommands[k];
                 mAudioCommands.removeAt(k);
                 break;
             }
