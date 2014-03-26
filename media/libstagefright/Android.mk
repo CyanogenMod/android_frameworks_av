c02dbb42a722083f981c1d92719e0b0937e4843f
Satya Krishna Pindiproli <satyak@codeaurora.org>
2014-03-26 14:48:24 +0530

    libstagefright: Modify the path of FLAC headers.
    The location of the headers of the FLAC CSIM library has changed.
    Modify the include path to reflect the right location.
    
    Change-Id: I13630a089716d8c1b35bfc5451d83ba3f3ade6a7

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -107,7 +107,7 @@ LOCAL_SHARED_LIBRARIES := \
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true)
 LOCAL_SRC_FILES += FLACDecoder.cpp
-LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio-noship/audio-flac
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audio-flac
 LOCAL_CFLAGS := -DQTI_FLAC_DECODER
 endif
 endif
