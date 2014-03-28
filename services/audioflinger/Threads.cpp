f5e3a506c2184f16bc852b33cf7abc57defbdb94
wjiang <wjiang@codeaurora.org>
2014-03-28 11:55:22 +0800

    audioflinger: forward device change to effects in direct thread
    Device change during offload playback is not informed to effect chain
    which causes effects still work on unexpected output devices. Add device
    change notification in direct output thread.
    
    CRs-Fixed: 630408
    
    Change-Id: I094a99bdf540479cee2fca6614ec35c2fa7d6046

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -3914,6 +3914,16 @@ bool AudioFlinger::DirectOutputThread::checkForNewParameters_l()
         POSTPRO_PATCH_OUTPROC_PLAY_ROUTE(this, param, value);
 #endif
 
+        if (param.getInt(String8(AudioParameter::keyRouting), value) == NO_ERROR) {
+            // forward device change to effects that have requested to be
+            // aware of attached audio device.
+            if (value != AUDIO_DEVICE_NONE) {
+                mOutDevice = value;
+                for (size_t i = 0; i < mEffectChains.size(); i++) {
+                    mEffectChains[i]->setDevice_l(mOutDevice);
+                }
+            }
+        }
         if (param.getInt(String8(AudioParameter::keyFrameCount), value) == NO_ERROR) {
             // do not accept frame count changes if tracks are open as the track buffer
             // size depends on frame count and correct behavior would not be garantied
