6c96db46b21f4874ea4473946d72268a5e847e1e
ApurupaPattapu <apurupa@codeaurora.org>
2014-03-27 17:31:23 -0700

    libstagefright: Check for data partitioned clips
    - Check for DP clips in ESDS atom of mpeg4 clips
    - If the clip is detected to have data partition, return a
      error, so that video decode will fallback to SW decoder
    
    CRs-Fixed: 630669
    Change-Id: I21800eabf2a68546b6ad389d18adbb3e9fb08ed7

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -645,6 +645,17 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
 #ifdef QCOM_HARDWARE
             }
 #endif
+#ifdef ENABLE_AV_ENHANCEMENTS
+            ALOGV("OMXCodec::configureCodec check for DP in ESDS atom");
+            if (!strncmp(mComponentName, "OMX.qcom.video.decoder.mpeg4",
+                         sizeof("OMX.qcom.video.decoder.mpeg4"))) {
+                bool isDP = ExtendedCodec::checkDPFromCodecSpecificData((const uint8_t*)data, size);
+                if (isDP) {
+                    ALOGE("H/W Decode Error: Data Partitioned bit set in the Header");
+                    return BAD_VALUE;
+                }
+            }
+#endif
         } else if (meta->findData(kKeyAVCC, &type, &data, &size)) {
             // Parse the AVCDecoderConfigurationRecord
 
