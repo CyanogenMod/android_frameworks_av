51a7e4031d2bcd5989a52f64358fe849d730c473
Jia Meng <jmeng@codeaurora.org>
2014-04-04 12:06:56 +0800

    stagefright: Cancel audioCheck event when setting nativewindow
    In html5 playback, setNativeWindow_l() will be called twice
    when switching from landscape to portrait mode. If the 2nd
    setNativeWindow_l gets the LOCK before proceeding mCheckAudioStatusEvent
    posted in the 1st setNativeWindow_l, it will get the infomation
    that seeking is completed and won't do seeking in this run.
    
    CRs-fixed: 624469
    Change-Id: Ia5c4e373e8a71b907b15b913085600d9481ef982

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1577,6 +1577,9 @@ void AwesomePlayer::shutdownVideoDecoder_l() {
 status_t AwesomePlayer::setNativeWindow_l(const sp<ANativeWindow> &native) {
     mNativeWindow = native;
 
+    mQueue.cancelEvent(mCheckAudioStatusEvent->eventID());
+    mAudioStatusEventPending = false;
+
     if (mVideoSource == NULL) {
         return OK;
     }
@@ -1598,6 +1601,7 @@ status_t AwesomePlayer::setNativeWindow_l(const sp<ANativeWindow> &native) {
     }
 
     if (mLastVideoTimeUs >= 0) {
+        mWatchForAudioSeekComplete = false;
         mSeeking = SEEK;
         mSeekTimeUs = mLastVideoTimeUs;
         modifyFlags((AT_EOS | AUDIO_AT_EOS | VIDEO_AT_EOS), CLEAR);
