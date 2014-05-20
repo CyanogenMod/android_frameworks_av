14ead787204a1bfc13e92067691efaefd624fcae
Andy Hung <hunga@google.com>
2014-05-19 21:36:20 -0700

    Fix OGG recognition for 64 bit
    Change-Id: I4cef86ca05e06f508929d109a52e375169c33bf9

--- media/libmediaplayerservice/MediaPlayerFactory.cpp
+++ media/libmediaplayerservice/MediaPlayerFactory.cpp
@@ -186,7 +186,7 @@ class StagefrightPlayerFactory :
         read(fd, buf, sizeof(buf));
         lseek(fd, offset, SEEK_SET);
 
-        long ident = *((long*)buf);
+        uint32_t ident = *((uint32_t*)buf);
 
         // Ogg vorbis?
         if (ident == 0x5367674f) // 'OggS'
