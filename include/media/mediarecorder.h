119686d196033246ed8ebcd453be3860377a41df
Praveen Chavan <pchavan@codeaurora.org>
2014-05-29 02:38:43 -0700

    MediaProfiles: Enhance profiles for extended usecases
    Add more supported video resolutions for camcorder
    and timelapse recording.
    
    Augment encoder profiles with HFR parameters.
    
    Change-Id: I887d6259db0a97b3a705813dc754b4b0faff47fc

--- include/media/mediarecorder.h
+++ include/media/mediarecorder.h
@@ -1,4 +1,7 @@
 /*
+ ** Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ ** Not a Contribution.
+ **
  ** Copyright (C) 2008 The Android Open Source Project
  **
  ** Licensed under the Apache License, Version 2.0 (the "License");
