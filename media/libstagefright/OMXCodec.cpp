3f1c150a60b7f87993a07277cea2a72e23258ff5
ZoliN <zltnngy@gmail.com>
2014-06-01 22:16:02 +0200

    libstagefright: Fix video encoder input buffer
    Ported from cm10, needed to avoid the encoder eating up all input buffers,
    which leaves no buffer left for the camera sometimes
    
    Change-Id: I0432cd87a9defd08725ac8831d7376edb98d7e18

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -3539,6 +3539,12 @@ void OMXCodec::drainInputBuffers() {
                 continue;
             }
 
+#ifdef SEMC_ICS_CAMERA_BLOB
+            if (mIsEncoder && mIsVideo && (i == 4)) {
+                break;
+            }
+#endif
+
             if (!drainInputBuffer(info)) {
                 break;
             }
