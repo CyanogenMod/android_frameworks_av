168a6dad0a50a2a9882c0acd08c295dc702b960c
timduru <timduru@gmail.com>
2014-06-01 23:12:45 +0200

    tf101: Skip NV OMX Plugin crash on Destroy
    Fixes Netflix crash on opening
    not deleting is better than crashing for the moment.
    
    Board flag is
    
    BOARD_USES_PROPRIETARY_OMX := TF101
    
    Patchset 3: Fix derped ifdef
    Patchset 4: Clean tims code...
    Patchset 6: Keep as 0
    Patchset 7: Cleanup
    Change-Id: If3466d37a6483523f3d740734647fd5ec2d4f3da

--- media/libstagefright/omx/OMXMaster.cpp
+++ media/libstagefright/omx/OMXMaster.cpp
@@ -48,6 +48,10 @@ void OMXMaster::addVendorPlugin() {
 #ifdef SAMSUNG_OMX
     addPlugin("libsomxcore.so");
 #endif
+
+#ifdef TF101_OMX
+    addPlugin("libnvomx.so");
+#endif
 }
 
 void OMXMaster::addPlugin(const char *libname) {
@@ -86,6 +90,7 @@ void OMXMaster::addPlugin(OMXPluginBase *plugin) {
                     name, sizeof(name), index++)) == OMX_ErrorNone) {
         String8 name8(name);
 
+        //ALOGV("plugin %d:%s ", plugin, name8.string());
         if (mPluginByComponentName.indexOfKey(name8) >= 0) {
             ALOGE("A component of name '%s' already exists, ignoring this one.",
                  name8.string());
@@ -112,13 +117,28 @@ void OMXMaster::clearPlugins() {
 
     mPluginByComponentName.clear();
 
+#ifdef TF101_OMX
+    int plugin = 1;
+#endif
     for (List<OMXPluginBase *>::iterator it = mPlugins.begin();
             it != mPlugins.end(); ++it) {
         if (destroyOMXPlugin)
             destroyOMXPlugin(*it);
+#ifdef TF101_OMX
+        else {
+           ALOGV("plugin %d", *it);
+           if( *it != NULL && plugin != 1) // 1st plugin is NV, crashes on delete => skip, better than crash for the moment
+	      delete *it;
+        }
+
+        *it = NULL;
+    plugin++;
+#else
         else
             delete *it;
+
         *it = NULL;
+#endif
     }
 
     mPlugins.clear();
