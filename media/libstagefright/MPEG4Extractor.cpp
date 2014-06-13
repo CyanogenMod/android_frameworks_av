18f6575b3326cf3f658152584ae8b1b335a6e9ee
Marco Nelissen <marcone@google.com>
2014-06-13 14:13:44 -0700

    Fail more gracefully on allocation failure
    Check allocations when the size is read from a file and might therefore
    be invalid.
    
    b/14388161
    
    Change-Id: Ia08cc0a6107f275a70e793ef3b50c0ce16ceeee0

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -1912,8 +1912,7 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
             if ((chunk_size > SIZE_MAX) || (SIZE_MAX - chunk_size <= size)) {
                 return ERROR_MALFORMED;
             }
-
-            uint8_t *buffer = new uint8_t[size + chunk_size];
+            uint8_t *buffer = new (std::nothrow) uint8_t[size + chunk_size];
             if (buffer == NULL) {
                 return ERROR_MALFORMED;
             }
