262a62c19e8b09c4e6b271c9522e7eb318e0d574
Tatenda Chipeperekwa <tatendac@codeaurora.org>
2014-07-02 19:09:04 -0700

    libstagefright: Add WFD specific flags to consumer usage flags
    Add the GRALLOC_USAGE_PRIVATE_WFD flag to the consumer usage
    flags to allow the display framework to distinguish WFD from
    other virtual display connections.
    
    Change-Id: I7cdbac3716fb5c18d1691701accb8e66f388de86

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -132,6 +132,11 @@ LOCAL_SHARED_LIBRARIES += \
 
 LOCAL_CFLAGS += -Wno-multichar -Werror -Wno-error=deprecated-declarations -Wall
 
+ifeq ($(TARGET_USES_QCOM_BSP), true)
+    LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
+    LOCAL_CFLAGS += -DQTI_BSP
+endif
+
 # enable experiments only in userdebug and eng builds
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 LOCAL_CFLAGS += -DENABLE_STAGEFRIGHT_EXPERIMENTS
