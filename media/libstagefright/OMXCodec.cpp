5e4a95a2fec7f9f71eb6f51b79899c2f19d1fbc9
Steve Kondik <shade@chemlab.org>
2014-07-15 12:59:03 -0700

    stagefright: Only use HW AAC for AV content
     * While this decoder can handle more streams, it can't handle gapless.
    
    Change-Id: I60cb3ca56c57a1280ca0805976e766e242ac1744

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -463,8 +463,11 @@ sp<MediaSource> OMXCodec::Create(
 
 #ifdef QCOM_HARDWARE
     int channelCount = 0;
+    int trackId = 0;
     meta->findInt32(kKeyChannelCount, &channelCount);
-    if (ExtendedCodec::useHWAACDecoder(mime, channelCount) && !createEncoder) {
+    source->getFormat()->findInt32(kKeyTrackID, &trackId);
+    if (ExtendedCodec::useHWAACDecoder(mime, channelCount) && !createEncoder
+            && trackId > 1) {
         findMatchingCodecs(mime, createEncoder,
             "OMX.qcom.audio.decoder.multiaac", flags, &matchingCodecs);
     } else {
