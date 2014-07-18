21cedf8f9e1b5d01d010a6f172f0de93387ec554
Zhou Song <zhous@codeaurora.org>
2014-07-18 17:34:36 +0800

    libstagefright: Decrease reference count before deleting AudioPlayer
    -Reference count of AudioSource is not decreased to 1 before deleting
     AudioPlayer, and thus cause the thread blocked in an infinite loop
     due to the AudioSource object is not deleted
    -Decrease the strong reference value of AudioSource before
     deleting AudioPlayer
    
    Change-Id: I804211dcca91213c62a4d622baf2001f3cc9955b
    CRs-Fixed: 691668

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1080,6 +1080,9 @@ status_t AwesomePlayer::play_l() {
             }
 
             if (err != OK) {
+                mAudioSource.clear();
+                mOmxSource.clear();
+
                 delete mAudioPlayer;
                 mAudioPlayer = NULL;
 
@@ -1153,6 +1156,7 @@ status_t AwesomePlayer::fallbackToSWDecoder() {
     if (!(mFlags & AUDIOPLAYER_STARTED)) {
         mAudioSource->stop();
     }
+    mAudioSource.clear();
     modifyFlags((AUDIO_RUNNING | AUDIOPLAYER_STARTED), CLEAR);
     mOffloadAudio = false;
 #ifdef ENABLE_AV_ENHANCEMENTS
@@ -1162,11 +1166,7 @@ status_t AwesomePlayer::fallbackToSWDecoder() {
 
     mAudioSource = mOmxSource;
     if (mAudioSource != NULL) {
-        err = mAudioSource->start();
-
-        if (err != OK) {
-            mAudioSource.clear();
-        } else {
+        if ((err = mAudioSource->start()) == OK) {
             mSeekNotificationSent = true;
             if (mExtractorFlags & MediaExtractor::CAN_SEEK) {
                 seekTo_l(curTimeUs);
@@ -2336,6 +2336,9 @@ void AwesomePlayer::onVideoEvent() {
         if (err != OK) {
             ALOGE("Failed to fallback to SW decoder err = %d", err);
             notifyListener_l(MEDIA_ERROR, MEDIA_ERROR_UNKNOWN, err);
+
+            mAudioSource.clear();
+            mOmxSource.clear();
 
             delete mAudioPlayer;
             mAudioPlayer = NULL;
