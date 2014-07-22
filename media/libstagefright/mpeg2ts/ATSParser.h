warpboard/platform_frameworks_av

AC3 support ATS (MPEG2TS) Parser
Added support for AC3 in TS parser.

Change-Id: Iaa7d29a7f791e1ca68156fa43a69d835dc4596ae

media/libstagefright/mpeg2ts/ATSParser.h
@@ -87,6 +87,8 @@ struct ATSParser : public RefBase {
         STREAMTYPE_MPEG2_AUDIO_ADTS     = 0x0f,
         STREAMTYPE_MPEG4_VIDEO          = 0x10,
         STREAMTYPE_H264                 = 0x1b,
+        // From Dolby AC3 A52b specification
+        STREAMTYPE_AC3                  = 0x81,
         STREAMTYPE_PCM_AUDIO            = 0x83,
     };
 

CyanogenMod/android_frameworks_av

media/libstagefright/mpeg2ts/ATSParser.h
@@ -89,6 +89,8 @@ struct ATSParser : public RefBase {
         STREAMTYPE_MPEG2_AUDIO_ADTS     = 0x0f,
         STREAMTYPE_MPEG4_VIDEO          = 0x10,
         STREAMTYPE_H264                 = 0x1b,
+        // From Dolby AC3 A52b specification
+        STREAMTYPE_AC3_AUDIO            = 0x81,
         STREAMTYPE_PCM_AUDIO            = 0x83,
     };
 