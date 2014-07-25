9347c28caadc67b20fce0b6633a908275defb05a
Steve Kondik <shade@chemlab.org>
2014-07-24 22:07:00 -0700

    stagefright: Handle offload fallback for 24-bit tracks
     * We need to carefully handle fallback in the 24-bit lossless
       sources for outputs such as A2DP, which cannot use PCM
       offload.
     * Clean up the code while in here as well.
    
    Change-Id: I398d964a3c5b18f58fcc79376bc29222e796badc

--- media/libstagefright/include/ExtendedUtils.h
+++ media/libstagefright/include/ExtendedUtils.h
@@ -167,6 +167,8 @@ struct ExtendedUtils {
 
     //helper function to parse rtp port range form system property
     static void parseRtpPortRangeFromSystemProperty(unsigned *start, unsigned *end);
+
+    static void updateOutputBitWidth(sp<MetaData> format, bool isOffload);
 };
 
 class VSyncLocker : public RefBase {
