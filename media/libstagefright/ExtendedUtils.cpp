ed1c91a5370d5c2254788d79a960b4e5cabd8683
Praneeth Paladugu <ppaladug@codeaurora.org>
2014-07-28 14:41:06 -0700

    libstagefright: Use QC MP4 parser
    Use QC MP4 parser by default for HEVC codec type. Incase
    codec is HEVC don't add extra start code.
    
    Change-Id: I26bab4208167b58166eea3756651e1113028057b

--- media/libstagefright/ExtendedUtils.cpp
+++ media/libstagefright/ExtendedUtils.cpp
@@ -339,7 +339,9 @@ sp<MediaExtractor> ExtendedUtils::MediaExtractor_CreateIfNeeded(sp<MediaExtracto
         bool success = meta->findCString(kKeyMIMEType, &mime);
         if ((success == true) &&
             (!strncasecmp(mime, MEDIA_MIMETYPE_AUDIO_AMR_WB_PLUS,
-                                strlen(MEDIA_MIMETYPE_AUDIO_AMR_WB_PLUS)) )) {
+                                strlen(MEDIA_MIMETYPE_AUDIO_AMR_WB_PLUS)) ||
+            !strncasecmp(mime, MEDIA_MIMETYPE_VIDEO_HEVC,
+                                strlen(MEDIA_MIMETYPE_VIDEO_HEVC)) )) {
 
             ALOGD("Discarding default extractor and using the extended one");
             bUseDefaultExtractor = false;
--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -3182,8 +3182,8 @@ bool OMXCodec::drainInputBuffer(BufferInfo *info) {
 
         size_t size = specific->mSize;
 
-        if ((!strcasecmp(MEDIA_MIMETYPE_VIDEO_AVC, mMIME) ||
-             !strcasecmp(MEDIA_MIMETYPE_VIDEO_HEVC, mMIME))
+        // Do not add start-code for HEVC since QC parser adds it
+        if ((!strcasecmp(MEDIA_MIMETYPE_VIDEO_AVC, mMIME))
                 && !(mQuirks & kWantsNALFragments)) {
             static const uint8_t kNALStartCode[4] =
                     { 0x00, 0x00, 0x00, 0x01 };
