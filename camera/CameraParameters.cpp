e223f499ba83c1a66c15b0d323ce8aa24f4d5d29
Konsta <konsta09@gmail.com>
2014-08-15 21:04:15 +0300

    camera: allow device to append camera parameters
    Overload using include/camera/CameraParametersExtra.h in device
    tree to add device specific camera parameters.
    
    Change-Id: Ie79380f4e49661348df4b2028ddf48daa67fca2d

--- camera/CameraParameters.cpp
+++ camera/CameraParameters.cpp
@@ -21,6 +21,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <camera/CameraParameters.h>
+#include <camera/CameraParametersExtra.h>
 #include <system/graphics.h>
 
 namespace android {
@@ -172,6 +173,10 @@ const char CameraParameters::FOCUS_MODE_CONTINUOUS_PICTURE[] = "continuous-pictu
 // Values for light fx settings
 const char CameraParameters::LIGHTFX_LOWLIGHT[] = "low-light";
 const char CameraParameters::LIGHTFX_HDR[] = "high-dynamic-range";
+
+#ifdef CAMERA_PARAMETERS_EXTRA_C
+CAMERA_PARAMETERS_EXTRA_C
+#endif
 
 CameraParameters::CameraParameters()
                 : mMap()
