caeb7517c333684b56ff444e762d362b216f23ae
Steve Kondik <shade@chemlab.org>
2014-08-15 09:29:53 -0700

    Revert "libstagefright: Keep LOOPING/AUTO_LOOPING flag during teardown"
    This reverts commit 9a59488dae2c70ab0c55cda7721824ca1a71340d.
    
    Change-Id: I9af6fe9f2086cfe17f55b214d5093d7acece791c

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -3523,7 +3523,6 @@ void AwesomePlayer::onAudioTearDownEvent() {
 
     // stream info is cleared by reset_l() so copy what we need
     mAudioTearDownWasPlaying = (mFlags & PLAYING);
-    uint32_t loopingFlags = (mFlags & (LOOPING | AUTO_LOOPING));
     KeyedVector<String8, String8> uriHeaders(mUriHeaders);
     sp<DataSource> fileSource(mFileSource);
 
@@ -3552,8 +3551,6 @@ void AwesomePlayer::onAudioTearDownEvent() {
         // a MEDIA_ERROR to the client and abort the prepare
         mFlags |= PREPARE_CANCELLED;
     }
-
-    mFlags |= loopingFlags;
 
     mAudioTearDown = true;
     mIsAsyncPrepare = true;
