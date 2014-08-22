e7cfa8b7d6ffacffe19322b744b3717022244ca4
Scott Mertz <scott@cyngn.com>
2014-08-22 14:15:31 -0700

    stagefright: stop audio source if player fails to start
    Change-Id: I70083f8bfe8409283e52eea017d27d94767c5244

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1080,6 +1080,10 @@ status_t AwesomePlayer::play_l() {
             }
 
             if (err != OK) {
+                mAudioSource->stop();
+                if (mAudioSource != mOmxSource && mOmxSource != NULL) {
+                    mOmxSource->stop();
+                }
                 mAudioSource.clear();
                 mOmxSource.clear();
 
