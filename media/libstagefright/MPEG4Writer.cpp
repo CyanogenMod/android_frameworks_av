e5bf3c1e355c45187e06c373e73f4d00a4c87311
Leena Winterrowd <lenhardw@codeaurora.org>
2014-08-27 11:13:29 -0700

    stagefright: Fix HEVC flag initialization
    In the case of audio-only tracks, the flag to indicate HEVC video
    was uninitialized and often passed a video-only condition. Properly
    initialize this flag to resolve the issue.
    
    CRs-Fixed: 718466
    Change-Id: I73dfd741f830664c444ff19c5969605c24cb52ad

--- media/libstagefright/MPEG4Writer.cpp
+++ media/libstagefright/MPEG4Writer.cpp
@@ -362,7 +362,8 @@ MPEG4Writer::MPEG4Writer(const char *filename)
       mLongitudex10000(0),
       mAreGeoTagsAvailable(false),
       mStartTimeOffsetMs(-1),
-      mHFRRatio(1) {
+      mHFRRatio(1),
+      mIsVideoHEVC(false) {
 
     mFd = open(filename, O_CREAT | O_LARGEFILE | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
     if (mFd >= 0) {
@@ -388,7 +389,8 @@ MPEG4Writer::MPEG4Writer(int fd)
       mLongitudex10000(0),
       mAreGeoTagsAvailable(false),
       mStartTimeOffsetMs(-1),
-      mHFRRatio(1) {
+      mHFRRatio(1),
+      mIsVideoHEVC(false) {
 }
 
 MPEG4Writer::~MPEG4Writer() {
