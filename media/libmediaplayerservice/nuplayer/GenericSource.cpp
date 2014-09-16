da23ab9b0dfa780175651daf057a02b0aa106dc8
Robert Shih <robertshih@google.com>
2014-09-16 11:34:08 -0700

    GenericSource: fix selectTrack parameter
    Bug: 17512187
    Change-Id: I19a0915fb0fb1b1ae036bf5df85b9c9e7657a7ea

--- media/libmediaplayerservice/nuplayer/GenericSource.cpp
+++ media/libmediaplayerservice/nuplayer/GenericSource.cpp
@@ -942,7 +942,7 @@ status_t NuPlayer::GenericSource::selectTrack(size_t trackIndex, bool select) {
     ALOGV("%s track: %zu", select ? "select" : "deselect", trackIndex);
     sp<AMessage> msg = new AMessage(kWhatSelectTrack, id());
     msg->setInt32("trackIndex", trackIndex);
-    msg->setInt32("select", trackIndex);
+    msg->setInt32("select", select);
 
     sp<AMessage> response;
     status_t err = msg->postAndAwaitResponse(&response);
