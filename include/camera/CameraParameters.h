f43fbd5ddcde26707b6f2d936cd983880615f09e
Tom Marshall <tdm@cyngn.com>
2014-09-17 15:56:23 -0700

    av: camera: Generic QCOM camera symbols
     * Several symbols declared Oppo specific are QCOM generic
    
    Change-Id: I0a1b5742d8e4d08963cbd43e69c87609eef45b59

--- include/camera/CameraParameters.h
+++ include/camera/CameraParameters.h
@@ -785,7 +785,7 @@ public:
     static const char WHITE_BALANCE_CLOUDY_DAYLIGHT[];
     static const char WHITE_BALANCE_TWILIGHT[];
     static const char WHITE_BALANCE_SHADE[];
-#ifdef OPPO_CAMERA_HARDWARE
+#ifdef QCOM_HARDWARE
     static const char WHITE_BALANCE_MANUAL_CCT[];
 #endif
 
@@ -949,7 +949,7 @@ public:
     // other modes.
     static const char FOCUS_MODE_CONTINUOUS_PICTURE[];
 
-#ifdef OPPO_CAMERA_HARDWARE
+#ifdef QCOM_HARDWARE
     static const char FOCUS_MODE_MANUAL_POSITION[];
 #endif
 
