5a791e1ed1318d3f9d9fe34accf3c4b28808da2a
Pavan Chikkala <pavanc@codeaurora.org>
2014-09-19 11:18:25 +0530

    frameworks/av: port fixes in LPA effects
    -audio: Fix for deadlock issue during LPA playback.
    -frameworks/av: Fix the LPA-AudioEffects dead lock issue CRs-Fixed: 477511
    -audioflinger: Fix pops in LPA while effects are applied CRs-Fixed: 526799
    -audioflinger: Fix delay in enabling effect for LPA CRs-Fixed: 525510
    
    Change-Id: Id3702bbeb0212cbe92370416b83daf0e383d6e3c

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -417,10 +417,8 @@ status_t AudioFlinger::ThreadBase::sendConfigEvent_l(sp<ConfigEvent>& event)
 }
 
 void AudioFlinger::ThreadBase::effectConfigChanged() {
-    mAudioFlinger->mLock.lock();
     ALOGV("New effect is being added to LPA chain, Notifying LPA Direct Track");
     mAudioFlinger->audioConfigChanged(AudioSystem::EFFECT_CONFIG_CHANGED, 0, NULL);
-    mAudioFlinger->mLock.unlock();
 }
 
 void AudioFlinger::ThreadBase::sendIoConfigEvent(int event, int param)
@@ -2597,8 +2595,10 @@ bool AudioFlinger::PlaybackThread::threadLoop()
             // only process effects if we're going to write
             if (sleepTime == 0 && mType != OFFLOAD) {
                 for (size_t i = 0; i < effectChains.size(); i ++) {
-                    effectChains[i]->process_l();
-                }
+                    if (effectChains[i] != mAudioFlinger->mLPAEffectChain) {
+                        effectChains[i]->process_l();
+                  }
+               }
             }
         }
         // Process effect chains for offloaded thread even if no audio
