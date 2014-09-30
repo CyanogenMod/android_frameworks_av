CyanogenMod/android_frameworks_av

commit b732cf5af93c33fa183769210ce9954521fb68cd

audio policy: favor mixed over direct output for PCM format
modify getOutputForDevice() to skip direct output selection
if the request can obviously be attached to a mixed output
This prevents from selecting a direct output profile that
can manage stereo PCM format if a mixer output is available
to reach the same device.

Bug: 17628413.
Change-Id: I02e9a0b02c9ae5f44ff5768ac4ed02ce84d521c5

services/audiopolicy/AudioPolicyManager.cpp
@@ -927,13 +927,21 @@ audio_io_handle_t AudioPolicyManager::getOutputForDevice(
         flags = (audio_output_flags_t)(flags | AUDIO_OUTPUT_FLAG_DIRECT);
     }
 
+    sp<IOProfile> profile;
+
+    // skip direct output selection if the request can obviously be attached to a mixed output
+    if (audio_is_linear_pcm(format) && samplingRate <= MAX_MIXER_SAMPLING_RATE &&
+            audio_channel_count_from_out_mask(channelMask) <= 2) {
+        goto non_direct_output;
+    }
+
     // Do not allow offloading if one non offloadable effect is enabled. This prevents from
     // creating an offloaded track and tearing it down immediately after start when audioflinger
     // detects there is an active non offloadable effect.
     // FIXME: We should check the audio session here but we do not have it in this context.
     // This may prevent offloading in rare situations where effects are left active by apps
     // in the background.
-    sp<IOProfile> profile;
+
     if (((flags & AUDIO_OUTPUT_FLAG_COMPRESS_OFFLOAD) == 0) ||
             !isNonOffloadableEffectEnabled()) {
         profile = getProfileForDirectOutput(device,
@@ -1015,6 +1023,8 @@ audio_io_handle_t AudioPolicyManager::getOutputForDevice(
         mpClientInterface->onAudioPortListUpdate();
         return output;
     }
+
+non_direct_output:
 
     // ignoring channel mask due to downmix capability in mixer
 

commit c260784e37dea73a2090d4ccd91472d61d3b6230

audio policy: fix HW A/V sync output selection
commit b732cf5a broke the selection mechanism for the
HW A/V sync direct output.
Add a rule to only select a mixed output for basic PCM
stereo content when a direct output is not explicitly requested.

Bug: 17702382.

Change-Id: I1387690ace77cd4d6a8faf0b525c3d1008fba3f1

services/audiopolicy/AudioPolicyManager.cpp
@@ -930,7 +930,9 @@ audio_io_handle_t AudioPolicyManager::getOutputForDevice(
     sp<IOProfile> profile;
 
     // skip direct output selection if the request can obviously be attached to a mixed output
-    if (audio_is_linear_pcm(format) && samplingRate <= MAX_MIXER_SAMPLING_RATE &&
+    // and not explicitly requested
+    if (((flags & AUDIO_OUTPUT_FLAG_DIRECT) == 0) &&
+            audio_is_linear_pcm(format) && samplingRate <= MAX_MIXER_SAMPLING_RATE &&
             audio_channel_count_from_out_mask(channelMask) <= 2) {
         goto non_direct_output;
     }