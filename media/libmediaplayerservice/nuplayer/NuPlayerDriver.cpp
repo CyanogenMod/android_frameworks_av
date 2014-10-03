8d121d41f5355b78b687f44e8d4aae4de2aa0359
Andy Hung <hunga@google.com>
2014-10-03 09:53:53 -0700

    NuPlayer: wait for renderer flush before decoder shutdown
    Bug: 17679341
    Change-Id: Ie3883686891e7ee6fb45ceb01af1eb60b559d3a0

--- media/libmediaplayerservice/nuplayer/NuPlayerDriver.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerDriver.cpp
@@ -582,7 +582,7 @@ status_t NuPlayerDriver::getMetadata(
 }
 
 void NuPlayerDriver::notifyResetComplete() {
-    ALOGI("notifyResetComplete(%p)", this);
+    ALOGD("notifyResetComplete(%p)", this);
     Mutex::Autolock autoLock(mLock);
 
     CHECK_EQ(mState, STATE_RESET_IN_PROGRESS);
