ccf980db08cee75d2aaabd686e1d4e181f6189f9
Steve Kondik <shade@chemlab.org>
2014-10-11 22:40:44 -0700

    stagefright: Query PCM output format and fix bugs
     * Ask the decoder about it's PCM output format instead
       of hardcoding exceptions.
     * Google's AAC codec fails on main profile, skip it.
     * Don't crash on unsupported Windows Media files.
    
    Change-Id: I2bfe19993e8b0cf7ff78b6d4b63465be093ae1d0

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -771,8 +771,14 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
         CHECK(meta->findInt32(kKeyChannelCount, &numChannels));
         CHECK(meta->findInt32(kKeySampleRate, &sampleRate));
 
-        if (!meta->findInt32(kKeyAACProfile, &aacProfile)) {
+        if (!meta->findInt32(kKeyAACAOT, &aacProfile)) {
             aacProfile = OMX_AUDIO_AACObjectNull;
+        }
+
+        // Google's AAC decoder can't handle MAIN profile
+        if (!strncmp(mComponentName, "OMX.google.", 11) &&
+                aacProfile == OMX_AUDIO_AACObjectMain) {
+            return ERROR_UNSUPPORTED;
         }
 
         int32_t isADTS;
@@ -860,12 +866,6 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
                 CODEC_LOGE("setAC3Format() failed (err = %d)", err);
                 return err;
             }
-#ifdef ENABLE_AV_ENHANCEMENTS
-            // FFMPEG will convert floating point to 24-bit PCM
-            if (ExtendedUtils::isHiresAudioEnabled()) {
-                meta->setInt32(kKeySampleBits, 24);
-            }
-#endif
         } else if (!strcasecmp(MEDIA_MIMETYPE_AUDIO_APE, mMIME))  {
             status_t err = setAPEFormat(meta);
             if (err != OK) {
@@ -878,12 +878,6 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
                 CODEC_LOGE("setDTSFormat() failed (err = %d)", err);
                 return err;
             }
-#ifdef ENABLE_AV_ENHANCEMENTS
-            // FFMPEG will convert DTS floating point to 24-bit PCM
-            if (ExtendedUtils::isHiresAudioEnabled()) {
-                meta->setInt32(kKeySampleBits, 24);
-            }
-#endif
         } else if (!strcasecmp(MEDIA_MIMETYPE_AUDIO_FFMPEG, mMIME))  {
             status_t err = setFFmpegAudioFormat(meta);
             if (err != OK) {
@@ -891,6 +885,8 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
                 return err;
             }
         }
+        getPCMOutputFormat(meta);
+        meta->dumpToLog();
 #ifdef QCOM_HARDWARE
     } else {
         if (!mIsVideo && mIsEncoder) {
@@ -4360,6 +4356,28 @@ status_t OMXCodec::setFFmpegVideoFormat(const sp<MetaData> &meta)
     return err;
 }
 
+status_t OMXCodec::getPCMOutputFormat(const sp<MetaData> &meta)
+{
+    int32_t bitsPerSample = 16;
+    status_t err = OK;
+
+    OMX_AUDIO_PARAM_PCMMODETYPE params;
+    InitOMXParams(&params);
+    params.nPortIndex = kPortIndexOutput;
+
+    err = mOMX->getParameter(
+            mNode, OMX_IndexParamAudioPcm, &params, sizeof(params));
+
+    if (err == OK) {
+        ALOGI("  nSamplingRate = %ld\n", params.nSamplingRate);
+        ALOGI("  nChannels = %ld\n", params.nChannels);
+        ALOGI("  nBitPerSample = %ld\n", params.nBitPerSample);
+
+        meta->setInt32(kKeySampleBits, params.nBitPerSample);
+    }
+    return err;
+}
+
 //audio
 status_t OMXCodec::setMP3Format(const sp<MetaData> &meta)
 {
@@ -4427,7 +4445,9 @@ status_t OMXCodec::setWMAFormat(const sp<MetaData> &meta)
     CHECK(meta->findInt32(kKeyChannelCount, &numChannels));
     CHECK(meta->findInt32(kKeySampleRate, &sampleRate));
     CHECK(meta->findInt32(kKeyBitRate, &bitRate));
-    CHECK(meta->findInt32(kKeyBlockAlign, &blockAlign));
+    if (!meta->findInt32(kKeyBlockAlign, &blockAlign)) {
+        return ERROR_UNSUPPORTED;
+    }
 
     CODEC_LOGV("Channels: %d, SampleRate: %d, BitRate: %d, blockAlign: %d",
             numChannels, sampleRate, bitRate, blockAlign);
@@ -5816,7 +5836,7 @@ void OMXCodec::initOutputFormat(const sp<MetaData> &inputFormat) {
                 CHECK_EQ(err, (status_t)OK);
 
                 CHECK_EQ((int)params.eNumData, (int)OMX_NumericalDataSigned);
-                CHECK_EQ(params.nBitPerSample, 16u);
+                //CHECK_EQ(params.nBitPerSample, 16u);
                 CHECK_EQ((int)params.ePCMMode, (int)OMX_AUDIO_PCMModeLinear);
 
                 int32_t numChannels, sampleRate;
