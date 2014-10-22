159c1b2c00cdcea0932eb46b8825d7c251d8e4a4
Steve Kondik <shade@chemlab.org>
2014-10-22 14:43:57 -0700

    Revert "stagefright: Disable HSR for now"
     * Adding support for HSR on new chips.
    
    This reverts commit 23373ed880fc124f20a6ee90e539576ffbfddba8.
    
    Change-Id: I468498e72bae874184bdbb85cbc92c735365b403

--- media/libstagefright/ExtendedUtils.cpp
+++ media/libstagefright/ExtendedUtils.cpp
@@ -80,7 +80,7 @@ void ExtendedUtils::HFR::setHFRIfEnabled(
         hfr = 0;
     }
 
-#if 0
+#ifndef LEGACY_MEDIA
     const char *hsr_str = params.get("video-hsr");
 
     if(hsr_str && !strncmp(hsr_str,"on",2)) {
@@ -96,7 +96,7 @@ status_t ExtendedUtils::HFR::initializeHFR(
         int64_t &maxFileDurationUs, video_encoder videoEncoder) {
     status_t retVal = OK;
 
-#if 0
+#ifndef LEGACY_MEDIA
     //Check HSR first, if HSR is enable set HSR to kKeyFrameRate
     int32_t hsr =0;
     if (meta->findInt32(kKeyHSR, &hsr)) {
