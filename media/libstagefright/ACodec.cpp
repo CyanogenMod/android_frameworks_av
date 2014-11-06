775cb99acb999fa173546d29b9d6ed861457afaa
Per Persson <per.persson@intel.com>
2014-11-06 13:26:51 +0100

    libstagefright: ACodec: Add missing message handling
    Add kWhatSetParameters as a valid received message in state
    OutputPortSettingsChangedState.
    Without this fix, the mediaserver can end up in an endless loop
    consuming significant extra cpu power.
    
    Change-Id: I9719f1698bf411914eb753d3c349a15b9fbf9df6
    Signed-off-by: Per Persson <per.persson@intel.com>
    Signed-off-by: Persson, Mikael <mikael.persson@intel.com>
    Signed-off-by: Fagerstedt, Axel <axel.fagerstedt@intel.com>
    Signed-off-by: Yong Yao <yong.yao@intel.com>

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -5956,6 +5956,7 @@ bool ACodec::OutputPortSettingsChangedState::onMessageReceived(
         case kWhatFlush:
         case kWhatShutdown:
         case kWhatResume:
+        case kWhatSetParameters:
         {
             if (msg->what() == kWhatResume) {
                 ALOGV("[%s] Deferring resume", mCodec->mComponentName.c_str());
