0fcb20024c9d40fc2f70959fae11a3ef118a7926
Ethan Chen <intervigil@gmail.com>
2014-11-09 00:09:14 -0800

    frameworks/av: Use project pathmap
    Change-Id: Icefaaa985a551bb3037f6f457a108fd3fe8c5363

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -147,13 +147,13 @@ LOCAL_STATIC_LIBRARIES := \
         libmedia_helper
 
 ifeq ($(TARGET_USES_QCOM_BSP), true)
-    LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
+    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/$(TARGET_BOARD_PLATFORM)/libgralloc
     LOCAL_CFLAGS += -DQCOM_BSP
 endif
 
 ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS),true)
        LOCAL_CFLAGS     += -DENABLE_AV_ENHANCEMENTS
-       LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/media/mm-core/inc
+       LOCAL_C_INCLUDES += $(TOP)/$(call project-path-for,qcom-media)/$(TARGET_BOARD_PLATFORM)/mm-core/inc
        LOCAL_C_INCLUDES += $(TOP)/frameworks/av/media/libstagefright/include
        LOCAL_SRC_FILES  += ExtendedMediaDefs.cpp
        LOCAL_SRC_FILES  += ExtendedWriter.cpp
@@ -163,7 +163,7 @@ endif #TARGET_ENABLE_AV_ENHANCEMENTS
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PCM_OFFLOAD_24)),true)
        LOCAL_CFLAGS     += -DPCM_OFFLOAD_ENABLED_24
-       LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/media/mm-core/inc
+       LOCAL_C_INCLUDES += $(TOP)/$(call project-path-for,qcom-media)/$(TARGET_BOARD_PLATFORM)/mm-core/inc
 endif
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PCM_OFFLOAD)),true)
        LOCAL_CFLAGS     += -DPCM_OFFLOAD_ENABLED
