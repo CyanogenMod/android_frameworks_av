dec93485132daa9b95908282f9bf77d9e55f1426
Steve Kondik <steve@cyngn.com>
2014-11-09 12:36:34 -0800

    libmedia: Add some ifdefs for QCOM-specific features
    Change-Id: Ia811205e7dc2d331a084b4d7230bf35c14882158

--- services/audiopolicy/AudioPolicyManager.cpp
+++ services/audiopolicy/AudioPolicyManager.cpp
@@ -5659,12 +5659,14 @@ const AudioPolicyManager::VolumeCurvePoint
         sDefaultMediaVolumeCurve, // DEVICE_CATEGORY_EARPIECE
         sDefaultMediaVolumeCurve  // DEVICE_CATEGORY_EXT_MEDIA
     },
+#ifdef QCOM_HARDWARE
     { // AUDIO_STREAM_INCALL_MUSIC
         sDefaultMediaVolumeCurve, // DEVICE_CATEGORY_HEADSET
         sSpeakerMediaVolumeCurve, // DEVICE_CATEGORY_SPEAKER
         sDefaultMediaVolumeCurve,  // DEVICE_CATEGORY_EARPIECE
         sDefaultMediaVolumeCurve  // DEVICE_CATEGORY_EXT_MEDIA
     },
+#endif
 };
 
 void AudioPolicyManager::initializeVolumeCurves()
