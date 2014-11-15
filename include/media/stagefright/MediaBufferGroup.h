8009a29b5381d02e2fbfe8f10d6d174a80c31898
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-15 10:12:13 -0600

    stagefright: add mising MediaBufferGroup::acquire_buffer symbol
    * Instead of implying false, use overloaded methods to set it false
    * Fixes blobs that link against this
    
    Change-Id: I215aa7e0dfe28bef98caa49528211185d43a4214

--- include/media/stagefright/MediaBufferGroup.h
+++ include/media/stagefright/MediaBufferGroup.h
@@ -39,7 +39,8 @@ public:
     // The returned buffer will have a reference count of 1.
     // If nonBlocking is true and a buffer is not immediately available,
     // buffer is set to NULL and it returns WOULD_BLOCK.
-    status_t acquire_buffer(MediaBuffer **buffer, bool nonBlocking = false);
+    status_t acquire_buffer(MediaBuffer **buffer);
+    status_t acquire_buffer(MediaBuffer **buffer, bool nonBlocking);
 
 protected:
     virtual void signalBufferReturned(MediaBuffer *buffer);
