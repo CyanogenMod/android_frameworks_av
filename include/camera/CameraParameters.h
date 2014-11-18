6c52703b9eb628f4c814f1af2d8742367eede57f
Thomas Wendt <thoemy@gmx.net>
2014-11-18 22:16:36 +0100

    Camera: Add HTC parameters
    Squashed commit of the following:
    
    commit 49eeada0dbefda45af6c6e346233f13a28bd58d9
    Author: atis112 <toth.ati13@gmail.com>
    Date:   Sun Jun 30 19:46:32 2013 +0100
    
        Camera: Add more htc parameters
    
        Change-Id: Ica43a42f6ec6e25671b22536c768140469fb9824
    
    commit 101014d6f2efdcc5cafbe01170e7bfcc4ac264fb
    Author: Daniel Hillenbrand <codeworkx@cyanogenmod.org>
    Date:   Thu Jun 27 17:16:10 2013 +0200
    
        camera: add more htc parameters
    
        Google Edition
    
        Change-Id: I67968ea749dec0bf3810110f99bccdd3edee3428
    
    commit 45745240d60370b8e36160ca525f6124d39f3b80
    Author: Daniel Hillenbrand <codeworkx@cyanogenmod.org>
    Date:   Sun May 5 15:56:08 2013 +0200
    
        camera: fixup htc params
    
        Change-Id: Iee0086e8a7319ada09f5191092c314c099e0d66a
    
    commit 7628e0fc88de55d298478854095d1aa8acf430ec
    Author: Daniel Hillenbrand <codeworkx@cyanogenmod.org>
    Date:   Sat May 4 19:48:15 2013 +0200
    
        [WIP]: camera: add parameters for htc cameras
    
        Change-Id: I1456e5af22d1cb68fd19626a136fef68c5573074
    
    Change-Id: Ifbe6d718db031eb9b4c0cb645fb2e60d25339a77

--- include/camera/CameraParameters.h
+++ include/camera/CameraParameters.h
@@ -536,6 +536,50 @@ public:
     // Example values: "lowlight,hdr".
     static const char KEY_LIGHTFX[];
 
+#ifdef HTC_CAMERA_HARDWARE
+    static const char KEY_TIME_CONS_POST_PROCESSING[];
+    static const char KEY_OIS_MODE[];
+    static const char KEY_APP_OIS_SETTING[];
+    static const char KEY_OIS_SUPPORT[];
+    static const char KEY_CONTIBURST_TYPE[];
+    static const char KEY_CAPTURE_MODE[];
+    static const char CAPTURE_MODE_NORMAL[];
+    static const char CAPTURE_MODE_CONTI_ZOE[];
+    static const char CAPTURE_MODE_CONTI_BURST[];
+    static const char CAPTURE_MODE_CONTI_BURST_ONE_SHOT[];
+    static const char CAPTURE_MODE_HDR[];
+    static const char CAPTURE_MODE_PANORAMA[];
+    static const char CAPTURE_MODE_ZOE[];
+    static const char KEY_CONTI_BURST_STATE[];
+    static const char KEY_SUPPORTED_CAPTURE_MODES[];
+    static const char KEY_MIN_CONTRAST[];
+    static const char KEY_DEF_CONTRAST[];
+    static const char KEY_MIN_SHARPNESS[];
+    static const char KEY_DEF_SHARPNESS[];
+    static const char KEY_MIN_SATURATION[];
+    static const char KEY_DEF_SATURATION[];
+    static const char KEY_SINGLE_ISP_OUTPUT_ENABLED[];
+    static const char POST_PROCESSING_ENABLE[];
+    static const char POST_PROCESSING_BYPASS[];
+    static const char POST_PROCESSING_DELAY[];
+    static const char SCENE_MODE_OFF[];
+    static const char SCENE_MODE_TEXT[];
+    static const char BURST_MODE_LIMIT20[];
+    static const char BURST_MODE_UNLIMITED[];
+    static const char OIS_MODE_OFF[];
+    static const char OIS_MODE_ON[];
+    static const char CONTI_BURST_CAPTURING[];
+    static const char CONTI_BURST_CAPTURE_DONE[];
+    static const char APP_OIS_SETTING_FALSE[];
+    static const char APP_OIS_SETTING_TRUE[];
+    static const char KEY_GPU_EFFECT[];
+    static const char KEY_GPU_EFFECT_PARAM_0[];
+    static const char KEY_GPU_EFFECT_PARAM_1[];
+    static const char KEY_GPU_EFFECT_PARAM_2[];
+    static const char KEY_GPU_EFFECT_PARAM_3[];
+    static const char KEY_FORCE_USE_AUDIO_ENABLED[];
+#endif
+
     // Value for KEY_ZOOM_SUPPORTED or KEY_SMOOTH_ZOOM_SUPPORTED.
     static const char TRUE[];
     static const char FALSE[];
