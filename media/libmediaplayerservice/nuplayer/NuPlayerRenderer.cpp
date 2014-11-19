a87f46ed873c8393ce7d79c470b9f75bbcd8851c
Ronghua Wu <ronghuawu@google.com>
2014-11-19 10:02:16 -0800

    DO NOT MERGE - NuPlayer: invalid anchor time when render is flushed.
    Also rememeber the pause time in NuPlayerDriver.
    
    Bug: 18436336
    Change-Id: If09f680a0d0f9137bd55c5f94201eb3aa783278a

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -1073,6 +1073,7 @@ void NuPlayer::Renderer::onFlush(const sp<AMessage> &msg) {
          if (!(offloadingAudio()) && !(mPaused)) {
              setPauseStartedTimeRealUs(-1);
          }
+         setAnchorTime(-1, -1);
     }
 
     ALOGV("flushing %s", audio ? "audio" : "video");
