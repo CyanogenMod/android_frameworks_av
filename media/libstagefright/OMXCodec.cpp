f4423d4b3e6c1468662fcba8f02adb7f9d9c0f0d
Steve Kondik <steve@cyngn.com>
2014-11-22 02:47:45 -0800

    av: Use QCOM_BSP_LEGACY ifdefs for interlace handling
     * This demolished video playback on 8974.
    
    Change-Id: Ifca5319669c463993fca05218c06ccf49cbe6c06

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -647,7 +647,9 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
                 return err;
             }
 
-	    ExtendedUtils::setArbitraryModeIfInterlaced((const uint8_t *)data, meta);
+#ifdef QCOM_BSP_LEGACY
+	        ExtendedUtils::setArbitraryModeIfInterlaced((const uint8_t *)data, meta);
+#endif
 
             CODEC_LOGI(
                     "AVC profile = %u (%s), level = %u",
@@ -2113,7 +2115,7 @@ status_t OMXCodec::allocateOutputBuffersFromNativeWindow() {
         return err;
     }
 
-#ifdef QCOM_BSP
+#ifdef QCOM_BSP_LEGACY
     err = mNativeWindow.get()->perform(mNativeWindow.get(),
 			     NATIVE_WINDOW_SET_BUFFERS_SIZE, def.nBufferSize);
     if (err != 0) {
@@ -2982,7 +2984,7 @@ void OMXCodec::onStateChange(OMX_STATETYPE newState) {
                 mPortStatus[kPortIndexOutput] = ENABLED;
 
                 if (mNativeWindow != NULL) {
-#ifdef QCOM_BSP
+#ifdef QCOM_BSP_LEGACY
 		    /*
 		     * reset buffer size field with SurfaceTexture
 		     * back to 0. This wil ensure proper size
