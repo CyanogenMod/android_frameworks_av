2a640efbaa5d4c8b2fb83803001493042eff28eb
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-23 22:54:59 -0600

    libstagefright: matroska: add missing include dir and header
    Change-Id: Ied9c32307e207d171182b83f65368b34c7204651

--- media/libstagefright/matroska/Android.mk
+++ media/libstagefright/matroska/Android.mk
@@ -8,6 +8,7 @@ LOCAL_C_INCLUDES:= \
         $(TOP)/external/libvpx/libwebm \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/frameworks/av/media/libstagefright/include \
+        $(TOP)/$(call project-path-for,qcom-media)/mm-core/inc \
 
 LOCAL_CFLAGS += -Wno-multichar -Werror
 
