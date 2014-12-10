9942a567b2f14f5b9518fb3831e43abb7970f200
Surajit Podder <spodder@codeaurora.org>
2014-12-10 14:55:52 +0530

    libstagefright: Explicitly disconnect HTTP connection
    The HTTP connection created by StagefrightMetadataRetriever
    is not closed explicitly, and tcp activity continues even
    after metadata retriever is destroyed
    
    Explicitly disconnect HTTP connection in StagefrightMetadataRetriever
    
    Change-Id: I261d369e8dcff1bc0a3c7225243e865858b4fc7c
    CRs-Fixed: 754684

--- media/libstagefright/StagefrightMetadataRetriever.cpp
+++ media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -22,6 +22,7 @@
 #include <utils/Log.h>
 
 #include "include/StagefrightMetadataRetriever.h"
+#include "include/HTTPBase.h"
 
 #include <media/IMediaHTTPService.h>
 #include <media/stagefright/foundation/ADebug.h>
@@ -54,6 +55,12 @@ StagefrightMetadataRetriever::~StagefrightMetadataRetriever() {
     mAlbumArt = NULL;
 
     mClient.disconnect();
+
+    if (mSource != NULL &&
+        (mSource->flags() & DataSource::kIsHTTPBasedSource)) {
+        mExtractor.clear();
+        static_cast<HTTPBase *>(mSource.get())->disconnect();
+    }
 }
 
 status_t StagefrightMetadataRetriever::setDataSource(
