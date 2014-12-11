ed3c442304eaa53ecd0ff1bd5162698bd2f6a4c2
Steve Kondik <steve@cyngn.com>
2014-12-10 16:30:12 -0800

    stagefright: Fix FFMPEG catchall decoders
     * Get this metadata properly flowing
     * Allows us to play tracks such as Apple Lossless :)
    
    Change-Id: I2990b30eef5b672da339d24444424c61a43b85c2

--- media/libstagefright/ExtendedCodec.cpp
+++ media/libstagefright/ExtendedCodec.cpp
@@ -105,6 +105,8 @@ static const MetaKeyEntry MetaKeyTable[] {
    {kKeyChannelCount         , "channel-count"          , INT32},
 
    {kKeySampleBits           , "bits-per-sample"        , INT32},
+   {kKeyCodecId              , "codec-id"               , INT32},
+   {kKeySampleFormat         , "sample-format"          , INT32},
 };
 
 const char* ExtendedCodec::getMsgKey(int key) {
