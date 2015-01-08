96b83c7a62803c460654e8d5156f1a3976cadf9e
Dan Pasanen <dan.pasanen@gmail.com>
2015-01-07 18:57:09 -0600

    MediaPlayerFactory: only pay attention to the persist prop for awesomeplayer
    * No need to have two props for this
    
    Change-Id: I5ffe24f912e7a9e344b31f16f086f898ba663e9c

--- media/libmediaplayerservice/MediaPlayerFactory.cpp
+++ media/libmediaplayerservice/MediaPlayerFactory.cpp
@@ -63,11 +63,6 @@ status_t MediaPlayerFactory::registerFactory_l(IFactory* factory,
 
 static player_type getDefaultPlayerType() {
     char value[PROPERTY_VALUE_MAX];
-    if (property_get("media.stagefright.use-awesome", value, NULL)
-            && (!strcmp("1", value) || !strcasecmp("true", value))) {
-        return STAGEFRIGHT_PLAYER;
-    }
-
     // TODO: remove this EXPERIMENTAL developer settings property
     if (property_get("persist.sys.media.use-awesome", value, NULL)
             && !strcasecmp("true", value)) {
