677096dbf052b5a94233ff1530d3e7f42121c885
Ramjee Singh <ramjee@codeaurora.org>
2015-01-13 19:36:47 +0530

    audio: Added support for USB AOA connect/disconnect events
    USB AOA is supported by primary HAL on A family through
    customized connect/disconnect parameters.Added support
    for that in AudioPolicyManager
    
    Change-Id: I5212027b2711c24a9750629774f69d669a9d164f

--- services/audiopolicy/AudioPolicyManager.cpp
+++ services/audiopolicy/AudioPolicyManager.cpp
@@ -392,6 +392,14 @@ status_t AudioPolicyManager::setDeviceConnectionStateInt(audio_devices_t device,
                mpClientInterface->setParameters(0, param.toString());
             }
 
+#ifdef QCOM_DIRECTTRACK
+            if (audio_is_usb_device(device)) {
+               AudioParameter param;
+               param.add(String8("usb_connected"), String8("true"));
+               mpClientInterface->setParameters(0, param.toString());
+            }
+#endif
+
             if (index >= 0) {
                 sp<HwModule> module = getModuleForDevice(device);
                 if (module == 0) {
@@ -463,6 +471,15 @@ status_t AudioPolicyManager::setDeviceConnectionStateInt(audio_devices_t device,
                param.add(String8("a2dp_connected"), String8("false"));
                mpClientInterface->setParameters(0, param.toString());
             }
+
+#ifdef QCOM_DIRECTTRACK
+            if (audio_is_usb_device(device)) {
+                // handle USB device disconnection
+                AudioParameter param;
+                param.add(String8("usb_connected"), String8("false"));
+                mpClientInterface->setParameters(0, param.toString());
+            }
+#endif
 
             checkOutputsForDevice(devDesc, state, outputs, address);
             } break;
