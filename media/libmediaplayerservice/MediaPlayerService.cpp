caf2300f87548a29ce54316501e046fa8ac3b7f3
Pavan Chikkala <pavanc@codeaurora.org>
2015-01-20 19:41:28 +0530

    audio: Added fallback mechanism for Tunnel Mode
    On L, checkTunnelExceptions always gets stream type as MUSIC
    because AudioOutput gets the actual stream only after AudioTrack
    is created. AwesomePlayer tries play MP3/AAC ringtones in tunnel
    mode due to this reason. Also, AudioOutput fails to set the
    actual stream type for tunnel case which results in MUSIC
    and RINGTONE getting mixed
    
    Added fallback mechasism for tunnel mode so that ring tones
    are rejected by the tunnel player and is switched to
    deep buffer mode. Updated stream type for tunnel case in
    AudioOutput after AudioTrack is created
    
    CRs-Fixed: 774774
    
    Change-Id: I408441c8c0562ef36beddd4f6efb76752d3fb0a0

--- media/libmediaplayerservice/MediaPlayerService.cpp
+++ media/libmediaplayerservice/MediaPlayerService.cpp
@@ -1651,7 +1651,12 @@ status_t MediaPlayerService::AudioOutput::open(
                              CallbackWrapper,
                              newcbd,
                              0,
-                             mSessionId);
+                             mSessionId,
+                             AudioTrack::TRANSFER_DEFAULT,
+                             NULL,
+                             -1,
+                             -1,
+                             mAttributes);
             if ((audioTrack == 0) || (audioTrack->initCheck() != NO_ERROR)) {
                 ALOGE("Unable to create audio track");
                 delete audioTrack;
@@ -1664,6 +1669,7 @@ status_t MediaPlayerService::AudioOutput::open(
         }
         deleteRecycledTrack();
 
+        mStreamType = audioTrack->streamType();
         ALOGV("setVolume");
         mCallbackData = newcbd;
         audioTrack->setVolume(mLeftVolume, mRightVolume);
