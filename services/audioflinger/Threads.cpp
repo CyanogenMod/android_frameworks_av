d751404dd05a612fb3c8340874ab3309400595a7
dk.han <dk.han@lge.com>
2015-01-20 11:00:42 +0900

    Fix bugs don't remove track of DirectOutputThread
    Don't remove track of DirectOutputThread by a wrong state control for track stop processor.
    Change a state to STOPPING_1 in Track::stop() function, but check STOPPING_2 state in DirectOutput::prepareTracks_l
    function.
    So the Track is doesn't add tracksToRemove vector.
    
    I thing, change check condition from isStopping_2() to isStopping() function in DirectOutput::prepareTracks_l() function.
    
    Change-Id: I68c4346f02ef1b8635a2180c2a0b3089ce2af955
    Signed-off-by: DongKyoon Han <dk.han@lge.com>

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -4065,7 +4065,7 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::DirectOutputThread::prep
         }
 
         if ((track->framesReady() >= minFrames) && track->isReady() && !track->isPaused() &&
-                !track->isStopping_2() && !track->isStopped())
+                !track->isStopping() && !track->isStopped())
         {
             ALOGVV("track %d s=%08x [OK]", track->name(), cblk->mServer);
 
