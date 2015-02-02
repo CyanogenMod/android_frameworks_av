8355aecb3c25b62cf5db4a350b2daeb97fc9449e
Ramjee Singh <ramjee@codeaurora.org>
2015-02-02 20:09:35 +0530

    audio: Set OutputFormat of AudioMixer to PCM16
    - MixerThread sets OutputFormat to PCM_FLOAT by default
      We are having issue with SRS Effects due to this format
    - Fix is to select always PCM16 format as Audio HAL supports
      only PCM16
    
    Change-Id: I26d23836180fe95b4c32b071593827b6fe4d674e

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -2856,11 +2856,15 @@ AudioFlinger::MixerThread::MixerThread(const sp<AudioFlinger>& audioFlinger, Aud
     }
     if (initFastMixer) {
         audio_format_t fastMixerFormat;
+#ifndef QCOM_DIRECTTRACK
         if (mMixerBufferEnabled && mEffectBufferEnabled) {
             fastMixerFormat = AUDIO_FORMAT_PCM_FLOAT;
         } else {
+#endif
             fastMixerFormat = AUDIO_FORMAT_PCM_16_BIT;
+#ifndef QCOM_DIRECTTRACK
         }
+#endif
         if (mFormat != fastMixerFormat) {
             // change our Sink format to accept our intermediate precision
             mFormat = fastMixerFormat;
