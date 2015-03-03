0414ebcce5dcac6d5333894fed2d636d3cb0c225
Dhananjay Kumar <dhakumar@codeaurora.org>
2015-03-03 22:00:32 +0530

    soundpool: update sample rate when reusing audiotrack
    User might update playback rate while calling Play on a sample.
    Update sample rate if reusing existing audio track.
    
    Change-Id: I7007c335fa405baf663a322408b92b9df53efdb0

--- media/libmedia/SoundPool.cpp
+++ media/libmedia/SoundPool.cpp
@@ -649,6 +649,7 @@ void SoundChannel::play(const sp<Sample>& sample, int nextChannelID, float leftV
             ALOGV("using new track %p for sample %d", newTrack.get(), sample->sampleID());
         } else {
             newTrack = mAudioTrack;
+            newTrack->setSampleRate(sampleRate);
             ALOGV("reusing track %p for sample %d", mAudioTrack.get(), sample->sampleID());
         }
         newTrack->setVolume(leftVolume, rightVolume);
