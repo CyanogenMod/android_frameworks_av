bc64b8c499a158a3938beb94a2ed415aca08de65
Apoorv Raghuvanshi <apoorvr@codeaurora.org>
2015-03-11 00:48:17 -0700

    stagefright: featurise QTI FLAC decoder-specific code
    This is QTI FLAC specific code, it should be placed under
    its feature tag. Change basically has the effect of
    retaining the old behavior if QTI_FLAC_DECODER is not
    defined.
    
    CRs-Fixed: 804151
    Change-Id: I7a7abc6736e2b28911f0922eef3c19736122860b

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -2041,10 +2041,17 @@ status_t ACodec::setupFlacCodec(
         }
     }
 
+#ifdef QTI_FLAC_DECODER
     return setupRawAudioFormat(
             kPortIndexInput,
             sampleRate,
             numChannels);
+#else
+    return setupRawAudioFormat(
+            encoder ? kPortIndexInput : kPortIndexOutput,
+            sampleRate,
+            numChannels);
+#endif
 }
 
 status_t ACodec::setupRawAudioFormat(
