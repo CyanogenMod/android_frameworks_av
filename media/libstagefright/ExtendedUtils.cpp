93045ceb7c480b3d64aca3657c5d05bd7161a2a5
Alexy Joseph <alexyj@codeaurora.org>
2015-04-23 18:22:08 -0700

    libstagefright: Support PCM channel mask update
    For PCM offloaded content that has a different
    channel count when compared to what is
    specified in the header, playback fails.
    Audio HAL expects the updated channel mask
    to be sent to it. Fix this by overwriting
    the channel mask retrieved from the header
    of the file format with the channel mask
    retrieved based on the decoders output
    
    Change-Id: Idf6f1efeeb764660bbb91a63e9f351d64d35a791
    CRs-Fixed: 825717

--- media/libstagefright/ExtendedUtils.cpp
+++ media/libstagefright/ExtendedUtils.cpp
@@ -1,4 +1,4 @@
-/*Copyright (c) 2013 - 2014, The Linux Foundation. All rights reserved.
+/*Copyright (c) 2013 - 2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -1966,6 +1966,10 @@ void ExtendedUtils::overWriteAudioFormat(
     int32_t schannels = 0;
     int32_t drate = 0;
     int32_t srate = 0;
+    int32_t dmask = 0;
+    int32_t smask = 0;
+    int32_t scmask = 0;
+    int32_t dcmask = 0;
 
     dst->findInt32("channel-count", &dchannels);
     src->findInt32("channel-count", &schannels);
@@ -1973,8 +1977,16 @@ void ExtendedUtils::overWriteAudioFormat(
     dst->findInt32("sample-rate", &drate);
     src->findInt32("sample-rate", &srate);
 
-    ALOGI("channel count src: %d dst: %d", dchannels, schannels);
-    ALOGI("sample rate src: %d dst:%d ", drate, srate);
+    dst->findInt32("channel-mask", &dmask);
+    src->findInt32("channel-mask", &smask);
+
+    ALOGI("channel count src: %d dst: %d", schannels, dchannels);
+    ALOGI("sample rate src: %d dst:%d ", srate, drate);
+
+    scmask = audio_channel_count_from_out_mask(smask);
+    dcmask = audio_channel_count_from_out_mask(dmask);
+    ALOGI("channel mask src: %d dst:%d ", smask, dmask);
+    ALOGI("channel count from mask src: %d dst:%d ", scmask, dcmask);
 
     if (schannels && dchannels != schannels) {
         dst->setInt32("channel-count", schannels);
@@ -1982,6 +1994,10 @@ void ExtendedUtils::overWriteAudioFormat(
 
     if (srate && drate != srate) {
         dst->setInt32("sample-rate", srate);
+    }
+
+    if (dmask != smask) {
+        dst->setInt32("channel-mask", smask);
     }
 
     return;
