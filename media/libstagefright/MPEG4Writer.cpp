fd27dfc9ba08c834c3d1b8aaf3206facf1f3d8a6
Ricardo Cerqueira <ricardo@cyngn.com>
2015-06-29 00:56:07 +0100

    Revert "libstagefright: free mMoovBoxBuffer"
    This reverts commit 5e179e6affdf7e361e1db884d34b49977db71367, it
    causes a MAPERR in concurrent/crossed camera accesses (like trying
    to record on a camera locked by another process)
    
    Fixes android.hardware.cts.CameraTest#testLockUnlock
    
    Change-Id: I2379dc257fc0d5e610a4142186d3100d5bcea6f6

--- media/libstagefright/MPEG4Writer.cpp
+++ media/libstagefright/MPEG4Writer.cpp
@@ -851,8 +851,6 @@ void MPEG4Writer::release() {
     mFd = -1;
     mInitCheck = NO_INIT;
     mStarted = false;
-    free(mMoovBoxBuffer);
-    mMoovBoxBuffer = NULL;
 }
 
 status_t MPEG4Writer::reset() {
