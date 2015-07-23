ad0c79ec27d6534ef7f4496859c5e9cb261153a8
Sridhar Vashist <svashist@motorola.com>
2015-07-23 13:48:54 -0500

    DO NOT MERGE AwesomePlayer: Stop posting buffering events once at EOS
    Stop posting buffering events once at end of stream to avoid
    perpetually holding the 'TimedEventQueue' wakelock in libstagefright.
    
    Change-Id: I3b8012886f2c27e830ce215b14090c35825635cd
    Signed-off-by: Sridhar Vashist <svashist@motorola.com>

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -826,7 +826,8 @@ void AwesomePlayer::onBufferingUpdate() {
         }
     }
 
-    if (mFlags & (PLAYING | PREPARING | CACHE_UNDERRUN)) {
+    if ( ((mFlags & PLAYING) && !eos) ||
+         (mFlags & (PREPARING | CACHE_UNDERRUN)) ) {
         postBufferingEvent_l();
     }
 }
