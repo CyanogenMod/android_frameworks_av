12777ca8c9409908d4f6e63975b2d3b7619954ce
Caio Schnepper <caioschnepper@gmail.com>
2015-08-05 16:17:14 -0300

    libstagefright: correct stride alignment on exynos4
    On the OMX_COLOR_FormatYUV420Planar color format case, the google's vp9 is used,
    in that case the colors were shifting 16 bytes per horizontal line,
    causing green lines to appear on video playback
    
    Change-Id: I4cf0fd40b79e53882d99f1c3f263c8a98fff1f30

--- media/libstagefright/colorconversion/SoftwareRenderer.cpp
+++ media/libstagefright/colorconversion/SoftwareRenderer.cpp
@@ -225,10 +225,19 @@ void SoftwareRenderer::render(
 
         uint8_t *dst_y = (uint8_t *)dst;
         size_t dst_y_size = buf->stride * buf->height;
+
+#ifdef EXYNOS4_ENHANCEMENTS
+        size_t dst_c_stride = buf->stride / 2;
+        size_t dst_c_size = dst_c_stride * buf->height / 2;
+        size_t dst_c_size_aligned = ALIGN(buf->stride / 2, 16) * buf->height / 2;
+        uint8_t *dst_v = dst_y + dst_y_size;
+        uint8_t *dst_u = dst_v + dst_c_size_aligned;
+#else
         size_t dst_c_stride = ALIGN(buf->stride / 2, 16);
         size_t dst_c_size = dst_c_stride * buf->height / 2;
         uint8_t *dst_v = dst_y + dst_y_size;
         uint8_t *dst_u = dst_v + dst_c_size;
+#endif
 
         for (int y = 0; y < mCropHeight; ++y) {
             memcpy(dst_y, src_y, mCropWidth);
