aae8b53765e2b4bd2fcde03db6c5a5013ee4e353
Vishwath Mohan <vishwath@google.com>
2015-08-11 15:54:42 -0700

    Fix benign unsigned integer overflows
    Bug: 23110888
    Change-Id: I1894a61e94a274795cab7fb23a81f3a611717c1d
    (cherry picked from 25f191c5cb2ec532e534be646c32806f9f85e196)

--- media/libstagefright/httplive/PlaylistFetcher.cpp
+++ media/libstagefright/httplive/PlaylistFetcher.cpp
@@ -1690,7 +1690,8 @@ status_t PlaylistFetcher::extractAndQueueAccessUnitsFromTs(const sp<ABuffer> &bu
     }
 
     status_t err = OK;
-    for (size_t i = mPacketSources.size(); i-- > 0;) {
+    for (size_t i = mPacketSources.size(); i > 0;) {
+        i--;
         sp<AnotherPacketSource> packetSource = mPacketSources.valueAt(i);
 
         const LiveSession::StreamType stream = mPacketSources.keyAt(i);
@@ -1814,7 +1815,8 @@ status_t PlaylistFetcher::extractAndQueueAccessUnitsFromTs(const sp<ABuffer> &bu
     }
 
     if (err != OK) {
-        for (size_t i = mPacketSources.size(); i-- > 0;) {
+        for (size_t i = mPacketSources.size(); i > 0;) {
+            i--;
             sp<AnotherPacketSource> packetSource = mPacketSources.valueAt(i);
             packetSource->clear();
         }
