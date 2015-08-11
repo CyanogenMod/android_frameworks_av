Schischu/aosp_android_frameworks_av

h263_enc: Add test
The test accepts a raw YUV file, encoding parameters as input
and dumps encoded bitstream to a file.

To run the test, use following command
m4v_h263_enc_test <input yuv> <output file> <mode> <width> <height> <frame rate> <bitrate in kbps>

Change-Id: I68155c4bb181c1330f49f46aa1d28ff17b3bf03a
Signed-off-by: Ashok Bhat <ashok.bhat@arm.com>

media/libstagefright/codecs/m4v_h263/enc/Android.mk
@@ -73,3 +73,23 @@ LOCAL_MODULE := libstagefright_soft_mpeg4enc
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+################################################################################
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := \
+        test/m4v_h263_enc_test.cpp
+
+LOCAL_C_INCLUDES := \
+        $(LOCAL_PATH)/src \
+        $(LOCAL_PATH)/include
+
+LOCAL_CFLAGS := -DOSCL_EXPORT_REF= -DOSCL_IMPORT_REF= -DBX_RC
+
+LOCAL_STATIC_LIBRARIES := \
+        libstagefright_m4vh263enc
+
+LOCAL_MODULE := libstagefright_m4vh263enc_test
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)

CyanogenMod/android_frameworks_av

Bug: 17722566

media/libstagefright/codecs/m4v_h263/enc/Android.mk
@@ -77,3 +77,23 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Werror
 
 include $(BUILD_SHARED_LIBRARY)
+
+################################################################################
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := \
+        test/m4v_h263_enc_test.cpp
+
+LOCAL_C_INCLUDES := \
+        $(LOCAL_PATH)/src \
+        $(LOCAL_PATH)/include
+
+LOCAL_CFLAGS := -DOSCL_EXPORT_REF= -DOSCL_IMPORT_REF= -DBX_RC
+
+LOCAL_STATIC_LIBRARIES := \
+        libstagefright_m4vh263enc
+
+LOCAL_MODULE := libstagefright_m4vh263enc_test
+LOCAL_MODULE_TAGS := tests
+
+include $(BUILD_EXECUTABLE)