Schischu/aosp_android_frameworks_av

Add test for AMR-WB decoder
The test accepts a .amr file as input and dumps output PCM
in a wav file.

To run the test, use following command
libstagefright_amrwbdec_test <input amr> <output wav>

Change-Id: Icbc780561b774cf02b48a6a23fd7c04e8e984ab6
Signed-off-by: Ashok Bhat <ashok.bhat@arm.com>

media/libstagefright/codecs/amrwb/Android.mk
@@ -53,3 +53,24 @@ LOCAL_CFLAGS := \
 LOCAL_MODULE := libstagefright_amrwbdec
 
 include $(BUILD_STATIC_LIBRARY)
+
+################################################################################
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := \
+        test/amrwbdec_test.cpp
+
+LOCAL_C_INCLUDES := \
+        $(LOCAL_PATH)/src \
+        $(LOCAL_PATH)/include \
+        $(call include-path-for, audio-utils)
+
+LOCAL_STATIC_LIBRARIES := \
+        libstagefright_amrwbdec libsndfile
+
+LOCAL_SHARED_LIBRARIES := \
+        libaudioutils
+
+LOCAL_MODULE := libstagefright_amrwbdec_test
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)

CyanogenMod/android_frameworks_av

Bug: 17722566

media/libstagefright/codecs/amrwb/Android.mk
@@ -55,3 +55,24 @@ LOCAL_CFLAGS += -Werror
 LOCAL_MODULE := libstagefright_amrwbdec
 
 include $(BUILD_STATIC_LIBRARY)
+
+################################################################################
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := \
+        test/amrwbdec_test.cpp
+
+LOCAL_C_INCLUDES := \
+        $(LOCAL_PATH)/src \
+        $(LOCAL_PATH)/include \
+        $(call include-path-for, audio-utils)
+
+LOCAL_STATIC_LIBRARIES := \
+        libstagefright_amrwbdec libsndfile
+
+LOCAL_SHARED_LIBRARIES := \
+        libaudioutils
+
+LOCAL_MODULE := libstagefright_amrwbdec_test
+LOCAL_MODULE_TAGS := tests
+
+include $(BUILD_EXECUTABLE)