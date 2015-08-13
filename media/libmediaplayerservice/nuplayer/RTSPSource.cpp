6d3cd2e22c73e5b554a2c4e34d51616f8737e571
Robert Shih <robertshih@google.com>
2015-08-13 12:09:52 -0700

    RTSPSource::pause mHandler NULL check
    Bug: 23151568
    Change-Id: I2dba3e7388b1b84b8b762dbc82c8e5330a158d97

--- media/libmediaplayerservice/nuplayer/RTSPSource.cpp
+++ media/libmediaplayerservice/nuplayer/RTSPSource.cpp
@@ -134,7 +134,9 @@ void NuPlayer::RTSPSource::pause() {
             return;
         }
     }
-    mHandler->pause();
+    if (mHandler != NULL) {
+        mHandler->pause();
+    }
 }
 
 void NuPlayer::RTSPSource::resume() {
