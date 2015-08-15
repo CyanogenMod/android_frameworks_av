c62db0899831827d2db8ed41b5ccbb3124b73be4
Neel Mehta <nmehta@google.com>
2015-08-14 17:38:48 -0700

    Fix for memory corruption in ID3::removeUnsynchronizationV2_4(). Bug: 23227354
    Change-Id: Iaa36cfda4fd84ca7e039f56086fd61b4118020db
    (cherry picked from commit 77e23413a539df16503e356bd4df4a952f3abc47)
    Ticket: CYNGNOS-985
    (cherry picked from commit d1efa0b104da5bd595b359e3a8a240896e2646a7)

--- media/libstagefright/id3/ID3.cpp
+++ media/libstagefright/id3/ID3.cpp
@@ -349,7 +349,7 @@ bool ID3::removeUnsynchronizationV2_4(bool iTunesHack) {
         if (flags & 1) {
             // Strip data length indicator
 
-            if (mSize < 14 || mSize - 14 < offset) {
+            if (mSize < 14 || mSize - 14 < offset || dataSize < 4) {
                 return false;
             }
             memmove(&mData[offset + 10], &mData[offset + 14], mSize - offset - 14);
