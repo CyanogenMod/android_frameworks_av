da73b6c7474aaa5616f0214e238776f12717f32b
Eric Laurent <elaurent@google.com>
2015-08-20 16:18:53 -0700

    audioflinger: increase shared memory heap size
    Bug: 21093153.
    Change-Id: I389af11451b01ce49fdb8957e2f322ba1925a62e

--- services/audioflinger/AudioFlinger.h
+++ services/audioflinger/AudioFlinger.h
@@ -88,6 +88,12 @@ class ServerProxy;
 
 static const nsecs_t kDefaultStandbyTimeInNsecs = seconds(3);
 
+
+// Max shared memory size for audio tracks and audio records per client process
+static const size_t kClientSharedHeapSizeBytes = 1024*1024;
+// Shared memory size multiplier for non low ram devices
+static const size_t kClientSharedHeapSizeMultiplier = 4;
+
 #define INCLUDING_FROM_AUDIOFLINGER_H
 
 class AudioFlinger :
@@ -423,7 +429,7 @@ private:
                             Client(const Client&);
                             Client& operator = (const Client&);
         const sp<AudioFlinger> mAudioFlinger;
-        const sp<MemoryDealer> mMemoryDealer;
+              sp<MemoryDealer> mMemoryDealer;
         const pid_t         mPid;
 
         Mutex               mTimedTrackLock;
