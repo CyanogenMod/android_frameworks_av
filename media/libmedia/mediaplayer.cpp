e096621f94f708b8634ad76333e6ac1882b46328
Marco Nelissen <marcone@google.com>
2015-09-02 12:56:09 -0700

    Remove unused code
    Change-Id: Ib4f194a1180b832ad39d9ded0962184e6daf5e29

--- media/libmedia/mediaplayer.cpp
+++ media/libmedia/mediaplayer.cpp
@@ -181,22 +181,6 @@ status_t MediaPlayer::setDataSource(int fd, int64_t offset, int64_t length)
     return err;
 }
 
-status_t MediaPlayer::setDataSource(const sp<IStreamSource> &source)
-{
-    ALOGV("setDataSource");
-    status_t err = UNKNOWN_ERROR;
-    const sp<IMediaPlayerService>& service(getMediaPlayerService());
-    if (service != 0) {
-        sp<IMediaPlayer> player(service->create(this, mAudioSessionId));
-        if ((NO_ERROR != doSetRetransmitEndpoint(player)) ||
-            (NO_ERROR != player->setDataSource(source))) {
-            player.clear();
-        }
-        err = attachNewPlayer(player);
-    }
-    return err;
-}
-
 status_t MediaPlayer::setDataSource(const sp<IDataSource> &source)
 {
     ALOGV("setDataSource(IDataSource)");
