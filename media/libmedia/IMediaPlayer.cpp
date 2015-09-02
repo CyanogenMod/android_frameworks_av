ca3612155e90618fbc06fe7b4433d1493f16f82e
Robert Shih <robertshih@google.com>
2015-09-02 16:46:59 -0700

    Zero out return values in media binder calls
    More specifically when handling:
    * GET_STREAM_VOLUME in IAudioPolicyService, and
    * GET_CURRENT_POSITION and GET_DURATION in IMediaPlayer
    
    This prevents leaking uninitialized values across binder in error cases.
    
    Bug: 23756261
    Change-Id: I0ffd900ab12b685b0611259ade4a3efb1ec5defe
    (cherry picked from commit 892354335d49f0b9fcd10e20e0c13e3cd0f1f1cb)

--- media/libmedia/IMediaPlayer.cpp
+++ media/libmedia/IMediaPlayer.cpp
@@ -441,7 +441,7 @@ status_t BnMediaPlayer::onTransact(
         } break;
         case GET_CURRENT_POSITION: {
             CHECK_INTERFACE(IMediaPlayer, data, reply);
-            int msec;
+            int msec = 0;
             status_t ret = getCurrentPosition(&msec);
             reply->writeInt32(msec);
             reply->writeInt32(ret);
@@ -449,7 +449,7 @@ status_t BnMediaPlayer::onTransact(
         } break;
         case GET_DURATION: {
             CHECK_INTERFACE(IMediaPlayer, data, reply);
-            int msec;
+            int msec = 0;
             status_t ret = getDuration(&msec);
             reply->writeInt32(msec);
             reply->writeInt32(ret);
