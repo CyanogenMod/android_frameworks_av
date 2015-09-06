c0b59484750c1ca6e0f9b811eaefd280b4201489
Rudolf Tammekivi <rtammekivi@gmail.com>
2015-09-06 13:00:28 +0300

    Revert "audioflinger: fix static track end detection"
    Causing bad audio stuttering with normal audio playback on lower-end devices.
    
    This reverts commit 41045ac5fbb441c044216005ae6e1d1e9f1bd3ae.
    
    Change-Id: I98e82d2870ff9af192b940cab7ed7f2d90ca9a57

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -3216,8 +3216,15 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::MixerThread::prepareTrac
                 (mMixerStatusIgnoringFastTracks == MIXER_TRACKS_READY)) {
             minFrames = desiredFrames;
         }
-
-        size_t framesReady = track->framesReady();
+        // It's not safe to call framesReady() for a static buffer track, so assume it's ready
+        size_t framesReady;
+        if (track->sharedBuffer() == 0) {
+            framesReady = track->framesReady();
+        } else if (track->isStopped()) {
+            framesReady = 0;
+        } else {
+            framesReady = 1;
+        }
         if ((framesReady >= minFrames) && track->isReady() &&
                 !track->isPaused() && !track->isTerminated())
         {
