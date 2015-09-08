ad8e19f9c668c6b338ae7f1f55e57fa3804d1456
Wonsik Kim <wonsik@google.com>
2015-09-08 17:32:28 +0900

    DO NOT MERGE NuCachedSource2: fix possible erroneous early free
    Because the constructor of NuCachedSource2 sent a message to
    AHandlerReflector object, AHandlerReflector::onMessageReceived could
    have executed just before the object gets wrapped in a strong
    pointer, resulting in erroneous early free. Fix the issue by using
    static Create function to ensure the message is sent after the
    object is wrapped in a sp.
    
    Ticket: CYNGNOS-1299
    Bug: 23882800
    
    Change-Id: I38a9d7a3083f184b4c81d0b00ba1661721278855
    (cherry picked from commit 0d35dd2068d6422c3c77fb68f248cbabf3d0b10c)

--- media/libstagefright/DataSource.cpp
+++ media/libstagefright/DataSource.cpp
@@ -336,7 +336,7 @@ sp<DataSource> DataSource::CreateFromURI(
                 *contentType = httpSource->getMIMEType();
             }
 
-            source = new NuCachedSource2(
+            source = NuCachedSource2::Create(
                     httpSource,
                     cacheConfig.isEmpty() ? NULL : cacheConfig.string(),
                     disconnectAtHighwatermark);
