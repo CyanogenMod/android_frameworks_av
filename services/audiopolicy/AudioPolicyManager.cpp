2f8020025db6a447edbd422ea952a1013498e273
Steve Kondik <steve@cyngn.com>
2015-09-12 18:41:36 -0700

    audiopolicy: Fix the incall fix
     * Use audio attributes instead of stream type so this doesn't apply to
       other streams which should stay unchanged.
    
    JALEBI-654
    
    Change-Id: Ia1760134eb30594f2d7f399675f8d409549522c7
    (cherry picked from commit 4d973926f66a67d61afbd7f4bd815152ea809460)

--- services/audiopolicy/AudioPolicyManager.cpp
+++ services/audiopolicy/AudioPolicyManager.cpp
@@ -1417,6 +1417,15 @@ status_t AudioPolicyManager::getOutputForAttr(const audio_attributes_t *attr,
 #endif
 
     *stream = streamTypefromAttributesInt(&attributes);
+
+#ifdef DEEP_BUFFER_RINGTONE
+    // don't use low latency for ringtones as it could cause i/o starvation
+    // in usecases like camera where a device may be in thermal mitigation
+    if (attributes.usage == AUDIO_USAGE_NOTIFICATION_TELEPHONY_RINGTONE) {
+        flags = AUDIO_OUTPUT_FLAG_DEEP_BUFFER;
+    }
+#endif
+
     *output = getOutputForDevice(device, session, *stream,
                                  samplingRate, format, channelMask,
                                  flags, offloadInfo);
@@ -1637,14 +1646,6 @@ audio_io_handle_t AudioPolicyManager::getOutputForDevice(
         //use DEEP_BUFFER as default output for music stream type
         flags = AUDIO_OUTPUT_FLAG_DEEP_BUFFER;
     }
-
-#ifdef DEEP_BUFFER_RINGTONE
-    // don't use low latency for ringtones as it could cause i/o starvation
-    // in usecases like camera where a device may be in thermal mitigation
-    if (stream == AUDIO_STREAM_RING) {
-        flags = AUDIO_OUTPUT_FLAG_DEEP_BUFFER;
-    }
-#endif
 
     sp<IOProfile> profile;
 
