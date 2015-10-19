bbb9b1da50edbbed193b342be19b62e232254f27
Praveen Chavan <pchavan@codeaurora.org>
2015-10-19 14:44:52 -0700

    GenericSource: Synchronize access to dataSource
    DataSource and HTTPSource can be accessed/modified by
    GenericSource's looper and Client's thread which can
    lead to race conditions when copying the sp<>.
    Add a Mutex lock to synchronize such accesses and avoid
    race conditions.
    
    CRs-Fixed: 906899
    Change-Id: I2fb4b4a7079e638e151f4fe67a780007a4011652

--- media/libmediaplayerservice/nuplayer/GenericSource.h
+++ media/libmediaplayerservice/nuplayer/GenericSource.h
@@ -136,6 +136,7 @@ protected:
     int64_t mOffset;
     int64_t mLength;
 
+    Mutex mSourceLock;
     sp<DataSource> mDataSource;
     sp<NuCachedSource2> mCachedSource;
     sp<DataSource> mHttpSource;
