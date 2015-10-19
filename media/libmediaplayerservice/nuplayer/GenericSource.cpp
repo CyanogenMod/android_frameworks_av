224858e719d045c8554856b12c4ab73d2375cf33
Wei Jia <wjia@google.com>
2015-10-19 16:14:14 -0700

    GenericSource: reset mDrmManagerClient when mDataSource is cleared.
    Bug: 25070434
    Change-Id: Iade3472c496ac42456e42db35e402f7b66416f5b
    (cherry picked from commit b41fd0d4929f0a89811bafcc4fd944b128f00ce2)

--- media/libmediaplayerservice/nuplayer/GenericSource.cpp
+++ media/libmediaplayerservice/nuplayer/GenericSource.cpp
@@ -381,6 +381,7 @@ void NuPlayer::GenericSource::notifyPreparedAndCleanup(status_t err) {
             {
                 Mutex::Autolock _l(mDisconnectLock);
                 mDataSource.clear();
+                mDrmManagerClient = NULL;
                 mCachedSource.clear();
                 mHttpSource.clear();
             }
