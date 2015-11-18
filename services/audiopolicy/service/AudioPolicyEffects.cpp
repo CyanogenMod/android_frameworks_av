011db2f6d47db469f186dd6d7a0f59786cc19876
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-18 10:22:19 -0500

    av: add TARGET_IGNORE_VENDOR_AUDIO_EFFECTS_CONF
    Devices with a vendor partition that include audio_effects.conf are
    hitting this and always using their audio_effects.conf, rather than
    the one we need for AudioFX.
    
    Adding this opt-in will always load the audio_effects.conf file in
    /system/etc and always ignore the one in /vendor/etc and /system/vendor/etc
    if the device opted-in to the vendor partition symlink
    
    TODO: test it
    
    Change-Id: Iaaf5f561629c720e303617837e59bb83a395c645

--- services/audiopolicy/service/AudioPolicyEffects.cpp
+++ services/audiopolicy/service/AudioPolicyEffects.cpp
@@ -40,11 +40,15 @@ namespace android {
 AudioPolicyEffects::AudioPolicyEffects()
 {
     // load automatic audio effect modules
+#ifdef TARGET_IGNORE_VENDOR_AUDIO_EFFECTS_CONF
+    loadEffectConfigFile(AUDIO_EFFECT_DEFAULT_CONFIG_FILE);
+#else
     if (access(AUDIO_EFFECT_VENDOR_CONFIG_FILE, R_OK) == 0) {
         loadAudioEffectConfig(AUDIO_EFFECT_VENDOR_CONFIG_FILE);
     } else if (access(AUDIO_EFFECT_DEFAULT_CONFIG_FILE, R_OK) == 0) {
         loadAudioEffectConfig(AUDIO_EFFECT_DEFAULT_CONFIG_FILE);
     }
+#endif
 }
 
 
