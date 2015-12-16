dccfa760cd2aa56b2ae493fd4a2e5327942f4302
Steve Kondik <steve@cyngn.com>
2015-12-16 17:28:32 -0500

    audiopolicy: Set sane defaults for QC audio features
    Change-Id: If91536d2be0165c90effccdfa2b92722223eb905

--- services/audiopolicy/enginedefault/Android.mk
+++ services/audiopolicy/enginedefault/Android.mk
@@ -32,7 +32,7 @@ LOCAL_C_INCLUDES := \
 LOCAL_MULTILIB := $(AUDIOSERVER_MULTILIB)
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PROXY_DEVICE)),true)
+ifneq ($(strip $(AUDIO_FEATURE_ENABLED_PROXY_DEVICE)),false)
 LOCAL_CFLAGS += -DAUDIO_EXTN_AFE_PROXY_ENABLED
 endif
 endif
