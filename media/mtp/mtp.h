fd86bc29d0bd59620e3c115e3b7426a163016a89
Jaesung Chung <jaesung@google.com>
2015-12-23 16:16:43 +0100

    Add DNG format in MTP
    Bug: 25871812
    Change-Id: I141d97cb8866edcaa7cab7fe04721e4386832d2b

--- media/mtp/mtp.h
+++ media/mtp/mtp.h
@@ -93,6 +93,7 @@
 #define MTP_FORMAT_TIFF_IT                              0x380E   // Tag Image File Format for Information Technology (graphic arts)
 #define MTP_FORMAT_JP2                                  0x380F   // JPEG2000 Baseline File Format
 #define MTP_FORMAT_JPX                                  0x3810   // JPEG2000 Extended File Format
+#define MTP_FORMAT_DNG                                  0x3811   // Digital Negative
 #define MTP_FORMAT_UNDEFINED_FIRMWARE                   0xB802
 #define MTP_FORMAT_WINDOWS_IMAGE_FORMAT                 0xB881
 #define MTP_FORMAT_UNDEFINED_AUDIO                      0xB900
