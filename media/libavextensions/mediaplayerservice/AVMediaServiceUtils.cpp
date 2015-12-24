8a69897a5a6a461e9c564dbab88bf966ededb55e
Leena Winterrowd <lenhardw@codeaurora.org>
2015-12-23 17:56:47 -0800

    libavextensions: Add LOG_NDEBUG define
    LOG_NDEBUG is inherited indirectly from both MyHandler.h & Log.h if
    no LOG_NDEBUG is defined locally within AVMediaServiceUtils. Add a
    local definition to keep debug usage for this file straightforward.
    
    Change-Id: I878cdc747e8349e937b13dd27c738d45f99dee0a

--- media/libavextensions/mediaplayerservice/AVMediaServiceUtils.cpp
+++ media/libavextensions/mediaplayerservice/AVMediaServiceUtils.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 - 2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013 - 2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -28,6 +28,7 @@
  */
 
 #define LOG_TAG "AVMediaServiceUtils"
+//#define LOG_NDEBUG 0
 #include <utils/Log.h>
 
 #include <media/stagefright/foundation/ADebug.h>
