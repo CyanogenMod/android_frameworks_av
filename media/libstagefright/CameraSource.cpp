8f2f779fb645c92e2c4714a9cec237b26b9dc5ec
Ethan Chen <intervigil@gmail.com>
2016-01-03 14:09:23 -0800

    libstagefright: Support YVU420SemiPlanar camera format
    Change-Id: I631ba6267b3769d9da498329a39ac89c609be6d8

--- media/libstagefright/CameraSource.cpp
+++ media/libstagefright/CameraSource.cpp
@@ -158,6 +158,10 @@ static int32_t getColorFormat(const char* colorFormat) {
         return OMX_COLOR_FormatAndroidOpaque;
     }
 
+    if (!strcmp(colorFormat, "YVU420SemiPlanar")) {
+        return OMX_QCOM_COLOR_FormatYVU420SemiPlanar;
+    }
+
     ALOGE("Uknown color format (%s), please add it to "
          "CameraSource::getColorFormat", colorFormat);
 
