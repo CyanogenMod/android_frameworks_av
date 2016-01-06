6d7f179e602b72780344c01f4fdb047f2504827c
c457 <android.c357@gmail.com>
2016-01-05 21:06:56 -0600

    av: squashed support for pre-kitkat audio blobs
    commit 40c41a743a4345ad26b6eab41755c5b474f75853
    Author: chrmhoffmann <chrmhoffmann@gmail.com>
    Date:   Sat Nov 29 19:32:27 2014 +0100
    
        pre kitkat audio legacy policy fix for hotword (ok google)
    
        Change-Id: I5bb90fb90f373461a98609226a5cea50ac5266e9
    
    commit d47b306954b96142323c79b976edeb3eaa3b46dd
    Author: chrmhoffmann <chrmhoffmann@gmail.com>
    Date:   Sun Nov 23 18:22:35 2014 +0100
    commit d47b306954b96142323c79b976edeb3eaa3b46dd
    Author: chrmhoffmann <chrmhoffmann@gmail.com>
    Date:   Sun Nov 23 18:22:35 2014 +0100
    
        pre-kitkat audio policy blobs re-activated.
    
        Change-Id: Idfcb03d0a4aa0a099bf5a17b7bf592ab402e3255
    
    Change-Id: Iae65c6ccb6dcb120a6fc20978b8a69e6af39a51a

--- services/audiopolicy/service/AudioPolicyInterfaceImplLegacy.cpp
+++ services/audiopolicy/service/AudioPolicyInterfaceImplLegacy.cpp
@@ -275,6 +275,11 @@ status_t AudioPolicyService::getInputForAttr(const audio_attributes_t *attr,
         return BAD_VALUE;
     }
 
+#ifdef HAVE_PRE_KITKAT_AUDIO_POLICY_BLOB
+    if (inputSource == AUDIO_SOURCE_HOTWORD)
+        inputSource = AUDIO_SOURCE_VOICE_RECOGNITION;
+#endif
+
     sp<AudioPolicyEffects>audioPolicyEffects;
     {
         Mutex::Autolock _l(mLock);
@@ -524,6 +529,11 @@ status_t AudioPolicyService::queryDefaultPreProcessing(int audioSession,
 
 bool AudioPolicyService::isOffloadSupported(const audio_offload_info_t& info)
 {
+#ifdef HAVE_PRE_KITKAT_AUDIO_POLICY_BLOB
+    return false;
+#else
+    // Do nothing
+#endif
     if (mpAudioPolicy == NULL) {
         ALOGV("mpAudioPolicy == NULL");
         return false;
