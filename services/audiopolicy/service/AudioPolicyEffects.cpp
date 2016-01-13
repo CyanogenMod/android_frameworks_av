0a8453132087c300eddcd4ede9c47942184efaae
Steve Kondik <steve@cyngn.com>
2016-01-12 16:34:27 -0800

    av: Add support for loading audio_effects_vendor.conf
     * In case a device has a /vendor partition which cannot be
       modified, this allows us to short-circuit the audio_effects.conf
       file which might be placed there with one of our own which
       lives on /system.
    
    Change-Id: Ief87bd4cfba2c3188b0dff122d91f773b7f3d92d

--- services/audiopolicy/service/AudioPolicyEffects.cpp
+++ services/audiopolicy/service/AudioPolicyEffects.cpp
@@ -42,7 +42,9 @@ AudioPolicyEffects::AudioPolicyEffects(AudioPolicyService *audioPolicyService) :
     mAudioPolicyService(audioPolicyService)
 {
     // load automatic audio effect modules
-    if (access(AUDIO_EFFECT_VENDOR_CONFIG_FILE, R_OK) == 0) {
+    if (access(AUDIO_EFFECT_VENDOR_CONFIG_FILE2, R_OK) == 0) {
+        loadAudioEffectConfig(AUDIO_EFFECT_VENDOR_CONFIG_FILE2);
+    } else if (access(AUDIO_EFFECT_VENDOR_CONFIG_FILE, R_OK) == 0) {
         loadAudioEffectConfig(AUDIO_EFFECT_VENDOR_CONFIG_FILE);
     } else if (access(AUDIO_EFFECT_DEFAULT_CONFIG_FILE, R_OK) == 0) {
         loadAudioEffectConfig(AUDIO_EFFECT_DEFAULT_CONFIG_FILE);
