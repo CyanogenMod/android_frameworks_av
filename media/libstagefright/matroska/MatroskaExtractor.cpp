9b4eb876ed1dfe6089d1616b2d5361cdbf682bbe
Steve Kondik <shade@chemlab.org>
2016-02-29 17:32:19 -0800

    stagefright: Don't crash on invalid / null AVCC atoms in MKV
     * Seen in the wild. If a file contains an invalid track, skip it.
     * Also correct AVCC atom size check in Matroska extractor.
    
    REF: CYNGNOS-2168
    Change-Id: I589aadbd689c9a00e1dca613e61fcec5b06ed69a

--- media/libstagefright/matroska/MatroskaExtractor.cpp
+++ media/libstagefright/matroska/MatroskaExtractor.cpp
@@ -224,18 +224,19 @@ MatroskaSource::MatroskaSource(
     mIsAudio = !strncasecmp("audio/", mime, 6);
 
     if (!strcasecmp(mime, MEDIA_MIMETYPE_VIDEO_AVC)) {
-        mType = AVC;
-
         uint32_t dummy;
         const uint8_t *avcc;
         size_t avccSize;
         CHECK(meta->findData(
                     kKeyAVCC, &dummy, (const void **)&avcc, &avccSize));
 
-        CHECK_GE(avccSize, 5u);
-
-        mNALSizeLen = 1 + (avcc[4] & 3);
-        ALOGV("mNALSizeLen = %zu", mNALSizeLen);
+        if (avccSize < 7) {
+            ALOGW("Invalid AVCC atom in track, size %zu", avccSize);
+        } else {
+            mNALSizeLen = 1 + (avcc[4] & 3);
+            ALOGV("mNALSizeLen = %zu", mNALSizeLen);
+            mType = AVC;
+        }
     } else if (!strcasecmp(mime, MEDIA_MIMETYPE_VIDEO_HEVC)) {
         mType = HEVC;
 
