dc893d2abab9a5b8ba5a6e4cb76791ea5ac9ceae
Marco Nelissen <marcone@google.com>
2016-03-10 14:29:26 -0800

    Fix initialization of AAC presentation struct
    Otherwise the new size checks trip on this.
    
    Ticket: CYNGNOS-2707
    Bug: 27207275
    Change-Id: I1f8f01097e3a88ff041b69279a6121be842f1766

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -2535,6 +2535,7 @@ status_t ACodec::setupAACCodec(
             : OMX_AUDIO_AACStreamFormatMP4FF;
 
     OMX_AUDIO_PARAM_ANDROID_AACPRESENTATIONTYPE presentation;
+    InitOMXParams(&presentation);
     presentation.nMaxOutputChannels = maxOutputChannelCount;
     presentation.nDrcCut = drc.drcCut;
     presentation.nDrcBoost = drc.drcBoost;
