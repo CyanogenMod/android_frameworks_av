8b0d8600b5e5c869755ffb542ca864df283908ad
Marco Nelissen <marcone@google.com>
2016-03-16 10:32:05 -0700

    Add VPX output buffer size check
    and handle dead observers more gracefully
    
    Ticket: CYNGNOS-2373
    Bug: 27597103
    Change-Id: Id7acb25d5ef69b197da15ec200a9e4f9e7b03518
    (cherry picked from commit ca00182c94d2ec1bdc9baeb1385e0cf3de9171d1)

--- media/libstagefright/omx/OMX.cpp
+++ media/libstagefright/omx/OMX.cpp
@@ -179,7 +179,12 @@ void OMX::binderDied(const wp<IBinder> &the_late_who) {
         Mutex::Autolock autoLock(mLock);
 
         ssize_t index = mLiveNodes.indexOfKey(the_late_who);
-        CHECK(index >= 0);
+
+        if (index < 0) {
+            ALOGE("b/27597103, nonexistent observer on binderDied");
+            android_errorWriteLog(0x534e4554, "27597103");
+            return;
+        }
 
         instance = mLiveNodes.editValueAt(index);
         mLiveNodes.removeItemsAt(index);
