Jackeagle/caf_frameworks_av

audio: Update anchor time only when sink start is successful
- During offload (WH) to non-offload fallback (BT),
  anchor time is getting updated incorrectly with
  offload renderer position when it tries to start
  sink using offload mode only.

- update anchor time post resume only when sink is
  started successfully.

Change-Id: I2c2992afa90506e08389226964b0bd5a5be07529
CRs-Fixed: 978596

media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -1548,7 +1548,7 @@ void NuPlayer::Renderer::onResume() {
             notifyAudioTearDown();
         }
         //Update anchor time after resuming playback.
-        if (offloadingAudio()) {
+        if (offloadingAudio() && err == OK) {
             int64_t nowUs = ALooper::GetNowUs();
             int64_t nowMediaUs =
                 mAudioFirstAnchorTimeMediaUs + getPlayedOutAudioDurationUs(nowUs);

CyanogenMod/android_frameworks_av

media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -1591,7 +1591,7 @@ void NuPlayer::Renderer::onResume() {
             notifyAudioTearDown();
         }
         //Update anchor time after resuming playback.
-        if (offloadingAudio()) {
+        if (offloadingAudio() && err == NO_ERROR) {
             int64_t nowUs = ALooper::GetNowUs();
             int64_t nowMediaUs =
                 mAudioFirstAnchorTimeMediaUs + getPlayedOutAudioDurationUs(nowUs);