f024eb7812b12a7710f91443ae9c72c1fb86dc6d
Marco Nelissen <marcone@google.com>
2016-04-08 10:04:48 -0700

    h264dec: check for overflows when calculating allocation size.
    Ticket: CYNGNOS-2707
    Bug: 27855419
    Change-Id: Idabedca52913ec31ea5cb6a6109ab94e3fb2badd
    (cherry picked from commit a3dd713893658baf50df88b261aba83bb0c40687)

--- media/libstagefright/codecs/on2/h264dec/source/h264bsd_decoder.c
+++ media/libstagefright/codecs/on2/h264dec/source/h264bsd_decoder.c
@@ -101,7 +101,7 @@ u32 h264bsdInit(storage_t *pStorage, u32 noOutputReordering)
      * specific NEON optimized "memset" for clearing the structure */
     size = (sizeof(macroblockLayer_t) + 63) & ~0x3F;
 
-    pStorage->mbLayer = (macroblockLayer_t*)H264SwDecMalloc(size);
+    pStorage->mbLayer = (macroblockLayer_t*)H264SwDecMalloc(size, 1);
     if (!pStorage->mbLayer)
         return HANTRO_NOK;
 
