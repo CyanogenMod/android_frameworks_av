8456ddd1e4c3daa8f32ea1cbb94c70275e5a22f5
Wei Jia <wjia@google.com>
2016-04-22 14:46:43 -0700

    NuPlayer: ignore stale message for teardown.
    Bug: 28121584
    Change-Id: I9df088b4d99ebae38c06a1138f3d66b31bf67529

--- media/libmediaplayerservice/nuplayer/NuPlayer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayer.cpp
@@ -1144,6 +1144,11 @@ void NuPlayer::onMessageReceived(const sp<AMessage> &msg) {
                 int32_t reason;
                 CHECK(msg->findInt32("reason", &reason));
                 ALOGV("Tear down audio with reason %d.", reason);
+                if (reason == Renderer::kDueToTimeout && !(mPaused && mOffloadAudio)) {
+                    // TimeoutWhenPaused is only for offload mode.
+                    ALOGW("Receive a stale message for teardown.");
+                    break;
+                }
                 int64_t positionUs;
                 if (!msg->findInt64("positionUs", &positionUs)) {
                     positionUs = mPreviousSeekTimeUs;
