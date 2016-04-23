3f9eb321481de3e118632a594bf1b0c9001c281c
Steve Kondik <steve@cyngn.com>
2016-04-22 18:32:39 -0700

    audiopolicy: Add AudioSessionInfo API
     * This patch introduces a new API which allows applications to
       query the state of the audio effects system, and receive
       callbacks with the necessary information to attach effects
       to any stream.
     * In the future, this may come as part of the AudioPort system,
       but since that's an active area of development by Google, we
       will dodge it for now.
     * The policy now simply keeps a refcounted list of objects which
       hold various bits of stream metadata. Callbacks are sent on
       stream open/close to applications which might be listening
       for them.
    
    Change-Id: I2d554d36e1378f4eb7b276010a3bfe8345c22ecd

--- include/media/IAudioPolicyService.h
+++ include/media/IAudioPolicyService.h
@@ -165,6 +165,9 @@ public:
                                       const audio_attributes_t *attributes,
                                       audio_io_handle_t *handle) = 0;
     virtual status_t stopAudioSource(audio_io_handle_t handle) = 0;
+
+    virtual status_t listAudioSessions(audio_stream_type_t streams,
+                                       Vector< sp<AudioSessionInfo>> &sessions) = 0;
 };
 
 
