1438f0b215040eb92e2b1e799f757ad32d5f8f22
Marco Nelissen <marcone@google.com>
2016-05-13 10:43:19 -0700

    DO NOT MERGE limit mediaserver memory
    Limit mediaserver using rlimit, to prevent it from bringing down the system
    via the low memory killer.
    Default max is 65% of total RAM, but can be customized via system property.
    
    Bug: 28471206
    Bug: 28615448
    Change-Id: I14fac1e12b5f3983be08a21bfbfc54feedbf3f16

--- media/mediaserver/main_mediaserver.cpp
+++ media/mediaserver/main_mediaserver.cpp
@@ -33,12 +33,18 @@
 #include "CameraService.h"
 #include "MediaLogService.h"
 #include "MediaPlayerService.h"
+#include "MediaUtils.h"
 #include "AudioPolicyService.h"
 
 using namespace android;
 
-int main(int argc, char** argv)
+int main(int argc __unused, char** argv)
 {
+    limitProcessMemory(
+        "ro.media.maxmem", /* property that defines limit */
+        SIZE_MAX, /* upper limit in bytes */
+        65 /* upper limit as percentage of physical RAM */);
+
     signal(SIGPIPE, SIG_IGN);
     char value[PROPERTY_VALUE_MAX];
     bool doLog = (property_get("ro.test_harness", value, "0") > 0) && (atoi(value) == 1);
