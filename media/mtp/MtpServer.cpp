120a94ab7d101ec7840a20c12b110b227e9325c0
Tom Marshall <tdm@cyngn.com>
2016-06-02 10:35:32 -0700

    MTP: Fix crash when no storages are available
    Change-Id: I6d7202ade46a5d781a3db5a1a3bdde17c8e70a60

--- media/mtp/MtpServer.cpp
+++ media/mtp/MtpServer.cpp
@@ -138,7 +138,7 @@ void MtpServer::removeStorage(MtpStorage* storage) {
 
 MtpStorage* MtpServer::getStorage(MtpStorageID id) {
     if (id == 0)
-        return mStorages[0];
+        return mStorages.empty() ? NULL : mStorages[0];
     for (size_t i = 0; i < mStorages.size(); i++) {
         MtpStorage* storage = mStorages[i];
         if (storage->getStorageID() == id)
