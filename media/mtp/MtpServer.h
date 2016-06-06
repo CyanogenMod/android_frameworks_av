863c7b4cec425b1b78f363e59b6f23e29ce9566a
Tom Marshall <tdm@cyngn.com>
2016-06-06 10:09:50 -0700

    MtpServer: Fix concurrent access to mStorages
     * getStorage is called both internally with mMutex held and externally
       without mMutex held.  Create getStorageLocked for internal use and
       make getStorage a wrapper.
    
     * hasStorage is only called internally with mMutex held.  Make it a
       private method.
    
    Change-Id: I8f73310ad6cca14cd88b8e29f20cc181b3a4fac3

--- media/mtp/MtpServer.h
+++ media/mtp/MtpServer.h
@@ -95,8 +95,6 @@ public:
     virtual             ~MtpServer();
 
     MtpStorage*         getStorage(MtpStorageID id);
-    inline bool         hasStorage() { return mStorages.size() > 0; }
-    bool                hasStorage(MtpStorageID id);
     void                addStorage(MtpStorage* storage);
     void                removeStorage(MtpStorage* storage);
 
@@ -108,6 +106,10 @@ public:
     void                sendObjectUpdated(MtpObjectHandle handle);
 
 private:
+    MtpStorage*         getStorageLocked(MtpStorageID id);
+    inline bool         hasStorage() { return mStorages.size() > 0; }
+    bool                hasStorage(MtpStorageID id);
+
     void                sendStoreAdded(MtpStorageID id);
     void                sendStoreRemoved(MtpStorageID id);
     void                sendEvent(MtpEventCode code, uint32_t param1);
