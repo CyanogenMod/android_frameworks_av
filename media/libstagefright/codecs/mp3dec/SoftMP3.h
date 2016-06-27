3003db631fe3d69c85723f4a4976e67752f6d984
Robert Shih <robertshih@google.com>
2016-06-27 13:55:14 -0700

    SoftMP3: memset safely
    CYNGNOS-3235
    Bug: 29422022
    Change-Id: I70c9e33269d16bf8c163815706ac24e18e34fe97

--- media/libstagefright/codecs/mp3dec/SoftMP3.h
+++ media/libstagefright/codecs/mp3dec/SoftMP3.h
@@ -75,6 +75,7 @@ private:
 
     void initPorts();
     void initDecoder();
+    void *memsetSafe(OMX_BUFFERHEADERTYPE *outHeader, int c, size_t len);
 
     DISALLOW_EVIL_CONSTRUCTORS(SoftMP3);
 };
