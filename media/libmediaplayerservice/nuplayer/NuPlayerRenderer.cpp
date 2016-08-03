e14296b136ae4fe679d1730ab09e2ae374048677
Sharad Sangle <assangle@codeaurora.org>
2016-08-03 22:31:14 +0530

    audio: do not update anchor time before opening of audio sink
     In onNewAudioMediaTime mAnchorTimeMediaUs is getting updated
     even if the mediaClock's anchor time is not updated in case of
     Audio sink is not started, this causes video frame drops as
     video checks mAnchorTimeMediaUs to decide media clock is
     initialized or not.
    
     Make sure mAnchorTimeMediaUs is not updated if mediaClocks's
     anchor time is not updated in onNewAudioMediaTime
    
     CRs-fixed: 1037592
    
    Change-Id: Icfd741bf8c14889e8ba525de6908815579e9de20

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -1112,6 +1112,7 @@ void NuPlayer::Renderer::onNewAudioMediaTime(int64_t mediaTimeUs) {
         if (nowUs >= mNextAudioClockUpdateTimeUs) {
             int64_t nowMediaUs = mediaTimeUs - getPendingAudioPlayoutDurationUs(nowUs);
             mMediaClock->updateAnchor(nowMediaUs, nowUs, mediaTimeUs);
+            mAnchorTimeMediaUs = mediaTimeUs;
             mUseVirtualAudioSink = false;
             mNextAudioClockUpdateTimeUs = nowUs + kMinimumAudioClockUpdatePeriodUs;
         }
@@ -1129,11 +1130,11 @@ void NuPlayer::Renderer::onNewAudioMediaTime(int64_t mediaTimeUs) {
             // and it's paced by system clock.
             ALOGW("AudioSink stuck. ARE YOU CONNECTED TO AUDIO OUT? Switching to system clock.");
             mMediaClock->updateAnchor(mAudioFirstAnchorTimeMediaUs, nowUs, mediaTimeUs);
+             mAnchorTimeMediaUs = mediaTimeUs;
             mUseVirtualAudioSink = true;
         }
     }
     mAnchorNumFramesWritten = mNumFramesWritten;
-    mAnchorTimeMediaUs = mediaTimeUs;
 }
 
 // Called without mLock acquired.
