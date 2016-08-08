8c2a47a8b0d788d324828865b18384284eb52bdd
ferhung <ferhung27@gmail.com>
2016-08-08 09:23:20 +0800

    Mediatek: fix building.

--- media/libstagefright/omx/OMXNodeInstance.cpp
+++ media/libstagefright/omx/OMXNodeInstance.cpp
@@ -1706,7 +1706,7 @@ OMX::buffer_id OMXNodeInstance::makeBufferID(OMX_BUFFERHEADERTYPE *bufferHeader)
     return (OMX::buffer_id)bufferHeader;
 }
 
-OMX_BUFFERHEADERTYPE *OMXNodeInstance::findBufferHeader(OMX::buffer_id buffer) {
+OMX_BUFFERHEADERTYPE *OMXNodeInstance::findBufferHeader(OMX::buffer_id buffer, OMX_U32 portIndex) {
     return (OMX_BUFFERHEADERTYPE *)buffer;
 }
 
