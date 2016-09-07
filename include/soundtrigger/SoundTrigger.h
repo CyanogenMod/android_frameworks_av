8a4af104602559ed2f72fbe66b9c674df11f954f
Eric Laurent <elaurent@google.com>
2016-09-07 16:58:39 -0700

    SoundTrigger: get service by value.
    Get strong pointer to service interface by value and not reference to
    prevent race conditions where the service pointer can be cleared
    by another thread while in use.
    
    RM-290
    
    Bug: 30907212
    Change-Id: I6f02ec3fd1e6392b842b334e1cc4f9aa23916009
    (cherry picked from commit 3b01b3019b1c62132792a1a94e2dc1f7cfc84f2e)

--- include/soundtrigger/SoundTrigger.h
+++ include/soundtrigger/SoundTrigger.h
@@ -68,7 +68,7 @@ public:
 private:
             SoundTrigger(sound_trigger_module_handle_t module,
                             const sp<SoundTriggerCallback>&);
-            static const sp<ISoundTriggerHwService>& getSoundTriggerHwService();
+            static const sp<ISoundTriggerHwService> getSoundTriggerHwService();
 
             Mutex                               mLock;
             sp<ISoundTrigger>                   mISoundTrigger;
