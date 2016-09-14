c2934acc782f7024b16c26a45ad2f652a825b44c
Lajos Molnar <lajos@google.com>
2016-09-14 10:01:37 -0700

    IOMX: allow configuration after going to loaded state
    This was disallowed recently but we still use it as MediaCodcec.stop
    only goes to loaded state, and does not free component.
    
    Bug: 31450460
    Change-Id: I72e092e4e55c9f23b1baee3e950d76e84a5ef28d
    (cherry picked from commit 6392ae13cf6e15c706fce45396ec1e79af6171a9)

--- media/libstagefright/omx/OMXNodeInstance.cpp
+++ media/libstagefright/omx/OMXNodeInstance.cpp
@@ -372,8 +372,7 @@ status_t OMXNodeInstance::freeNode(OMXMaster *master) {
 status_t OMXNodeInstance::sendCommand(
         OMX_COMMANDTYPE cmd, OMX_S32 param) {
     if (cmd == OMX_CommandStateSet) {
-        // We do not support returning from unloaded state, so there are no configurations past
-        // first StateSet command.
+        // There are no configurations past first StateSet command.
         mSailed = true;
     }
     const sp<GraphicBufferSource> bufferSource(getGraphicBufferSource());
@@ -1843,6 +1842,13 @@ void OMXNodeInstance::onEvent(
             && arg1 == OMX_CommandStateSet
             && arg2 == OMX_StateExecuting) {
         bufferSource->omxExecuting();
+    }
+
+    // allow configuration if we return to the loaded state
+    if (event == OMX_EventCmdComplete
+            && arg1 == OMX_CommandStateSet
+            && arg2 == OMX_StateLoaded) {
+        mSailed = false;
     }
 }
 
