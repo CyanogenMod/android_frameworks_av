16ba670d0368808756810adbaf6b59a193ec580c
Arne Coucheron <arco68@gmail.com>
2016-10-09 00:12:59 +0200

    mediaserver: Fix LOCAL_CFLAGS
     * Don't override previous defined CFLAGS
    
    Change-Id: I28ab1597e13e53fa9d41569b6bc8b6eb77cbb86d

--- media/mediaserver/Android.mk
+++ media/mediaserver/Android.mk
@@ -35,15 +35,15 @@ LOCAL_C_INCLUDES := \
     frameworks/av/services/camera/libcameraservice \
     frameworks/av/services/mediaresourcemanager \
 
-ifneq ($(BOARD_NUMBER_OF_CAMERAS),)
-    LOCAL_CFLAGS += -DMAX_CAMERAS=$(BOARD_NUMBER_OF_CAMERAS)
-endif
-
 LOCAL_MODULE:= mediaserver
 LOCAL_32_BIT_ONLY := true
 
 LOCAL_INIT_RC := mediaserver.rc
 
 LOCAL_CFLAGS := -Werror -Wall
+
+ifneq ($(BOARD_NUMBER_OF_CAMERAS),)
+    LOCAL_CFLAGS += -DMAX_CAMERAS=$(BOARD_NUMBER_OF_CAMERAS)
+endif
 
 include $(BUILD_EXECUTABLE)
