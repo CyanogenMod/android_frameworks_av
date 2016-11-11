819bb2f417584cee45b1f825609328707cd5fcb8
LuK1337 <priv.luk@gmail.com>
2016-11-11 19:10:42 +0100

    OMXNodeInstance: Fix legacy HAL1 after merge of android-7.1.0_r7
    Change-Id: I53985a966b891b902437f71d4ebccbd68138329e

--- media/libstagefright/omx/Android.mk
+++ media/libstagefright/omx/Android.mk
@@ -37,6 +37,12 @@ LOCAL_CFLAGS += -DQTI_FLAC_DECODER
 endif
 endif
 
+ifneq ($(TARGET_USES_MEDIA_EXTENSIONS),true)
+ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1),true)
+LOCAL_CFLAGS += -DCAMCORDER_GRALLOC_SOURCE
+endif
+endif
+
 LOCAL_MODULE:= libstagefright_omx
 LOCAL_CFLAGS += -Werror -Wall
 LOCAL_CLANG := true
