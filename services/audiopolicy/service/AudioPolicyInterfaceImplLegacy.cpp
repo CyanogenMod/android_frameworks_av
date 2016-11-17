976694c87a00333cf41dde76081e80dd5a0b61d9
corphish <d97.avinaba@gmail.com>
2016-11-17 19:30:55 +0530

    audiopolicylegacy: Declare unused function parameters as 'unused'
    - Builds fail for devices using USE_LEGACY_AUDIO_POLICY := 1,
      due to unused parameters in some functions.
    - So actually declare them unused to fix the build.
    
    Change-Id: Id58d3e1316aaa4c547edc63248e3c504f272b1c3

--- services/audiopolicy/service/AudioPolicyInterfaceImplLegacy.cpp
+++ services/audiopolicy/service/AudioPolicyInterfaceImplLegacy.cpp
@@ -622,30 +622,30 @@ status_t AudioPolicyService::registerPolicyMixes(Vector<AudioMix> mixes __unused
     return INVALID_OPERATION;
 }
 
-status_t AudioPolicyService::startAudioSource(const struct audio_port_config *source,
-                                  const audio_attributes_t *attributes,
-                                  audio_io_handle_t *handle)
+status_t AudioPolicyService::startAudioSource(const struct audio_port_config *source __unused,
+                                  const audio_attributes_t *attributes __unused,
+                                  audio_io_handle_t *handle __unused)
 {
     return INVALID_OPERATION;
 }
 
-status_t AudioPolicyService::stopAudioSource(audio_io_handle_t handle)
+status_t AudioPolicyService::stopAudioSource(audio_io_handle_t handle __unused)
 {
     return INVALID_OPERATION;
 }
 
-status_t AudioPolicyService::setMasterMono(bool mono)
+status_t AudioPolicyService::setMasterMono(bool mono __unused)
 {
     return INVALID_OPERATION;
 }
 
-status_t AudioPolicyService::getMasterMono(bool *mono)
+status_t AudioPolicyService::getMasterMono(bool *mono __unused)
 {
     return INVALID_OPERATION;
 }
 
-status_t AudioPolicyService::listAudioSessions(audio_stream_type_t streams,
-                                  Vector< sp<AudioSessionInfo>> &sessions)
+status_t AudioPolicyService::listAudioSessions(audio_stream_type_t streams __unused,
+                                  Vector< sp<AudioSessionInfo>> &sessions __unused)
 {
     return INVALID_OPERATION;
 }
