a728027b0ae2ee98c14fcc4e2341185567f52700
dianlujitao <dianlujitao@gmail.com>
2016-12-20 08:10:50 +0800

    audiopolicy: Remove duplicated AUDIO_FEATURE_ENABLED_PROXY_DEVICE flag
    Change-Id: I21ba8b27b7d400511dde6a8dc396e667f578a9de

--- services/audiopolicy/common/managerdefinitions/Android.mk
+++ services/audiopolicy/common/managerdefinitions/Android.mk
@@ -38,9 +38,6 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_FLAC_OFFLOAD)),true)
 LOCAL_CFLAGS     += -DFLAC_OFFLOAD_ENABLED
 endif
-ifneq ($(strip $(AUDIO_FEATURE_ENABLED_PROXY_DEVICE)),false)
-LOCAL_CFLAGS     += -DAUDIO_EXTN_AFE_PROXY_ENABLED
-endif
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_WMA_OFFLOAD)),true)
 LOCAL_CFLAGS     += -DWMA_OFFLOAD_ENABLED
 endif
@@ -79,7 +76,7 @@ LOCAL_SRC_FILES += \
 endif #ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PROXY_DEVICE)),true)
+ifneq ($(strip $(AUDIO_FEATURE_ENABLED_PROXY_DEVICE)),false)
 LOCAL_CFLAGS += -DAUDIO_EXTN_AFE_PROXY_ENABLED
 endif
 endif
