f91b925d22377b58d6903ae1e3b586f4afe627e8
mautz-kernel <motorrad@posteo.de>
2018-11-12 02:16:08 +0100

    Revert "IOMX: do not clear buffer if it's allocated by component"
    This reverts commit 81f4830629cc97dc378840500c9711eb8336d39d.

--- media/libstagefright/omx/OMXNodeInstance.cpp
+++ media/libstagefright/omx/OMXNodeInstance.cpp
@@ -1184,6 +1184,7 @@ status_t OMXNodeInstance::allocateBufferWithBackup(
     }
 
     CHECK_EQ(header->pAppPrivate, buffer_meta);
+    memset(header->pBuffer, 0, header->nAllocLen);
 
     *buffer = makeBufferID(header);
 
